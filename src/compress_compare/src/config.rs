3be01669423be8b8e9d4b96bc7269044367e99cc
Marvin Löbel <loebel.marvin@gmail.com>
2015-10-07 16:04:11 +0200

    Added compress_compare tool to the repository

---
+++ src/compress_compare/src/config.rs
@@ -0,0 +1,79 @@
+use toml;
+use std::collections::BTreeMap;
+
+#[derive(Debug)]
+pub struct Profile {
+    pub inputs: Vec<String>,
+    pub commands: Vec<(String, String, String)>,
+    pub compare_commands: bool,
+    pub with_mem: bool,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub profiles: BTreeMap<String, Profile>,
+}
+
+#[derive(Debug)]
+pub struct TomlErr;
+
+fn parse_profile(name: &str, toml: &toml::Table) -> Result<Profile, TomlErr> {
+    let mut profile = Profile {
+        commands: vec![],
+        inputs: vec![],
+        compare_commands: true,
+        with_mem: false,
+    };
+
+    let toml_entry = try! {
+        toml.get(name).and_then(|entry| entry.as_table()).ok_or(TomlErr)
+    };
+
+    if let Some(parent) = toml_entry.get("inherit_from") {
+        let parent = try!(parent.as_str().ok_or(TomlErr));
+        profile = try!(parse_profile(parent, toml));
+    }
+
+    if let Some(commands) = toml_entry.get("commands") {
+        let commands = try!(commands.as_slice().ok_or(TomlErr));
+        for command in commands {
+            let command = try!(command.as_slice().ok_or(TomlErr));
+            let cmd0 = try!(command.get(0).and_then(|s| s.as_str()).ok_or(TomlErr)).to_owned();
+            let cmd1 = try!(command.get(1).and_then(|s| s.as_str()).ok_or(TomlErr)).to_owned();
+            let cmd2 = try!(command.get(2).and_then(|s| s.as_str()).ok_or(TomlErr)).to_owned();
+
+            profile.commands.push((cmd0, cmd1, cmd2));
+        }
+    }
+
+    if let Some(inputs) = toml_entry.get("inputs") {
+        let inputs = try!(inputs.as_slice().ok_or(TomlErr));
+        for input in inputs {
+            let input = try!(input.as_str().ok_or(TomlErr)).to_owned();
+
+            profile.inputs.push(input);
+        }
+    }
+
+    if let Some(compare_commands) = toml_entry.get("compare_commands") {
+        profile.compare_commands = try! {
+            compare_commands.as_bool().ok_or(TomlErr)
+        };
+    }
+
+    if let Some(with_mem) = toml_entry.get("with_mem") {
+        profile.with_mem = try! {
+            with_mem.as_bool().ok_or(TomlErr)
+        };
+    }
+
+    Ok(profile)
+}
+
+pub fn parse_config(toml: toml::Table) -> Result<Config, TomlErr> {
+    let mut config = Config { profiles: BTreeMap::new() };
+    for (key, _) in &toml {
+        config.profiles.insert(key.to_owned(), try!(parse_profile(key, &toml)));
+    }
+    Ok(config)
+}
---
+++ src/compress_compare/src/main.rs
@@ -0,0 +1,146 @@
+extern crate time;
+extern crate toml;
+
+use std::process::*;
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::thread;
+use std::io::Read;
+
+mod config;
+
+
+fn nice_size(size: u64) -> String {
+    if size >= 1024 * 1024 {
+        format!("{:6.2} MiB", (size as f64) / 1024.0 / 1024.0)
+    } else if size >= 1024 {
+        format!("{:6.2} KiB", (size as f64) / 1024.0)
+    } else {
+        format!("{:6.2} B  ", (size as f64))
+    }
+}
+
+fn load_config(arg: &str) -> config::Config {
+    let mut toml = String::new();
+    fs::File::open(&arg)
+        .expect("config file not found")
+        .read_to_string(&mut toml)
+        .unwrap();
+    let toml = toml::Parser::new(&toml)
+        .parse()
+        .expect("config file not in valid TOML format");
+
+    config::parse_config(toml).expect("Invalid config file")
+}
+
+// type, value, time, size, compsize, compratio
+type Line<'a> = (&'a str, &'a str, f64, &'a str, &'a str, f64);
+type LinePad = (usize,);
+
+fn print_header(kind: &str, value: &str, padding: LinePad) {
+    println!("{:4} | {:6$} | {:^9} | {:^12} | {:^12} | {:^8}",
+        kind, value, "time", "size", "comp. size", "ratio", padding.0);
+}
+
+fn print_line(line: Line, padding: LinePad) {
+    println!("{:4} | {:6$} | {:7.3} s | {:>12} | {:>12} | {:6.2} %",
+        line.0, line.1, line.2, line.3, line.4, line.5, padding.0);
+}
+
+fn print_sep(padding: LinePad) {
+    println!("{:-^4}---{:-^6$}---{:-^7}-----{:-^12}---{:-^12}---{:-^6}--",
+        "", "", "", "", "", "", padding.0);
+}
+
+// let mem_cmd = r##"valgrind --tool=massif --pages-as-heap=yes --massif-out-file=${mofile} ${cmd}; grep mem_heap_B ${mofile} | sed -e 's/mem_heap_B=\(.*\)/\1/' | sort -g | tail -n 1"##;
+
+fn alphabet_size(file: &str) -> usize {
+    let mut bytes = [0u64; 256];
+    let file = fs::File::open(file).unwrap();
+    for b in file.bytes() {
+        bytes[b.unwrap() as usize] += 1;
+    }
+    let mut counter = 0;
+    for &count in &bytes[..] {
+        if count > 0 {
+            counter += 1;
+        }
+    }
+    counter
+}
+
+fn main() {
+    let arg = &env::args().nth(1).expect("need to be given a filename");
+    let profile_name = &env::args().nth(2).expect("need to be given a profile");
+
+    let config = load_config(&arg);
+    let profile = &config.profiles[profile_name];
+
+    let inputs = profile.inputs.iter().map(|input| {
+        (input, fs::metadata(input).expect("input file does not exist").len())
+    }).collect::<Vec<_>>();
+
+    let input = inputs[0].0;
+    let input_size = inputs[0].1;
+
+    let commands = &profile.commands;
+    let commands: Vec<_> = commands.iter().map(|&(ref cmd, ref cargs, ref ending)| {
+        let command_output = format!("{}.{}",
+            Path::new(input).file_name().unwrap().to_str().unwrap()
+            , ending);
+        let c = format!("{} {}", cmd, cargs);
+        let command = c.replace("${1}", input)
+                       .replace("${2}", &command_output);
+        (command, command_output, &cmd[..])
+    }).collect();
+
+    let headers = inputs.iter().map(|x| &x.0);
+    let rows = commands.iter().map(|x| &x.2);
+
+    let padding = headers.map(|s| s.len())
+                         .chain(rows.map(|s| s.len()))
+                         .max()
+                         .unwrap_or(0);
+    let padding = (padding,);
+
+    let run_row = |kind: &str, command: &str, file: &str, value: &str| {
+        let mut cmd = Command::new("bash");
+        cmd.arg("-c").arg(&format!("rm {}", file));
+        cmd.output().unwrap();
+
+        let run = command;
+        let mut cmd = Command::new("bash");
+        cmd.arg("-c").arg(run);
+        let time_start = time::precise_time_ns();
+        cmd.output().expect("command not successful");
+        let time_end = time::precise_time_ns();
+        let time_span = ((time_end - time_start) as f64)
+            / 1000.0
+            / 1000.0
+            / 1000.0;
+
+        let out_size = fs::metadata(file).unwrap().len();
+        let cmd = value;
+        let comp_size = &nice_size(out_size);
+        let inp_size = &nice_size(input_size);
+        let ratio = (out_size as f64) / (input_size as f64) * 100.0;
+        print_line(
+            (kind, cmd, time_span, inp_size, comp_size, ratio),
+            padding
+        );
+    };
+
+    if profile.compare_commands {
+
+    } else {
+
+    }
+
+    print_header("file", &input, padding);
+    print_sep(padding);
+    for command in &commands {
+        run_row("cmd", &command.0, &command.1, &command.2);
+    }
+
+}
