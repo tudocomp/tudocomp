0fe2ea33c15a2645963d3b957e1a3b2e1a0d782a
Marvin Löbel <loebel.marvin@gmail.com>
2016-01-20 14:30:22 +0100

    Tried to make cmakelist cmake 2.8 compatible

--- cmakemodules/FindGlog.cmake
+++ cmakemodules/FindGlog.cmake
@@ -4,7 +4,7 @@
 # The following variables are optionally searched for defaults
 #  GLOG_ROOT_DIR:            Base directory where all GLOG components are found
 #
-# The following are set after configuration is done: 
+# The following are set after configuration is done:
 #  GLOG_FOUND
 #  GLOG_INCLUDE_DIRS
 #  GLOG_LIBRARIES
@@ -40,10 +40,12 @@ else()
             lib64)
 endif()
 
-find_package_handle_standard_args(GLOG DEFAULT_MSG
+find_package_handle_standard_args(Glog DEFAULT_MSG
     GLOG_INCLUDE_DIR GLOG_LIBRARY)
 
 if(GLOG_FOUND)
     set(GLOG_INCLUDE_DIRS ${GLOG_INCLUDE_DIR})
     set(GLOG_LIBRARIES ${GLOG_LIBRARY})
+else()
+
 endif()
