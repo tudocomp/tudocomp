072c51723384e7bd76fd798572c11ae613073fa1
Marvin Löbel <loebel.marvin@gmail.com>
2016-01-25 12:50:27 +0100

    Still trying to fix the travis config

--- .travis-build-glog.sh
+++ .travis-build-glog.sh
@@ -2,9 +2,10 @@
 set -e
 set -x
 
-git clone https://github.com/google/glog.git
 mkdir -p $GLOG_ROOT_INSTALL
+mkdir -p $GLOG_ROOT
 pushd $GLOG_ROOT
+  git clone https://github.com/google/glog.git .
   mkdir build
   pushd build
     cmake -DCMAKE_INSTALL_PREFIX:PATH=$GLOG_ROOT_INSTALL ..
