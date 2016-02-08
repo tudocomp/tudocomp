2b8dbe1e162f5ad2ba9946a0193083660775da11
Marvin Löbel <loebel.marvin@gmail.com>
2016-02-08 15:01:50 +0100

    Prepared for real lz77 rules

---
+++ src/lz77rule/include/lz77/lz77_rule_compressor.h
@@ -0,0 +1,132 @@
+#ifndef LZ77RULE_H
+#define LZ77RULE_H
+
+#include "tudocomp.h"
+#include "rule.h"
+#include "rules.h"
+
+namespace lz77rule {
+
+using namespace tudocomp;
+
+// /// Type of the list of Rules the compression step produces
+// using Rules = std::vector<Rule>;
+
+class Lz77RuleCoder;
+class Lz77RuleCompressor;
+
+const std::string THRESHOLD_OPTION = "lz77rule.threshold";
+const std::string THRESHOLD_LOG = "lz77rule.threshold";
+const std::string RULESET_SIZE_LOG = "lz77rule.rule_count";
+
+struct Lz77Rule: public Compressor {
+    Lz77RuleCompressor* m_compressor;
+    Lz77RuleCoder* m_encoder;
+
+    inline Lz77Rule(Env& env,
+                    Lz77RuleCompressor* compressor,
+                    Lz77RuleCoder* encoder):
+        Compressor(env),
+        m_compressor(compressor),
+        m_encoder(encoder) {};
+
+    inline virtual void compress(Input& input, Output& output) override final;
+
+    inline virtual void decompress(Input& input, Output& output) override final;
+};
+
+/// Interface for a compressor into LZ77-like substitution rules.
+///
+/// A (rule-based) compressor works by receiving the Input text, and
+/// generating a list of substitution rules.
+/// The rules in combination with the input parts not covered by them
+/// can regenerate the full input.
+class Lz77RuleCompressor {
+public:
+    Env& env;
+
+    /// Class needs to be constructed with an `Env&` argument.
+    inline Lz77RuleCompressor() = delete;
+
+    /// Construct the class with an environment.
+    inline Lz77RuleCompressor(Env& env_): env(env_) {}
+
+    /// Compress the input.
+    ///
+    /// \param input The input to be compressed.
+    /// \param threshold The threshold in bytes that limits how small an area a
+    ///                  substitution rule may maximally
+    ///                  cover. For example, a threshold of 3 means no
+    ///                  rules for substitutions of length 2 should be generated.
+    /// \return The list of rules.
+    virtual Rules compress(Input& input, size_t threshold) = 0;
+};
+
+/// Interface for a coder from LZ77-like substitution rules.
+///
+/// This takes a list of Rules and the input text, and outputs
+/// an encoded form of them to a `ostream`. Also provided is a decoder,
+/// that takes such an encoded stream and outputs the fully
+/// decoded and decompressed original text.
+class Lz77RuleCoder {
+public:
+    Env& env;
+
+    /// Class needs to be constructed with an `Env&` argument.
+    inline Lz77RuleCoder() = delete;
+
+    /// Construct the class with an environment.
+    inline Lz77RuleCoder(Env& env_): env(env_) {}
+
+    /// Encode a list or Rules and the input text.
+    ///
+    /// \param rules The list of substitution rules
+    /// \param input The input text
+    /// \param out `ostream` where the encoded output will be written to.
+    virtual void code(Rules&& rules, Input& input, Output& output) = 0;
+
+    /// Decode and decompress `inp` into `out`.
+    ///
+    /// This method expects `inp` to be encoded with the same encoding
+    /// that the `code()` method emits.
+    ///
+    /// \param inp The input stream.
+    /// \param out The output stream.
+    virtual void decode(Input& inp, Output& out) = 0;
+
+    /// Return the expected minimum encoded
+    /// length in bytes of a single rule if encoded with this encoder.
+    ///
+    /// This can be used by compressors to directly filter
+    /// out rules that would not be beneficial in the encoded output.
+    ///
+    /// \param input_size The length of the input in bytes
+    virtual size_t min_encoded_rule_length(size_t input_size = SIZE_MAX) = 0;
+};
+
+inline void Lz77Rule::compress(Input& input, Output& output) {
+    uint64_t threshold = 0;
+
+    if (env.has_option(THRESHOLD_OPTION)) {
+        threshold = env.option_as<uint64_t>(THRESHOLD_OPTION);
+    } else if (input.has_size()) {
+        threshold = m_encoder->min_encoded_rule_length(input.size());
+    } else {
+        threshold = m_encoder->min_encoded_rule_length();
+    }
+
+    CHECK(threshold > 0);
+
+    env.log_stat(THRESHOLD_LOG, threshold);
+    auto rules = m_compressor->compress(input, threshold);
+    env.log_stat(RULESET_SIZE_LOG, rules.size());
+    m_encoder->code(std::move(rules), input, output);
+}
+
+inline void Lz77Rule::decompress(Input& inp, Output& out) {
+    m_encoder->decode(inp, out);
+}
+
+}
+
+#endif
---
+++ src/lz77rule/include/lz77/rule.h
@@ -0,0 +1,48 @@
+#ifndef RULE_H
+#define RULE_H
+
+#include <cstdint>
+#include <iostream>
+
+namespace lz77rule {
+
+/// A substitution rule.
+struct Rule {
+    /// Target location where that rule points to.
+    size_t target;
+    /// Source location where the rule takes the bytes from.
+    size_t source;
+    /// Size of the rule, that is number of bytes covered.
+    size_t num;
+};
+
+inline std::ostream& operator<< (std::ostream& stream, const Rule& rule) {
+    return stream << "("
+           << rule.target << ", "
+           << rule.source << ", "
+           << rule.num << ")";
+}
+
+inline bool operator== (const Rule& left, const Rule& right) {
+    return left.target == right.target
+        && left.source == right.source
+        && left.num == right.num;
+}
+
+inline bool operator!= (const Rule& left, const Rule& right) {
+    return left.target != right.target
+        || left.source != right.source
+        || left.num != right.num;
+}
+
+/// Callable object, acts as a comparator of Rules, to order them
+/// acording to target position.
+struct rule_compare {
+    inline bool operator() (const Rule& lhs, const Rule& rhs) const {
+        return lhs.target < rhs.target;
+    }
+};
+
+}
+
+#endif
