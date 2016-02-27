6bb98999a6cf1c2c08fe8e41724a566c3bd80ae2
Marvin Löbel <loebel.marvin@gmail.com>
2016-02-27 14:06:56 +0100

    Fixed commited trailing whtespace issues

--- include/tudocomp/DummyCoder.hpp
+++ include/tudocomp/DummyCoder.hpp
@@ -11,24 +11,24 @@ namespace tudocomp {
  */
 template<typename F>
 class DummyCoder : public Coder<F> {
-    
+
     const char init_sep = ':';
-    
+
     static char fact_start;
     static char fact_end;
 
 public:
     /// The default constructor is not supported.
     DummyCoder() = delete;
-    
+
     /// Constructor for an environment.
     DummyCoder(Env& env) : Coder<F>(env) {
     }
-    
+
     /// Destructor
     ~DummyCoder() {
     }
-    
+
     /// Initiates the encoding with no total length information.
     inline virtual void encode_init(Output& out) override {
         encode_init(out, 0); //TODO throw exception!
@@ -38,15 +38,15 @@ public:
     inline virtual void encode_init(Output& out, size_t len) override {
         *(out.as_stream()) << len << init_sep;
     }
-    
+
     /// Encodes a raw symbol.
     inline virtual void encode_sym(Output& out, char32_t sym) override {
         *(out.as_stream()) << char(sym);
     }
-    
+
     /// Encodes a factor of the supported type.
     virtual void encode_fact(Output& out, const F& fact) override;
-    
+
     /// Finalizes the encoding.
     inline virtual void encode_finalize(Output& out) override {
     }
@@ -60,20 +60,20 @@ public:
     inline virtual void decode(Input& in, Output& out) override {
         auto in_guard = in.as_stream();
         std::istream& ins = *in_guard;
-        
+
         //read original text length
         size_t len = 0;
         {
             std::stringbuf buf;
             ins.get(buf, init_sep); //TODO fail safety
             ins.get(); //pop delimeter
-            
+
             std::stringstream(buf.str()) >> len;
         }
-        
+
         //decode and defactorize
         DecodeBuffer decbuf(len);
-        
+
         char c;
         while(ins.get(c)) {
             if(c == fact_start) {
@@ -82,14 +82,14 @@ public:
                 std::stringbuf buf;
                 ins.get(buf, fact_end); //TODO fail safety
                 ins.get(); //pop delimeter
-                
+
                 decode_fact(decbuf, buf.str());
             } else {
                 //raw symbol
                 decbuf.push_decoded_byte(c);
             }
         }
-        
+
         decbuf.write_to(*(out.as_stream()));
     }
 };
