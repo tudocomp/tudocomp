a8f1758080f7e1eff6f8b53e1d6e91bdfe48f375
Patrick Dinklage <pdinklag@gmail.com>
2016-03-29 09:32:34 +0200

    Enter LZSS compressors and coders into algorithm registry

--- src/tudocomp_driver/tudocomp_algorithms.cpp
+++ src/tudocomp_driver/tudocomp_algorithms.cpp
@@ -10,34 +10,22 @@
 
 #include <tudocomp/lz78/lzcics/Lz78cicsCompressor.hpp>
 
-//TODO: esacomp needs to be re-inserted!
-/*
-#include "lz_compressor.h"
-#include "esacomp/esacomp_rule_compressor.h"
-#include "esa_compressor.h"
-#include "max_lcp_sorted_suffix_list.h"
-#include "max_lcp_heap.h"
-#include "dummy_compressor.h"
+#include <tudocomp/lzss/LZ77SSSlidingWindowCompressor.hpp>
+#include <tudocomp/lzss/LZ77SSLCPCompressor.hpp>
+#include <tudocomp/lzss/LZSSESACompressor.hpp>
 
-#include "code0.h"
-#include "code1.h"
-#include "code2.h"
-#include "dummy_encoder.h"
-*/
+#include <tudocomp/lzss/DebugLZSSCoder.hpp>
+#include <tudocomp/lzss/OnlineLZSSCoder.hpp>
+#include <tudocomp/lzss/OfflineLZSSCoder.hpp>
+
+#include <tudocomp/alphabet/OnlineAlphabetCoder.hpp>
+#include <tudocomp/alphabet/OfflineAlphabetCoder.hpp>
 
 namespace tudocomp_driver {
 
 using namespace tudocomp;
 
 // Algorithm implementations
-
-//TODO: esacomp needs to be re-inserted!
-/*
-using esacomp::EsacompRuleCompressor;
-using esacomp::EsacompCompressStrategy;
-using esacomp::EsacompEncodeStrategy;
-*/
-
 using lzw::LzwCompressor;
 using lzw::LzwDebugCoder;
 using lzw::LzwBitCoder;
@@ -48,12 +36,13 @@ using lz78::Lz78BitCoder;
 
 using lz78::lzcics::Lz78cicsCompressor;
 
-//TODO: esacomp needs to be re-inserted!
-/*
-using esacomp::LZCompressor;
-using esacomp::DummyCompressor;
-using esacomp::DummyCoder;
-*/
+using lzss::LZ77SSSlidingWindowCompressor;
+using lzss::LZ77SSLCPCompressor;
+using lzss::LZSSESACompressor;
+
+using lzss::DebugLZSSCoder;
+using lzss::OnlineLZSSCoder;
+using lzss::OfflineLZSSCoder;
 
 /// A small helper function for directly constructing a Compressor class.
 template<class C>
@@ -74,72 +63,64 @@ C make(Env& env) {
 //   Compressor or Encoder.
 
 void register_algos(Registry& r) {
-    //TODO: esacomp needs to be re-inserted!
-    /*
-    registry.with_info<EsacompRuleCompressor>(
-        // TODO: Name right
-        // - lz77rule ~ LZSS rules with implicit escape bits
-        // - lz ~ unknown named variant of lz with suffix array
-        "Esacomp rules", "esacomp",
-        "A Family of compression algorithms making use "
-        "of esacomp-like replacement rules.")
-    .with_sub_algos<EsacompCompressStrategy>([](AlgorithmRegistry<EsacompCompressStrategy>& registry) {
-        registry.set_name("Compressor");
-
-        registry.with_info<DummyCompressor>(
-            "Dummy", "dummy",
-            "Dummy compressor that does not produce any rules.").do_register();
-
-        registry.with_info<LZCompressor>(
-            "LZ SA", "lz",
-            "LZ impl included in esacomp. Uses a suffix array to determine the LZ factorisation up-front").do_register();
-
-        registry.with_info<ESACompressor<>>(
-            "ESA", "esa",
-            "Esacomp (defaults to using a suffix list internally).").do_register();
-
-        registry.with_info<ESACompressor<MaxLCPSortedSuffixList>>(
-            "ESA (Sorted Suffix List)", "esa_list",
-            "Esacomp using a suffix list internally.").do_register();
-
-    })
-    .with_sub_algos<EsacompEncodeStrategy>([](AlgorithmRegistry<EsacompEncodeStrategy>& registry) {
-        registry.set_name("Coder");
-
-        registry.with_info<DummyCoder>(
-            "Dummy", "dummy",
-            "Dummy encoding, outputs input text unchanged.").do_register();
-
-        registry.with_info<Code0Coder>(
-            "Code0", "debug",
-            "Human readable output of text and rules.").do_register();
-
-        registry.with_info<Code1Coder>(
-            "Code1", "code1",
-            "Byte based encoding.").do_register();
-
-        registry.with_info<Code2Coder>(
-            "Code2", "code2",
-            "Bit based encoding.").do_register();
-    })
-    .do_register();*/
-
     /// Define which algorithm combinations exist in the tool:
 
-    r.algo("lz78", "Lempel-Ziv 78 algorithm", "")
+    r.algo("lz78", "Lempel-Ziv 78", "")
         .sub_algo("Coder", [](Registry& r) {
             r.algo("bit", "Bit coder", "basic variable-bit-width encoding of the symbols");
             r.algo("debug", "Debug coder", "human readable, comma separated stream of (integer, char) tuples");
         });
-    r.algo("lzw", "Lempel-Ziv-Welch algorithm", "")
+    r.algo("lzw", "Lempel-Ziv-Welch", "")
         .sub_algo("Coder", [](Registry& r) {
             r.algo("bit", "Bit coder", "basic variable-bit-width encoding of the symbols");
             r.algo("debug", "Debug coder", "human readable, comma separated stream of integers");
         });
-    r.algo("lz78_cics", "Lempel-Ziv 78 algorithm", "")
+    r.algo("lz78_cics", "Lempel-Ziv 78", "")
         .sub_algo("Coder", [](Registry& r) {
             r.algo("bit", "Bit coder", "basic variable-bit-width encoding of the symbols");
             r.algo("debug", "Debug coder", "human readable, comma separated stream of integers");
+        });
+    r.algo("lz77ss", "Lempel-Ziv-Storer-Szymanski", "")
+        .sub_algo("Coder", [](Registry& r) {
+            r.algo("debug", "Debug coder", "direct encoding in ASCII");
+            r.algo("online", "Online factor coder", "direct encoding of factors")
+                .sub_algo("Alphabet Coder", [](Registry& r) {
+                    r.algo("online", "Online alphabet coder", "direct ASCII encoding of symbols");
+                    r.algo("offline", "Offline alphabet coder", "optimized symbol encoding using alphabet statistics");
+            });
+            r.algo("offline", "Offline factor coder", "analysis of created factors and optimized encoding")
+                .sub_algo("Alphabet Coder", [](Registry& r) {
+                    r.algo("online", "Online alphabet coder", "direct ASCII encoding of symbols");
+                    r.algo("offline", "Offline alphabet coder", "optimized symbol encoding using alphabet statistics");
+            });
+        });
+    r.algo("lz77ss_lcp", "LZ77 Factorization using LCP", "")
+        .sub_algo("Coder", [](Registry& r) {
+            r.algo("debug", "Debug coder", "direct encoding in ASCII");
+            r.algo("online", "Online factor coder", "direct encoding of factors")
+                .sub_algo("Alphabet Coder", [](Registry& r) {
+                    r.algo("online", "Online alphabet coder", "direct ASCII encoding of symbols");
+                    r.algo("offline", "Offline alphabet coder", "optimized symbol encoding using alphabet statistics");
+            });
+            r.algo("offline", "Offline factor coder", "analysis of created factors and optimized encoding")
+                .sub_algo("Alphabet Coder", [](Registry& r) {
+                    r.algo("online", "Online alphabet coder", "direct ASCII encoding of symbols");
+                    r.algo("offline", "Offline alphabet coder", "optimized symbol encoding using alphabet statistics");
+            });
+        });
+    r.algo("esacomp", "ESAComp", "")
+        .sub_algo("Coder", [](Registry& r) {
+            r.algo("debug", "Debug coder", "direct encoding in ASCII");
+            r.algo("online", "Online factor coder", "direct encoding of factors")
+                .sub_algo("Alphabet Coder", [](Registry& r) {
+                    r.algo("online", "Online alphabet coder", "direct ASCII encoding of symbols");
+                    r.algo("offline", "Offline alphabet coder", "optimized symbol encoding using alphabet statistics");
+            });
+            r.algo("offline", "Offline factor coder", "analysis of created factors and optimized encoding")
+                .sub_algo("Alphabet Coder", [](Registry& r) {
+                    r.algo("online", "Online alphabet coder", "direct ASCII encoding of symbols");
+                    r.algo("offline", "Offline alphabet coder", "optimized symbol encoding using alphabet statistics");
+            });
         });
 
     // Define which implementations to use for each combination:
@@ -154,12 +135,27 @@ void register_algos(Registry& r) {
     // But since most of the algorithms take no additional constructor
     // arguments we use the make() helper function to make this less verbose.
 
-    r.compressor("lzw.debug",       make<LzwCompressor<LzwDebugCoder>>);
-    r.compressor("lzw.bit",         make<LzwCompressor<LzwBitCoder>>);
-    r.compressor("lz78.debug",      make<Lz78Compressor<Lz78DebugCoder>>);
-    r.compressor("lz78.bit",        make<Lz78Compressor<Lz78BitCoder>>);
-    r.compressor("lz78_cics.debug", make<Lz78cicsCompressor<Lz78DebugCoder>>);
-    r.compressor("lz78_cics.bit",   make<Lz78cicsCompressor<Lz78BitCoder>>);
+    r.compressor("lzw.debug",           make<LzwCompressor<LzwDebugCoder>>);
+    r.compressor("lzw.bit",             make<LzwCompressor<LzwBitCoder>>);
+    r.compressor("lz78.debug",          make<Lz78Compressor<Lz78DebugCoder>>);
+    r.compressor("lz78.bit",            make<Lz78Compressor<Lz78BitCoder>>);
+    r.compressor("lz78_cics.debug",     make<Lz78cicsCompressor<Lz78DebugCoder>>);
+    r.compressor("lz78_cics.bit",       make<Lz78cicsCompressor<Lz78BitCoder>>);
+    r.compressor("lz77ss.debug",           make<LZ77SSSlidingWindowCompressor<DebugLZSSCoder>>);
+    r.compressor("lz77ss.online.online",   make<LZ77SSSlidingWindowCompressor<OnlineLZSSCoder<OnlineAlphabetCoder>>>);
+    r.compressor("lz77ss.online.offline",  make<LZ77SSSlidingWindowCompressor<OnlineLZSSCoder<OfflineAlphabetCoder>>>);
+    r.compressor("lz77ss.offline.online",  make<LZ77SSSlidingWindowCompressor<OfflineLZSSCoder<OnlineAlphabetCoder>>>);
+    r.compressor("lz77ss.offline.offline", make<LZ77SSSlidingWindowCompressor<OfflineLZSSCoder<OfflineAlphabetCoder>>>);
+    r.compressor("lz77ss_lcp.debug",           make<LZ77SSLCPCompressor<DebugLZSSCoder>>);
+    r.compressor("lz77ss_lcp.online.online",   make<LZ77SSLCPCompressor<OnlineLZSSCoder<OnlineAlphabetCoder>>>);
+    r.compressor("lz77ss_lcp.online.offline",  make<LZ77SSLCPCompressor<OnlineLZSSCoder<OfflineAlphabetCoder>>>);
+    r.compressor("lz77ss_lcp.offline.online",  make<LZ77SSLCPCompressor<OfflineLZSSCoder<OnlineAlphabetCoder>>>);
+    r.compressor("lz77ss_lcp.offline.offline", make<LZ77SSLCPCompressor<OfflineLZSSCoder<OfflineAlphabetCoder>>>);
+    r.compressor("esacomp.debug",           make<LZSSESACompressor<DebugLZSSCoder>>);
+    r.compressor("esacomp.online.online",   make<LZSSESACompressor<OnlineLZSSCoder<OnlineAlphabetCoder>>>);
+    r.compressor("esacomp.online.offline",  make<LZSSESACompressor<OnlineLZSSCoder<OfflineAlphabetCoder>>>);
+    r.compressor("esacomp.offline.online",  make<LZSSESACompressor<OfflineLZSSCoder<OnlineAlphabetCoder>>>);
+    r.compressor("esacomp.offline.offline", make<LZSSESACompressor<OfflineLZSSCoder<OfflineAlphabetCoder>>>);
 
 }
 
