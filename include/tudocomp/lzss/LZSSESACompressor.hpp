6808020ee359ad85aa1c42a0ec703bdc24105cb6
Patrick Dinklage <pdinklag@gmail.com>
2016-05-03 11:50:02 +0200

    ESAComp: SA/LCP processing outsourced into strategy

--- include/tudocomp/lzss/LZSSESACompressor.hpp
+++ include/tudocomp/lzss/LZSSESACompressor.hpp
@@ -3,13 +3,13 @@
 
 #include <sdsl/int_vector.hpp>
 #include <sdsl/suffix_arrays.hpp>
-#include <sdsl/lcp.hpp>
 
 #include <tudocomp/sdslex/int_vector_wrapper.hpp>
 #include <tudocomp/util.h>
-#include <tudocomp/util/MaxLCPSuffixList.hpp>
 
 #include <tudocomp/lzss/LZSSCompressor.hpp>
+
+#include <tudocomp/lzss/esacomp/ESACompMaxLCP.hpp>
 
 namespace tudocomp {
 namespace lzss {
@@ -56,49 +56,14 @@ public:
         sdsl::util::bit_compress(isa);
         sdsl::util::bit_compress(lcp);
         
-        //Construct MaxLCPSuffixList
+        //Use strategy to generate factors
         size_t fact_min = 3; //factor threshold
-        
-        MaxLCPSuffixList<sdsl::csa_bitcompressed<>, sdsl::int_vector<>> list(sa, lcp, fact_min);
-
-        //Factorize
         std::vector<LZSSFactor>& factors = LZSSCompressor<C>::m_factors;
-        
-        while(list.size() > 0) {
-            //get suffix with longest LCP
-            size_t m = list.first();
-            
-            //generate factor
-            LZSSFactor fact(sa[m], sa[m-1], lcp[m]);
-            factors.push_back(fact);
-            //DLOG(INFO) << "Factor: (" << fact.pos << ", " << fact.src << ", " << fact.num << ")";
-            
-            //remove overlapped entries
-            for(size_t k = 0; k < fact.num; k++) {
-                size_t i = isa[fact.pos + k];
-                if(list.contains(i)) {
-                    list.remove(i);
-                }
-            }
-            
-            //correct intersecting entries
-            for(size_t k = 0; k < fact.num && fact.pos > k; k++) {
-                size_t s = fact.pos - k - 1;
-                size_t i = isa[s];
-                if(list.contains(i)) {
-                    if(s + lcp[i] > fact.pos) {
-                        list.remove(i);
-                        
-                        size_t l = fact.pos - s;
-                        lcp[i] = l;
-                        if(l >= fact_min) {
-                            list.insert(i);
-                        }
-                    }
-                }
-            }
-        }
-        
+
+        ESACompMaxLCP interval_selector;
+        interval_selector.factorize(sa, isa, lcp, fact_min, factors);
+
+        //sort
         std::sort(factors.begin(), factors.end());
         return true;
     }
@@ -111,7 +76,6 @@ public:
     /// \copydoc
     inline virtual void factorize(Input& input) override {
     }
-    
 };
 
 }}
