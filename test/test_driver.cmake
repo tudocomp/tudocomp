da5faa330657f894e2a312360ef8315b2894391a
Marvin Löbel <loebel.marvin@gmail.com>
2016-05-16 13:03:53 +0200

    Updated test_driver.cmake to new gtest source repo

--- test/test_driver.cmake
+++ test/test_driver.cmake
@@ -40,11 +40,9 @@ include(ExternalProject)
 # Download and install GoogleTest
 ExternalProject_Add(
     gtest
-	GIT_REPOSITORY https://github.com/google/googletest
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    # Disable install and update step
+    GIT_REPOSITORY https://github.com/google/googletest
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
     UPDATE_COMMAND ""
-    INSTALL_COMMAND ""
 )
 
 # Create a libgtest target to be used as a dependency by test programs
@@ -54,15 +52,16 @@ add_library(libgtest_main IMPORTED STATIC GLOBAL)
 add_dependencies(libgtest_main gtest)
 
 # Set gtest properties
-ExternalProject_Get_Property(gtest source_dir binary_dir)
+ExternalProject_Get_Property(gtest source_dir install_dir)
+file(MAKE_DIRECTORY "${install_dir}/include")
 set_target_properties(libgtest PROPERTIES
-    "IMPORTED_LOCATION" "${binary_dir}/libgtest.a"
+    "IMPORTED_LOCATION" "${install_dir}/lib/libgtest.a"
     "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    #"INTERFACE_INCLUDE_DIRECTORIES" "${source_dir}/include"
+    "INTERFACE_INCLUDE_DIRECTORIES" "${install_dir}/include"
 )
 set_target_properties(libgtest_main PROPERTIES
-    "IMPORTED_LOCATION" "${binary_dir}/libgtest_main.a"
+    "IMPORTED_LOCATION" "${install_dir}/lib/libgtest_main.a"
     "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    #"INTERFACE_INCLUDE_DIRECTORIES" "${source_dir}/include"
+    "INTERFACE_INCLUDE_DIRECTORIES" "${install_dir}/include"
 )
-include_directories("${source_dir}/include")
+#include_directories("${source_dir}/include")
