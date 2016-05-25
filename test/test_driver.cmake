e5472e68903c62b336d9385c736a187b2811143a
Patrick Dinklage <pdinklag@gmail.com>
2016-05-25 09:58:16 +0200

    gtest is now an actual dependency instead of a forced download

--- test/test_driver.cmake
+++ test/test_driver.cmake
@@ -1,3 +1,6 @@
+# Grab gtest
+find_or_download_package(GTest GTEST gtest)
+
 # Custom test target to run the googletest tests
 add_custom_target(check)
 add_custom_command(
@@ -15,8 +18,7 @@ macro(run_test test_target)
         ${test_target}.cpp
     )
     target_link_libraries(${test_target}
-        libgtest
-        # glog is needed by the testrunner
+        gtest
         glog
         ${ARGN}
     )
@@ -37,37 +39,3 @@ macro(run_test test_target)
     )
 endmacro()
 
-# We need thread support
-find_package(Threads REQUIRED)
-
-# Enable ExternalProject CMake module
-include(ExternalProject)
-
-# Download and install GoogleTest
-ExternalProject_Add(
-    gtest
-    GIT_REPOSITORY https://github.com/google/googletest
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-    UPDATE_COMMAND ""
-)
-
-# Create a libgtest target to be used as a dependency by test programs
-add_library(libgtest IMPORTED STATIC GLOBAL)
-add_dependencies(libgtest gtest)
-add_library(libgtest_main IMPORTED STATIC GLOBAL)
-add_dependencies(libgtest_main gtest)
-
-# Set gtest properties
-ExternalProject_Get_Property(gtest source_dir install_dir)
-file(MAKE_DIRECTORY "${install_dir}/include")
-set_target_properties(libgtest PROPERTIES
-    "IMPORTED_LOCATION" "${install_dir}/lib/libgtest.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    "INTERFACE_INCLUDE_DIRECTORIES" "${install_dir}/include"
-)
-set_target_properties(libgtest_main PROPERTIES
-    "IMPORTED_LOCATION" "${install_dir}/lib/libgtest_main.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    "INTERFACE_INCLUDE_DIRECTORIES" "${install_dir}/include"
-)
-#include_directories("${source_dir}/include")
