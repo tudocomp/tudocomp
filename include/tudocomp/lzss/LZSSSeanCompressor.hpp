6cf542b5d4dfd15d0a0555269cf8850edcf9e574
Sean Tohidi <tohidi@gips.cs.tu-dortmund.de>
2016-06-13 10:35:07 +0200

    Completed the first LZSS compressor

--- include/tudocomp/lzss/LZSSSeanCompressor.hpp
+++ include/tudocomp/lzss/LZSSSeanCompressor.hpp
@@ -2,32 +2,35 @@
     This will be a basic implementation of the LZ77 compressor
 */
 
-#ifndef _INCLUDED_LZ77_TEST_COMPRESSOR_HPP
-#define _INCLUDED_LZ77_TEST_COMPRESSOR_HPP
+#ifndef _INCLUDED_LZSS_SEAN_COMPRESSOR_HPP
+#define _INCLUDED_LZSS_SEAN_COMPRESSOR_HPP
 
 #include <sdsl/int_vector.hpp>
 
 #include <tudocomp/util.h>
 #include <tudocomp/Compressor.hpp>
 #include <tudocomp/io.h>
+#include <tudocomp/io/BitOStream.hpp>
+#include <tudocomp/io/BitIStream.hpp>
 
 namespace tudocomp {
 namespace lzss {
 
 // const std::string WINDOW_OPTION = "lzss.window";
+const size_t SLIDING_WINDOW_SIZE = 1000;
 
 /// Computes the LZ77 factorization of the input
-class LZ77TestCompressor : public Compressor {
+class LZSSSeanCompressor : public Compressor {
 
 private:
     // size_t m_window;
 
 public:
     /// Default constructor (not supported).
-    inline LZ77TestCompressor() = delete;
+    inline LZSSSeanCompressor() = delete;
 
     /// Construct the class with an environment.
-    inline LZ77TestCompressor(Env& env) : Compressor(env) {
+    inline LZSSSeanCompressor(Env& env) : Compressor(env) {
         // m_window = env.option_as<size_t>(WINDOW_OPTION, 16);
     }
 
@@ -38,10 +41,10 @@ public:
         auto in_guard = input.as_stream();
         std::istream& in = *in_guard;
         auto out_guard = output.as_stream();
-        std::ostream& out = *out_guard;
+        BitOStream* bito = new BitOStream(*out_guard);
 
         std::stringstream input_str;
-        size_t curr_pos=0, text_length, pos, len;
+        size_t curr_pos=0, text_length, pos, len, iter_pos_limit = SLIDING_WINDOW_SIZE;
 
         // Here we grab the input text and store it in input_text
         while(in.get(c) && c != '\0') {
@@ -51,8 +54,8 @@ public:
         input_text = input_str.str();
 
         // Here we store the size of the compressed string
-        text_length = input_text.length();
-        out << text_length << ':';
+        text_length = (input_text.length());
+        bito->write_compressed_int(text_length);
 
         // Here we convert the string into compressed factors
         while(curr_pos < text_length) {
@@ -61,7 +64,8 @@ public:
             size_t iter_pos = 1;
 
             // Here we check for the longest match in the string             
-            while(curr_pos >= iter_pos) {
+            while(curr_pos >= iter_pos && iter_pos <= iter_pos_limit) {
+            // while(curr_pos >= iter_pos) {                
                 bool searchFlag = false;
                 size_t iter_len = 0, i = 0;
 
@@ -80,12 +84,13 @@ public:
                     }
                     // Here there was no match found or the matching sequence ended
                     else {
+
                         searchFlag = true;
                     } 
                 }
 
                 // We store the longest length matched sequence
-                if(len < iter_len) {
+                if(len < iter_len && iter_len > 1) {
                     len = iter_len;
                     pos = iter_pos;
                 }
@@ -93,78 +98,69 @@ public:
                 iter_pos++;
             }
 
-            // If the length is 0, we have a symbol
-            if (len == 0) {
-                pos = 0;
-            } else {
-                curr_pos += len;
-            }
-
-            // Here we output the current factor
-            out << "(" << pos << "," << len << ",";
-
-            // The $ character indicates the end of the string
-            if(curr_pos < text_length) {
-                out << input_text[curr_pos] << ")";
+            // Here we output a symbol, which is initialized with a 0 bit
+            if(len == 0) {
+                bito->writeBit(0);
+                // bito->flush();
+                // bito->write_aligned_bytes(&input_text[curr_pos],1);
+                bito->write(input_text[curr_pos], 8);
                 curr_pos++;
-            } else {
-                out << "$" << ")";
+            }
+            // Here we output a factor, which is initialized with a 1 bit
+            else {
+                curr_pos += len;
+                bito->writeBit(1);
+                // bito->flush();
+                bito->write_compressed_int(pos);
+                bito->write_compressed_int(len);
             }
         }
+
+        // Flush out the last few bits
+        bito->flush();
     }
 
     /// Decompress the input into an output
     virtual void decompress(Input& input, Output& output) override {
-        char c, nextChar='\0';
+        // char c;
+        bool done = false;
+        bool factor = false;
 
         auto in_guard = input.as_stream();
-        std::istream& in = *in_guard;
+        // std::istream& in = *in_guard;
         auto out_guard = output.as_stream();
         std::ostream& out = *out_guard;
+        BitIStream* biti = new BitIStream(*in_guard, done);
 
         std::stringstream total_len_str;
-        size_t curr_pos=0, total_len;
+        size_t curr_pos=0, total_len=0;
 
         // Here we get the length of the original string from the compressed string
-        // and use it to declare a vector of the correct size to hold the result
-        while(in.get(c) && c != ':') {
-            total_len_str << c;
-        }
-        total_len_str >> total_len;
+        // and use it to declare a vector of the correct size to hold the result        
+        total_len = biti->read_compressed_int<size_t>();
         sdsl::int_vector<8> text = sdsl::int_vector<8>(total_len, 0);
 
         // Here we start moving through each of the factors in the compressed string
-        while(in.get(c) && nextChar != '$') {
+        while(total_len > 0) {
             size_t pos, len;
-            std::stringstream pos_str, len_str;
-
-            while(in.get(c) && c != ',') {
-                pos_str << c;
+            factor = biti->readBit();
+            // biti->readBits<char>(7);
+            if(!factor) {
+;               text[curr_pos] = biti->readBits<char>(8);
+                curr_pos++;
+                total_len--;
             }
-            while(in.get(c) && c != ',') {
-                len_str << c;
-            }
-            pos_str >> pos;
-            len_str >> len;
-
-            // Factors which are not symbols get decompressed
-            if(len != 0) {
+            // Here we are decompressing a factor
+            else {
+                pos = biti->read_compressed_int<size_t>();
+                len = biti->read_compressed_int<size_t>();
                 while(len != 0) {
                     text[curr_pos] = text[curr_pos-pos];
                     curr_pos++;
+                    total_len--;
                     len--;
                 }
-            } 
-
-            // The next character is added to the text, unless we have reached the end
-            in.get(c);
-            nextChar = c;
-            if(nextChar != '$') {
-                text[curr_pos] = uint8_t(c);
-                curr_pos++;                
             }
-            in.get(c);
-
         }
 
         // Here we write the output of the decompressed string
--- include/tudocomp/lzss/LZSSSeanCompressorWithoutBitSteams.hpp
+++ include/tudocomp/lzss/LZSSSeanCompressorWithoutBitSteams.hpp
@@ -2,14 +2,15 @@
     This will be a basic implementation of the LZ77 compressor
 */
 
-#ifndef _INCLUDED_LZ77_TEST_COMPRESSOR_HPP
-#define _INCLUDED_LZ77_TEST_COMPRESSOR_HPP
+#ifndef _INCLUDED_LZSS_SEAN_COMPRESSOR_HPP
+#define _INCLUDED_LZSS_SEAN_COMPRESSOR_HPP
 
 #include <sdsl/int_vector.hpp>
 
 #include <tudocomp/util.h>
 #include <tudocomp/Compressor.hpp>
 #include <tudocomp/io.h>
+#include <tudocomp/io/BitOStream.hpp>
 
 namespace tudocomp {
 namespace lzss {
@@ -17,17 +18,17 @@ namespace lzss {
 // const std::string WINDOW_OPTION = "lzss.window";
 
 /// Computes the LZ77 factorization of the input
-class LZ77TestCompressor : public Compressor {
+class LZSSSeanCompressor : public Compressor {
 
 private:
     // size_t m_window;
 
 public:
     /// Default constructor (not supported).
-    inline LZ77TestCompressor() = delete;
+    inline LZSSSeanCompressor() = delete;
 
     /// Construct the class with an environment.
-    inline LZ77TestCompressor(Env& env) : Compressor(env) {
+    inline LZSSSeanCompressor(Env& env) : Compressor(env) {
         // m_window = env.option_as<size_t>(WINDOW_OPTION, 16);
     }
 
@@ -39,6 +40,7 @@ public:
         std::istream& in = *in_guard;
         auto out_guard = output.as_stream();
         std::ostream& out = *out_guard;
+        BitOStream* bito = new BitOStream(*out_guard);
 
         std::stringstream input_str;
         size_t curr_pos=0, text_length, pos, len;
@@ -52,7 +54,9 @@ public:
 
         // Here we store the size of the compressed string
         text_length = input_text.length();
-        out << text_length << ':';
+        // out << text_length << ':';
+        bito->write(text_length);
+        bito->write_aligned_bytes(":",1);
 
         // Here we convert the string into compressed factors
         while(curr_pos < text_length) {
@@ -80,12 +84,13 @@ public:
                     }
                     // Here there was no match found or the matching sequence ended
                     else {
+
                         searchFlag = true;
                     } 
                 }
 
                 // We store the longest length matched sequence
-                if(len < iter_len) {
+                if(len < iter_len && iter_len > 1) {
                     len = iter_len;
                     pos = iter_pos;
                 }
@@ -93,29 +98,23 @@ public:
                 iter_pos++;
             }
 
-            // If the length is 0, we have a symbol
-            if (len == 0) {
-                pos = 0;
-            } else {
-                curr_pos += len;
-            }
-
-            // Here we output the current factor
-            out << "(" << pos << "," << len << ",";
-
-            // The $ character indicates the end of the string
-            if(curr_pos < text_length) {
-                out << input_text[curr_pos] << ")";
+            // Here we output a symbol
+            if(len == 0) {
+                // out << input_text[curr_pos];
+                bito->write_aligned_bytes(&input_text[curr_pos],1);
                 curr_pos++;
-            } else {
-                out << "$" << ")";
+            }
+            // Here we output a factor
+            else {
+                curr_pos += len;
+                out << "(" << pos << "," << len << ")";
             }
         }
     }
 
     /// Decompress the input into an output
     virtual void decompress(Input& input, Output& output) override {
-        char c, nextChar='\0';
+        char c;
 
         auto in_guard = input.as_stream();
         std::istream& in = *in_guard;
@@ -134,37 +133,33 @@ public:
         sdsl::int_vector<8> text = sdsl::int_vector<8>(total_len, 0);
 
         // Here we start moving through each of the factors in the compressed string
-        while(in.get(c) && nextChar != '$') {
+        while(in.get(c)) {
             size_t pos, len;
             std::stringstream pos_str, len_str;
 
-            while(in.get(c) && c != ',') {
-                pos_str << c;
+            // Here we have a symbol
+            if(c != '(') {
+                text[curr_pos] = uint8_t(c);
+                curr_pos++;
             }
-            while(in.get(c) && c != ',') {
-                len_str << c;
-            }
-            pos_str >> pos;
-            len_str >> len;
+            // Here we are decompressing a factor
+            else {
+                while(in.get(c) && c != ',') {
+                    pos_str << c;
+                }
+                while(in.get(c) && c != ')') {
+                    len_str << c;
+                }
+                pos_str >> pos;
+                len_str >> len;
 
-            // Factors which are not symbols get decompressed
-            if(len != 0) {
+                // Factors which are not symbols get decompressed
                 while(len != 0) {
                     text[curr_pos] = text[curr_pos-pos];
                     curr_pos++;
                     len--;
                 }
-            } 
-
-            // The next character is added to the text, unless we have reached the end
-            in.get(c);
-            nextChar = c;
-            if(nextChar != '$') {
-                text[curr_pos] = uint8_t(c);
-                curr_pos++;                
             }
-            in.get(c);
-
         }
 
         // Here we write the output of the decompressed string
