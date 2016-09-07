fc7ddde61ac39fa0071af14bfa5ab7af78b36774
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2016-09-07 11:06:57 +0200

    +forward.hh

---
+++ include/tudocomp/ds/forward.hh
@@ -0,0 +1,21 @@
+#ifndef FORWARD_HH
+#define FORWARD_HH
+namespace tudocomp {
+
+template<typename T>
+class SuffixArray;
+template<typename T>
+class InverseSuffixArray;
+template<typename T>
+class PhiArray;
+template<typename T, typename sa_t, typename select_t>
+class lcp_sada;
+template<typename T>
+class LCPArray;
+
+template<template<typename> class lcp_t, template<typename> class isa_t>
+class TextDS;
+
+
+}//ns
+#endif /* FORWARD_HH */
