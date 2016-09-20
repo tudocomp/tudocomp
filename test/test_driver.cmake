d86e05732a6be95f9aca51eb966fe7e0b399bb6a
Marvin Löbel <loebel.marvin@gmail.com>
2016-09-20 14:45:25 +0200

    Added a indirection to the test build system to ensure that test executable do not get deleted if a test failed.

--- test/test_driver.cmake
+++ test/test_driver.cmake
@@ -9,6 +9,8 @@ add_custom_command(
     COMMENT "All tests were successful!" VERBATIM
 )
 
+file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/stamps)
+
 # will compile and run ${test_target}.cpp
 # and add all further arguments as dependencies
 macro(run_test test_target)
@@ -17,23 +19,44 @@ macro(run_test test_target)
     set(multiValueArgs DEPS BIN_DEPS)
     cmake_parse_arguments(TEST_TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
-    add_executable(${test_target}
+    # The test executable itself
+    add_executable(${test_target}_testrunner
         EXCLUDE_FROM_ALL
         test_driver.cpp
         ${test_target}.cpp
     )
-    target_link_libraries(${test_target}
+    target_link_libraries(${test_target}_testrunner
         gtest
         glog
         ${TEST_TARGET_DEPS}
     )
+
+    # Runs the test and generates a stamp file on success.
     add_custom_command(
-        TARGET ${test_target}
-        POST_BUILD
-        COMMAND ${test_target} #cmake 2.6 required
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
-        COMMENT "Running ${test_target} ..." VERBATIM
+        OUTPUT
+            stamps/${test_target}_testrunner.stamp
+        DEPENDS
+            ${test_target}_testrunner
+        COMMAND
+            ${test_target}_testrunner
+        COMMAND
+            cmake -E touch ${CMAKE_CURRENT_BINARY_DIR}/stamps/${test_target}_testrunner.stamp
+        WORKING_DIRECTORY
+            "${CMAKE_BINARY_DIR}"
+        COMMENT
+            "Running test ${test_target} ..."
+        VERBATIM
     )
+
+    # The test target. Depends on the stamp file to ensure the
+    # test is only run if the source changed
+    add_custom_target(
+        ${test_target}
+        DEPENDS
+            stamps/${test_target}_testrunner.stamp
+    )
+
+    # Hook into check target
     add_custom_command(
         TARGET check
         PRE_BUILD
