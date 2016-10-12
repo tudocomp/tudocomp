04faaeb3b41d8e3b5a45c7c56249a64a73bc0458
Marvin Löbel <loebel.marvin@gmail.com>
2016-10-12 13:50:37 +0200

    Started work on int vector abstraction

---
+++ include/tudocomp/GenericIntVector.hpp
@@ -0,0 +1,38 @@
+#ifndef TUDOCOMP_GENERIC_INT_VECTOR_H
+#define TUDOCOMP_GENERIC_INT_VECTOR_H
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <iomanip>
+#include <cstring>
+#include <glog/logging.hpp>
+
+namespace tdc {
+    class Dynamic {};
+    // uint32_t
+    // uint64_t
+    // size_t
+    // uint_T<N>
+
+    template<class T>
+    GenericIntVectorTrait {
+
+    };
+
+    template<class T>
+    class GenericIntVector {
+
+    };
+
+
+}
+
+#endif
