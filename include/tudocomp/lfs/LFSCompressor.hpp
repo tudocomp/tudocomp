b2d453de04958aa6b45b064c4fcc4df8e3b9e238
Ole Bergenholtz <ole.bergenholtz@tu-dortmund.de>
2016-11-03 17:59:32 +0100

    non terminal symbols added

--- include/tudocomp/lfs/LFSCompressor.hpp
+++ include/tudocomp/lfs/LFSCompressor.hpp
@@ -12,6 +12,7 @@
 //#include <iostream>
 #include <sdsl/lcp.hpp>
 #include <sdsl/suffix_arrays.hpp>
+#include <sdsl/bit_vectors.hpp>
 
 //#include <sdsl/suffixtrees.hpp>
 
@@ -117,20 +118,117 @@ public:
         }
         std::cout << std::endl;
         std::cout << "repeating factors:" << std::endl;
+        // Pop PQ, Select occurences of suffix, check if contains replaced symbols
+        sdsl::bit_vector non_terminals(csa.size(), 0);
+        //Pq for the non-terminal symbols
+        //the first in pair is position, the seconds the number of the non terminal symbol
+        std::priority_queue<std::pair<int,int>> non_terminal_symbols;
+        int non_terminal_symbol_number = 1;
         while(!pq.empty()){
 
             std::pair<int,int> top = pq.top();
-            std::cout << "pos1: "<< csa[top.second] << "pos2: "<< csa[top.second-1] << " len: " << top.first << "; ";
+            std::cout << "pos1: "<< csa[top.second] << " pos2: "<< csa[top.second-1] << " len: " << top.first << "; ";
             std::cout << std::endl;
             pq.pop();
 
             std::string substring = text.substr(csa[top.second-1], top.first);
-            std::cout << substring << std::endl;
+            std::cout << "string: " << substring << std::endl;
+
+            //detect all starting positions of this string using the sa and lcp:
+            std::vector<int> starting_positions;
+
+            starting_positions.push_back(csa[top.second]);
+            std::cout <<"starting positions: " << std::endl;
+            int i = top.second;
+            while(i>=0 && lcp[i]>=top.first){
+                starting_positions.push_back(csa[i-1]);
+                //std::cout << csa[i-1] << " ";
+                i--;
+            }
+            i = top.second+1;
+            while(i< lcp.size() && lcp[i]>=top.first){
+                starting_positions.push_back(csa[i]);
+                //std::cout << csa[i];
+                i++;
+            }
+            std::sort(starting_positions.begin(), starting_positions.end());
+            for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
+                std::cout << ' ' << *it;
+            std::cout << '\n';
+            //select occurences greedily:
+            int last =  starting_positions.front();
+            int current;
+            for (std::vector<int>::iterator it=starting_positions.begin()+1; it!=starting_positions.end(); ++it){
+
+                current = *it;
+                if(last+top.first>current){
+                    it = starting_positions.erase(it);
+                }
+                last = current;
+            }
+            std::cout <<"selected starting positions: " << std::endl;
+            for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
+                std::cout << ' ' << *it;
+            std::cout << '\n';
+
+            //now ceck in bitvector viable starting positions
+            // there is no 1 bit on the corresponding positions
+
+            std::vector<int>::iterator it=starting_positions.begin();
+
+            while(it!=starting_positions.end()){
+                bool non_viable = false;
+                std::cout << "check position: " << *it << std::endl;
+                for(int k = 0; k<top.first; k++){
+                    if(non_terminals[*it+k]==1){
+
+                        non_viable=true;
+                        break;
+                    }
+                }
+                if(non_viable){
+                    std::cout << "position not viable: " << *it << std::endl;
+                    //it =
+                    it = starting_positions.erase(it);
+                } else {
+                    it++;
+                }
+                //std::cout << "position viable: " << *it << std::endl;
+            }
+            //if the factor is still repeating, make the corresponding positions unviable
+
+
+            if(starting_positions.size()>=2){
+                std::cout << "These starting positions are still viable." <<  std::endl;
+                std::cout << "non-terminals before:" << std::endl << non_terminals << std::endl;
+
+                for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it){
+
+                    std::cout << "position removed: " << *it << " to " << *it+top.first << std::endl;
+                    for(int k = 0; k<top.first; k++){
+                        //std::cout << "position non viable: " << *it+k << std::endl;
+                        non_terminals[*it+k]=1;
+                    }
+                }
+                std::pair<int,int> symbol(*it, non_terminal_symbol_number);
+                non_terminal_symbols.push(symbol);
+                std::cout << "non-terminals after:" << std::endl << non_terminals << std::endl;
+            }
+
+
+
+
         }
         std::cout << std::endl;
 
+        while(!non_terminal_symbols.empty()){
+            std::pair<int,int> position = non_terminal_symbols.top();
+            std::cout << "at pos: " << position.first << " symbol: " << position.second << std::endl;
+            non_terminal_symbols.pop();
+        }
 
-        // Pop PQ, Select occurences of suffix, check if contains replaced symbols
+
+
         // replace
 
         //encode string
