a0a6db8b1164d5aa511de06ae681f5e35fd0fa20
Ole Bergenholtz <ole.bergenholtz@tu-dortmund.de>
2016-11-09 11:35:35 +0100

    replaced std::cout by glog

--- include/tudocomp/lfs/LFSCompressor.hpp
+++ include/tudocomp/lfs/LFSCompressor.hpp
@@ -32,7 +32,7 @@ public:
     }
 
     /*inline LFSCompressor(Env&& env) : Compressor(std::move(env)) {
-        std::cout << "output"<< std::endl;
+        DLOG(INFO) << "output"<< std::endl;
     }*/
 
     inline LFSCompressor(Env&& env):
@@ -40,7 +40,7 @@ public:
     {
         // ...
 
-        std::cout << "Compressor instantiated"<< std::endl;
+        DLOG(INFO) << "Compressor instantiated"<< std::endl;
         //env.begin_stat_phase("LFS compression");
 
     }
@@ -62,12 +62,12 @@ public:
        // env.end_stat_phase();
 
 
-        t.print(std::cout);
+        t.print(DLOG(INFO));
 
         auto& sa_t = t.require_sa();
         auto& lcp_t = t.require_lcp();
         //t.sa[0];
-       // std::cout << sa_t[0] << std::endl;
+       // DLOG(INFO) << sa_t[0] << std::endl;
 
 
 
@@ -97,15 +97,15 @@ public:
        // sdsl::lcp_wt<> lcp;
        // sdsl::construct(lcp, file, 1);
 
-        //std::cout << "Text:" << std::endl << text << std::endl  << std::endl;
+        //DLOG(INFO) << "Text:" << std::endl << text << std::endl  << std::endl;
 
-        //std::cout << "suffix array:" << std::endl;
-       // std::cout << csa << std::endl;
-        //std::cout << "-------" << std::endl;
-        //std::cout << "lcp array:" << std::endl;
-        //std::cout << lcp << std::endl;
+        //DLOG(INFO) << "suffix array:" << std::endl;
+       // DLOG(INFO) << csa << std::endl;
+        //DLOG(INFO) << "-------" << std::endl;
+        //DLOG(INFO) << "lcp array:" << std::endl;
+        //DLOG(INFO) << lcp << std::endl;
 
-        //std::cout << lcp << std::endl;
+        //DLOG(INFO) << lcp << std::endl;
         // iterate over lcp array, add indexes with non overlapping prefix length grater than 2 to pq
         std::priority_queue<std::pair<int,int>> pq;
 
@@ -113,7 +113,7 @@ public:
         int factor_length;
         for(uint i = 1; i<lcp_t.size(); i++){
 
-            //std::cout <<  lcp[i] << " ";
+            //DLOG(INFO) <<  lcp[i] << " ";
             if(lcp_t[i] >= 2){
                 //compute length of non-overlapping factor:
 
@@ -136,8 +136,8 @@ public:
                 }
             }
         }
-        std::cout << std::endl;
-        std::cout << "repeating factors:" << std::endl;
+        DLOG(INFO) << std::endl;
+        DLOG(INFO) << "repeating factors:" << std::endl;
         // Pop PQ, Select occurences of suffix, check if contains replaced symbols
         sdsl::bit_vector non_terminals(sa_t.size(), 0);
         //Pq for the non-terminal symbols
@@ -147,8 +147,8 @@ public:
         while(!pq.empty()){
 
             std::pair<int,int> top = pq.top();
-            std::cout << "pos1: "<< sa_t[top.second] << " pos2: "<< sa_t[top.second-1] << " len: " << top.first << "; ";
-            std::cout << std::endl;
+            DLOG(INFO) << "pos1: "<< sa_t[top.second] << " pos2: "<< sa_t[top.second-1] << " len: " << top.first << "; ";
+            DLOG(INFO) << std::endl;
             pq.pop();
 
             std::string substring;// = text.substr(sa_t[top.second-1], top.first);
@@ -156,29 +156,29 @@ public:
             for(int k = 0; k<top.first;k++){
                 substring += t[offset+k];
             }
-            std::cout << "string: " << substring << std::endl;
+            DLOG(INFO) << "string: " << substring << std::endl;
 
             //detect all starting positions of this string using the sa and lcp:
             std::vector<int> starting_positions;
 
             starting_positions.push_back(sa_t[top.second]);
-            std::cout <<"starting positions: " << std::endl;
+            DLOG(INFO) <<"starting positions: " << std::endl;
             int i = top.second;
             while(i>=0 && lcp_t[i]>=top.first){
                 starting_positions.push_back(sa_t[i-1]);
-                //std::cout << sa_t[i-1] << " ";
+                //DLOG(INFO) << sa_t[i-1] << " ";
                 i--;
             }
             i = top.second+1;
             while(i< lcp_t.size() && lcp_t[i]>=top.first){
                 starting_positions.push_back(sa_t[i]);
-                //std::cout << csa[i];
+                //DLOG(INFO) << csa[i];
                 i++;
             }
             std::sort(starting_positions.begin(), starting_positions.end());
             for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
-                std::cout << ' ' << *it;
-            std::cout << '\n';
+                DLOG(INFO) << ' ' << *it;
+            DLOG(INFO) << '\n';
             //select occurences greedily:
             int last =  starting_positions.front();
             int current;
@@ -190,10 +190,10 @@ public:
                 }
                 last = current;
             }
-            std::cout <<"selected starting positions: " << std::endl;
+            DLOG(INFO) <<"selected starting positions: " << std::endl;
             for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
-                std::cout << ' ' << *it;
-            std::cout << '\n';
+                DLOG(INFO) << ' ' << *it;
+            DLOG(INFO) << '\n';
 
             //now ceck in bitvector viable starting positions
             // there is no 1 bit on the corresponding positions
@@ -202,7 +202,7 @@ public:
 
             while(it!=starting_positions.end()){
                 bool non_viable = false;
-                std::cout << "check position: " << *it << std::endl;
+                DLOG(INFO) << "check position: " << *it << std::endl;
                 for(int k = 0; k<top.first; k++){
                     if(non_terminals[*it+k]==1){
 
@@ -211,43 +211,43 @@ public:
                     }
                 }
                 if(non_viable){
-                    std::cout << "position not viable: " << *it << std::endl;
+                    DLOG(INFO) << "position not viable: " << *it << std::endl;
                     //it =
                     it = starting_positions.erase(it);
                 } else {
                     it++;
                 }
-                //std::cout << "position viable: " << *it << std::endl;
+                //DLOG(INFO) << "position viable: " << *it << std::endl;
             }
             //if the factor is still repeating, make the corresponding positions unviable
 
 
             if(starting_positions.size()>=2){
-                std::cout << "These starting positions are still viable." <<  std::endl;
-                std::cout << "non-terminals before:" << std::endl << non_terminals << std::endl;
+                DLOG(INFO) << "These starting positions are still viable." <<  std::endl;
+                DLOG(INFO) << "non-terminals before:" << std::endl << non_terminals << std::endl;
 
                 for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it){
 
-                    std::cout << "position removed: " << *it << " to " << *it+top.first << std::endl;
+                    DLOG(INFO) << "position removed: " << *it << " to " << *it+top.first << std::endl;
                     for(int k = 0; k<top.first; k++){
-                        //std::cout << "position non viable: " << *it+k << std::endl;
+                        //DLOG(INFO) << "position non viable: " << *it+k << std::endl;
                         non_terminals[*it+k]=1;
                     }
                 }
                 std::pair<int,int> symbol(*it, non_terminal_symbol_number);
                 non_terminal_symbols.push(symbol);
-                std::cout << "non-terminals after:" << std::endl << non_terminals << std::endl;
+                DLOG(INFO) << "non-terminals after:" << std::endl << non_terminals << std::endl;
             }
 
 
 
 
         }
-        std::cout << std::endl;
+        DLOG(INFO) << std::endl;
 
         while(!non_terminal_symbols.empty()){
             std::pair<int,int> position = non_terminal_symbols.top();
-            std::cout << "at pos: " << position.first << " symbol: " << position.second << std::endl;
+            DLOG(INFO) << "at pos: " << position.first << " symbol: " << position.second << std::endl;
             non_terminal_symbols.pop();
         }
 
