d38bbb4e0cd89f3857ecbbba04c81649d771dc79
Ole Bergenholtz <ole.bergenholtz@tu-dortmund.de>
2016-11-15 17:45:02 +0100

    implemented decoding

--- include/tudocomp/lfs/LFSCompressor.hpp
+++ include/tudocomp/lfs/LFSCompressor.hpp
@@ -49,7 +49,7 @@ public:
        // env().log_stat("A statistic", 147);
         //build lcp array and suffix array:
         //auto istream = input.as_stream();
-       // auto ostream = output.as_stream();
+        auto ostream = output.as_stream();
 
 
 
@@ -62,7 +62,7 @@ public:
        // env.end_stat_phase();
 
 
-        t.print(DLOG(INFO));
+       // t.print(DLOG(INFO));
 
         auto& sa_t = t.require_sa();
         auto& lcp_t = t.require_lcp();
@@ -78,8 +78,6 @@ public:
        //     text += cur_char;
        // }
 
-        //text = "abaaabbababb$";
-        //input.as_stream;
 
         //just necessary for construct()
         //std::ofstream myfile;
@@ -109,6 +107,8 @@ public:
         // iterate over lcp array, add indexes with non overlapping prefix length grater than 2 to pq
         std::priority_queue<std::pair<int,int>> pq;
 
+        std::list<std::string> dictionary;
+
         int dif ;
         int factor_length;
         for(uint i = 1; i<lcp_t.size(); i++){
@@ -136,8 +136,8 @@ public:
                 }
             }
         }
-        DLOG(INFO) << std::endl;
-        DLOG(INFO) << "repeating factors:" << std::endl;
+       // DLOG(INFO) << std::endl;
+       // DLOG(INFO) << "repeating factors:" << std::endl;
         // Pop PQ, Select occurences of suffix, check if contains replaced symbols
         sdsl::bit_vector non_terminals(sa_t.size(), 0);
         //Pq for the non-terminal symbols
@@ -147,8 +147,8 @@ public:
         while(!pq.empty()){
 
             std::pair<int,int> top = pq.top();
-            DLOG(INFO) << "pos1: "<< sa_t[top.second] << " pos2: "<< sa_t[top.second-1] << " len: " << top.first << "; ";
-            DLOG(INFO) << std::endl;
+          //  DLOG(INFO) << "pos1: "<< sa_t[top.second] << " pos2: "<< sa_t[top.second-1] << " len: " << top.first << "; ";
+          //  DLOG(INFO) << std::endl;
             pq.pop();
 
             std::string substring;// = text.substr(sa_t[top.second-1], top.first);
@@ -156,13 +156,13 @@ public:
             for(int k = 0; k<top.first;k++){
                 substring += t[offset+k];
             }
-            DLOG(INFO) << "string: " << substring << std::endl;
+          //  DLOG(INFO) << "string: " << substring << std::endl;
 
             //detect all starting positions of this string using the sa and lcp:
             std::vector<int> starting_positions;
 
             starting_positions.push_back(sa_t[top.second]);
-            DLOG(INFO) <<"starting positions: " << std::endl;
+           // DLOG(INFO) <<"starting positions: " << std::endl;
             int i = top.second;
             while(i>=0 && lcp_t[i]>=top.first){
                 starting_positions.push_back(sa_t[i-1]);
@@ -176,9 +176,9 @@ public:
                 i++;
             }
             std::sort(starting_positions.begin(), starting_positions.end());
-            for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
-                DLOG(INFO) << ' ' << *it;
-            DLOG(INFO) << '\n';
+          //  for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
+          //      DLOG(INFO) << ' ' << *it;
+          //  DLOG(INFO) << '\n';
             //select occurences greedily:
             int last =  starting_positions.front();
             int current;
@@ -190,10 +190,10 @@ public:
                 }
                 last = current;
             }
-            DLOG(INFO) <<"selected starting positions: " << std::endl;
-            for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
-                DLOG(INFO) << ' ' << *it;
-            DLOG(INFO) << '\n';
+            //DLOG(INFO) <<"selected starting positions: " << std::endl;
+           // for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it)
+          //      DLOG(INFO) << ' ' << *it;
+           // DLOG(INFO) << '\n';
 
             //now ceck in bitvector viable starting positions
             // there is no 1 bit on the corresponding positions
@@ -202,7 +202,7 @@ public:
 
             while(it!=starting_positions.end()){
                 bool non_viable = false;
-                DLOG(INFO) << "check position: " << *it << std::endl;
+                //DLOG(INFO) << "check position: " << *it << std::endl;
                 for(int k = 0; k<top.first; k++){
                     if(non_terminals[*it+k]==1){
 
@@ -211,7 +211,7 @@ public:
                     }
                 }
                 if(non_viable){
-                    DLOG(INFO) << "position not viable: " << *it << std::endl;
+                   // DLOG(INFO) << "position not viable: " << *it << std::endl;
                     //it =
                     it = starting_positions.erase(it);
                 } else {
@@ -223,45 +223,158 @@ public:
 
 
             if(starting_positions.size()>=2){
-                DLOG(INFO) << "These starting positions are still viable." <<  std::endl;
-                DLOG(INFO) << "non-terminals before:" << std::endl << non_terminals << std::endl;
+                //DLOG(INFO) << "These starting positions are still viable." <<  std::endl;
+                //DLOG(INFO) << "non-terminals before:" << std::endl << non_terminals << std::endl;
 
                 for (std::vector<int>::iterator it=starting_positions.begin(); it!=starting_positions.end(); ++it){
 
-                    DLOG(INFO) << "position removed: " << *it << " to " << *it+top.first << std::endl;
+                   // DLOG(INFO) << "position removed: " << *it << " to " << *it+top.first << std::endl;
                     for(int k = 0; k<top.first; k++){
                         //DLOG(INFO) << "position non viable: " << *it+k << std::endl;
                         non_terminals[*it+k]=1;
                     }
                     int length_of_symbol = top.first;
                     std::tuple<int,int,int> symbol(*it, non_terminal_symbol_number, length_of_symbol);
-                    DLOG(INFO) << "added symbol: " << non_terminal_symbol_number << " at pos " << *it << std::endl;
+                    DLOG(INFO) << "added symbol: " << non_terminal_symbol_number << " at pos " << *it;
                     non_terminal_symbols.push(symbol);
                 }
+                dictionary.push_back(substring);
+                DLOG(INFO) << "added rule:";
+                DLOG(INFO) << (char) (64+non_terminal_symbol_number) << " -> " << substring;
                 non_terminal_symbol_number++;
-                DLOG(INFO) << "non-terminals after:" << std::endl << non_terminals << std::endl;
+                //DLOG(INFO) << "non-terminals after:" << std::endl << non_terminals << std::endl;
+
             }
 
 
 
 
         }
-        DLOG(INFO) << std::endl;
+        //DLOG(INFO) << std::endl;
 
+        //while(!non_terminal_symbols.empty()){
+         //   std::tuple<int,int,int> position = non_terminal_symbols.top();
+          //  DLOG(INFO) << "at pos: " << std::get<0>(position) << " symbol: " << std::get<1>(position)  << " length: "<< std::get<2>(position)  <<  std::endl;
+           // non_terminal_symbols.pop();
+       // }
+
+
+
+        std::string output_string ="";
+
+        // replace
+        int pos = 0;
+        int start_position;
+        int symbol_number ;
+        int symbol_length;
         while(!non_terminal_symbols.empty()){
-            std::tuple<int,int,int> position = non_terminal_symbols.top();
-            DLOG(INFO) << "at pos: " << std::get<0>(position) << " symbol: " << std::get<1>(position)  << " length: "<< std::get<2>(position)  <<  std::endl;
+            std::tuple<int,int,int> next_position = non_terminal_symbols.top();
+            start_position = std::get<0>(next_position);
+            symbol_number =std::get<1>(next_position);
+            symbol_length = std::get<2>(next_position);
             non_terminal_symbols.pop();
+
+            while(pos< start_position){
+                //get original text, because no symbol...
+                output_string+=t[pos];
+                pos++;
+            }
+
+            //write symbol number
+             output_string += '\\';
+            output_string += (char) (64+symbol_number);
+            pos+= symbol_length;
+
+        }
+        //start_position=0;
+        //if no more terminals, write rest of text
+        while( pos<t.size()){
+            output_string+=t[pos++];
+        }
+
+        DLOG(INFO) << "output string: ";
+        DLOG(INFO) << output_string ;
+        ostream << output_string;
+        ostream << "\\$";
+
+        DLOG(INFO) << "dictionary: ";
+
+
+        auto it = dictionary.begin();
+
+        int rule =0;
+        while(it != dictionary.end()){
+
+            DLOG(INFO) <<  (char) (65+rule++)<< " -> " << *it;
+            ostream << *it;
+            ostream << "\\$";
+            it++;
         }
 
 
-
-        // replace
 
         //encode string
     }
 
     inline virtual void decompress(Input& input, Output& output) override {
+        std::vector<std::pair<int,int>> symbol_positions;// = new std::vector<std::pair<int,int>>;
+        //auto istream = input.as_stream();
+        auto in = input.as_stream();
+        std::string text;
+        char cur_char;
+        bool escape=false;
+        bool dict = false;
+        char escape_symbol = '\\';
+        char dollar_symbol = '$';
+        std::string symbol;
+        std::vector<std::string> symbol_list;// = new std::vector<std::string>;
+        while(in.get(cur_char)){
+            if(dict){
+                if(cur_char != escape_symbol ){
+                    symbol += cur_char;
+                } else {
+                    //symbol ends...
+
+                    symbol_list.push_back(symbol);
+                     DLOG(INFO) << "symobl: " << symbol << " symbol number: " << symbol_list.size();
+                    symbol="";
+                    in.get(cur_char);
+                }
+            } else
+            if(!escape){
+                escape=false;
+                if(cur_char != escape_symbol ){
+                    text += cur_char;
+                } else {
+                    escape = true;
+                }
+            } else {
+                escape=false;
+                if(cur_char == dollar_symbol){
+                    DLOG(INFO) << text;
+                    dict=true;
+                } else {
+                    std::pair<int,int> pair (text.length(), cur_char-64);
+                    symbol_positions.push_back(pair);
+                    DLOG(INFO) << "non terminal symbol at: " << text.length() << " symbol: " << cur_char-64;
+                }
+
+            }
+
+        }
+        int added_symbols=0;
+        auto it = symbol_positions.begin();
+        while(it!= symbol_positions.end()){
+            std::pair<int,int> position = *it;
+            std::string symbol = symbol_list[position.second-1];
+            text = text.substr(0, position.first + added_symbols) + symbol + text.substr(position.first + added_symbols , text.size());
+            DLOG(INFO) << text;
+            it++;
+
+        }
+
+        auto ostream = output.as_stream();
+        ostream << text;
     }
 
 };
--- test/lfs_tests.cpp
+++ test/lfs_tests.cpp
@@ -18,6 +18,8 @@
 using tdc::LFSCompressor;
 
 TEST(lfs, test) {
+
+    std::string comp_result;
     auto compressor = tdc::create_algo<LFSCompressor>();
 
 //std::cout << "output"<< std::endl;
@@ -26,17 +28,70 @@ TEST(lfs, test) {
     // create the input for the test (a string constant)
     tdc::Input input("abaaabbababb$");
 
+    DLOG(INFO) << "input:";
+    DLOG(INFO) << "abaaabbababb$";
     // create the output for the test (a buffer)
-    std::vector<uint8_t> buffer;
-    tdc::Output output(buffer);
+    //std::vector<uint8_t> buffer;
+   // tdc::Output output(buffer);
+
+
+    std::stringstream stm;
+    Output output = Output::from_stream(stm);
+
+
+
+
 
     // invoke the compress method
     compressor.compress(input, output);
 
-    tdc::Input input2("mississippi$");
-    compressor.compress(input2, output);
-        // retrieve the output as a string
-    std::string output_str(buffer.begin(), buffer.end());
+    comp_result = stm.str();
+
+    DLOG(INFO) << "encoded:";
+    DLOG(INFO) << comp_result;
+    tdc::Input input_decompress(comp_result);
+
+    compressor.decompress(input_decompress, output);
+
+    // compare the expected result against the output string to determine test failure or success
+    //ASSERT_EQ("abc%6%de", output_str);
+    ASSERT_TRUE(true);
+}
+
+TEST(lfs, test2) {
+
+    std::string comp_result;
+    auto compressor = tdc::create_algo<LFSCompressor>();
+
+//std::cout << "output"<< std::endl;
+  //  compressor = tdc::create_algo<LFSCompressor>();
+    //comp.compress("String", null);
+    // create the input for the test (a string constant)
+    tdc::Input input("mississippi$");
+    DLOG(INFO) << "input:";
+    DLOG(INFO) << "mississippi$";
+    // create the output for the test (a buffer)
+    //std::vector<uint8_t> buffer;
+   // tdc::Output output(buffer);
+
+
+    std::stringstream stm;
+    Output output = Output::from_stream(stm);
+
+
+
+
+
+    // invoke the compress method
+    compressor.compress(input, output);
+
+    comp_result = stm.str();
+    DLOG(INFO) << "encoded:";
+    DLOG(INFO) << comp_result << std::endl;
+
+    tdc::Input input_decompress(comp_result);
+
+    compressor.decompress(input_decompress, output);
 
     // compare the expected result against the output string to determine test failure or success
     //ASSERT_EQ("abc%6%de", output_str);
