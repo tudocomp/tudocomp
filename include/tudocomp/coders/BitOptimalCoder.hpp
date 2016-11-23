072d57291493e8e6d8cdca3636fbe9b27ceca415
Patrick Dinklage <pdinklag@gmail.com>
2016-11-23 13:19:34 +0100

    Use vbyte coding for lengths in bit optimal coding

--- include/tudocomp/coders/BitOptimalCoder.hpp
+++ include/tudocomp/coders/BitOptimalCoder.hpp
@@ -27,6 +27,11 @@ public:
                 m_out->write_int(v - r.min(), bits_for(r.max() - r.min()));
             }
         }
+
+        template<typename value_t>
+        inline void encode(value_t v, const LengthRange&) {
+            m_out->write_compressed_int(v);
+        }
     };
 
     class Decoder : public tdc::Decoder {
@@ -41,6 +46,11 @@ public:
                 return (value_t)r.min() +
                     m_in->read_int<value_t>(bits_for(r.max() - r.min()));
             }
+        }
+
+        template<typename value_t>
+        inline value_t decode(const LengthRange&) {
+            return m_in->read_compressed_int<value_t>();
         }
     };
 };
