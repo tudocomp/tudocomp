f3a54f3d0b2065a3539a95c9354063442b34bd93
Marvin Löbel <loebel.marvin@gmail.com>
2016-11-24 17:21:07 +0100

    Add generic view types

---
+++ include/tudocomp/pre_header/GenericViewBase.hpp
@@ -0,0 +1,261 @@
+#ifndef TUDOCOMP_GENERIC_VIEW_BASE_H
+#define TUDOCOMP_GENERIC_VIEW_BASE_H
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <iomanip>
+#include <cstring>
+#include <glog/logging.h>
+
+namespace tdc {
+
+/// \cond INTERNAL
+
+template<class T, class P>
+class GenericViewBase {
+    // TODO: Hack, fix sometime
+    //protected:
+public:
+
+    friend class GenericViewBase<T, const T*>;
+
+    using value_type             = T;
+    using const_reference        = const T&;
+    using const_pointer          = const T*;
+    using const_iterator         = const_pointer;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    using difference_type        = ptrdiff_t;
+    using size_type              = size_t;
+
+    static const size_type npos = -1;
+
+    P      m_data;
+    size_t m_size;
+
+    inline void bound_check(size_t pos) const {
+        if (pos >= m_size) {
+            std::stringstream ss;
+            ss << "accessing view with bounds [0, ";
+            ss << m_size;
+            ss << ") at out-of-bounds index ";
+            ss << pos;
+            throw std::out_of_range(ss.str());
+        }
+    }
+
+    inline void debug_bound_check(size_t pos) const {
+#ifdef DEBUG
+        bound_check(pos);
+#endif
+    }
+
+    inline GenericViewBase(P data, size_t size): m_data(data), m_size(size) {}
+    inline GenericViewBase(): GenericViewBase(P(""), 0) {} // Need a valid static pointer...
+    inline GenericViewBase(const GenericViewBase& other):
+        GenericViewBase(other.m_data, other.m_size) {}
+    inline GenericViewBase(const std::vector<T>& other):
+        GenericViewBase(other.data(), other.size()) {}
+    template<size_t N>
+    inline GenericViewBase(const std::array<T, N>& other):
+        GenericViewBase(other.data(), other.size()) {}
+
+    inline operator std::vector<T>() const {
+        return std::vector<T>(cbegin(), cend());
+    }
+
+    inline GenericViewBase& operator=(const GenericViewBase& other) {
+        m_data = other.m_data;
+        m_size = other.m_size;
+        return *this;
+    }
+
+    inline const_iterator begin() const {
+        return m_data;
+    }
+
+    inline const_iterator end() const {
+        return m_data + m_size;
+    }
+
+    inline const_reverse_iterator rbegin() const {
+        return std::reverse_iterator<const_iterator>(end());
+    }
+
+    inline const_reverse_iterator rend() const {
+        return std::reverse_iterator<const_iterator>(begin());
+    }
+
+    inline const_iterator cbegin() const {
+        return m_data;
+    }
+
+    inline const_iterator cend() const {
+        return m_data + m_size;
+    }
+
+    inline const_reverse_iterator crbegin() const {
+        return std::reverse_iterator<const_iterator>(cend());
+    }
+
+    inline const_reverse_iterator crend() const {
+        return std::reverse_iterator<const_iterator>(cbegin());
+    }
+
+    inline size_type size() const {
+        return m_size;
+    }
+
+    inline size_type max_size() const {
+        return size();
+    }
+
+    inline bool empty() const {
+        return size() == 0;
+    }
+
+    inline const_reference operator[](size_type n) const {
+        debug_bound_check(n);
+        return m_data[n];
+    }
+
+    inline const_reference at(size_type n) const {
+        bound_check(n);
+        return m_data[n];
+    }
+
+    inline const_reference front() const {
+        debug_bound_check(0);
+        return m_data[0];
+    }
+
+    inline const_reference back() const {
+        debug_bound_check(size() - 1);
+        return m_data[size() - 1];
+    }
+
+    inline const value_type* data() const noexcept {
+        return m_data;
+    }
+
+    inline void pop_back() {
+        m_size--;
+    }
+
+    inline void pop_front() {
+        m_data++;
+        m_size--;
+    }
+
+    inline void swap(GenericViewBase& other) {
+        using std::swap;
+        swap(m_data, other.m_data);
+        swap(m_size, other.m_size);
+    }
+
+    inline void clear() {
+        m_size = 0;
+    }
+
+    inline GenericViewBase substr(size_type from, size_type to = npos) const {
+        if (to == npos) {
+            to = m_size;
+        }
+
+        DCHECK(from <= to);
+        DCHECK(from <= size());
+        DCHECK(to <= size());
+
+        return GenericViewBase(m_data + from, to - from);
+    }
+
+    inline void remove_prefix(size_type n) {
+        *this = substr(n);
+    }
+
+    inline void remove_suffix(size_type n) {
+        *this = substr(0, m_size - n);
+    }
+
+    inline bool starts_with(const T& c) const {
+        return !empty() && (front() == c);
+    }
+
+    inline bool starts_with(const GenericViewBase<T, const T*>& x) const {
+        GenericViewBase<T, const T*> y(m_data, m_size);
+        return (x.size() <= y.size()) && (y.substr(0, x.size()) == x);
+    }
+
+    inline bool ends_with(const T& c) const {
+        return !empty() && (back() == c);
+    }
+
+    inline bool ends_with(const GenericViewBase<T, const T*>& x) const {
+        GenericViewBase<T, const T*> y(m_data, m_size);
+        return (x.size() <= y.size()) && (y.substr(size() - x.size()) == x);
+    }
+
+    template<class U, class Q>
+    friend bool operator==(const GenericViewBase<U, Q>& lhs, const GenericViewBase<U, Q>& rhs);
+    template<class U, class Q>
+    friend bool operator!=(const GenericViewBase<U, Q>& lhs, const GenericViewBase<U, Q>& rhs);
+    template<class U, class Q>
+    friend bool operator<(const GenericViewBase<U, Q>& lhs, const GenericViewBase<U, Q>& rhs);
+    template<class U, class Q>
+    friend bool operator<=(const GenericViewBase<U, Q>& lhs, const GenericViewBase<U, Q>& rhs);
+    template<class U, class Q>
+    friend bool operator>(const GenericViewBase<U, Q>& lhs, const GenericViewBase<U, Q>& rhs);
+    template<class U, class Q>
+    friend bool operator>=(const GenericViewBase<U, Q>& lhs, const GenericViewBase<U, Q>& rhs);
+    template<class U, class Q>
+    friend void swap(GenericViewBase<U, Q>& lhs, GenericViewBase<U, Q>& rhs);
+};
+
+template<class T, class P>
+bool operator==(const GenericViewBase<T, P>& lhs, const GenericViewBase<T, P>& rhs) {
+    if (lhs.size() != rhs.size()) return false;
+    return std::equal(lhs.cbegin(), lhs.cend(), rhs.cbegin());
+}
+
+template<class T, class P>
+bool operator!=(const GenericViewBase<T, P>& lhs, const GenericViewBase<T, P>& rhs) {
+    return !(lhs == rhs);
+}
+
+template<class T, class P>
+bool operator<(const GenericViewBase<T, P>& lhs, const GenericViewBase<T, P>& rhs) {
+    return std::lexicographical_compare(lhs.cbegin(), lhs.cend(), rhs.cbegin(), rhs.cend());
+}
+
+template<class T, class P>
+bool operator<=(const GenericViewBase<T, P>& lhs, const GenericViewBase<T, P>& rhs) {
+    return !(lhs > rhs);
+}
+
+template<class T, class P>
+bool operator>(const GenericViewBase<T, P>& lhs, const GenericViewBase<T, P>& rhs) {
+    return std::lexicographical_compare(rhs.cbegin(), rhs.cend(), lhs.cbegin(), lhs.cend());
+}
+
+template<class T, class P>
+bool operator>=(const GenericViewBase<T, P>& lhs, const GenericViewBase<T, P>& rhs) {
+    return !(lhs < rhs);
+}
+
+template<class T, class P>
+void swap(GenericViewBase<T, P>& lhs, GenericViewBase<T, P>& rhs) {
+    lhs.swap(rhs);
+}
+
+/// \endcond
+
+}
+
+#endif
---
+++ include/tudocomp/util/GenericView.hpp
@@ -0,0 +1,499 @@
+#ifndef TUDOCOMP_GENERIC_VIEW_H
+#define TUDOCOMP_GENERIC_VIEW_H
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <iomanip>
+#include <cstring>
+#include <glog/logging.h>
+
+#include <tudocomp/pre_header/GenericViewBase.hpp>
+
+namespace tdc {
+
+template<class T>
+class ConstGenericView;
+
+/// A view into a slice of memory.
+///
+/// This is an abstraction around a pointer to `T` and a size,
+/// and represents N elements of memory starting at that pointer.
+///
+/// Creating/Copying/Modifying a View will not copy any of the data it points at.
+/// Its API mostly mirrors that of a `std::vector<T>`.
+template<class T>
+class GenericView: GenericViewBase<T, T*> {
+    using Super = GenericViewBase<T, T*>;
+    inline GenericView(const Super& other): Super::GenericViewBase(other) {}
+public:
+    using value_type             = typename Super::value_type;
+    using reference              = T&;
+    using const_reference        = typename Super::const_reference;
+    using pointer                = T*;
+    using const_pointer          = typename Super::const_pointer;
+    using iterator               = pointer;
+    using const_iterator         = typename Super::const_iterator;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = typename Super::const_reverse_iterator;
+    using difference_type        = typename Super::difference_type;
+    using size_type              = typename Super::size_type;
+
+    static const size_type npos  = Super::npos;
+
+    /// Construct a empty View
+    inline GenericView(): Super::GenericViewBase() {}
+
+    /// Construct a View pointing at `len` elements starting from `data`
+    inline GenericView(T* data, size_t len): Super::GenericViewBase(data, len) {}
+
+    /// Construct a View as a copy of `other`
+    inline GenericView(const GenericView& other):
+        Super::GenericViewBase(other) {}
+
+    /// Construct a View pointing at the contents of a vector
+    inline GenericView(std::vector<T>& other):
+        Super::GenericViewBase(other.data(), other.size()) {}
+
+    /// Construct a vector with the contents of this View
+    using Super::operator std::vector<T>;
+
+    using Super::begin;
+    inline iterator begin() {
+        return this->m_data;
+    }
+
+    using Super::end;
+    inline iterator end() {
+        return this->m_data + this->m_size;
+    }
+
+    using Super::rbegin;
+    inline reverse_iterator rbegin() {
+        return std::reverse_iterator<iterator>(end());
+    }
+
+    using Super::rend;
+    inline reverse_iterator rend() {
+        return std::reverse_iterator<iterator>(begin());
+    }
+
+    using Super::cbegin;
+    using Super::cend;
+    using Super::crbegin;
+    using Super::crend;
+
+    using Super::size;
+    using Super::max_size;
+    using Super::empty;
+
+    using Super::operator[];
+    inline reference operator[](size_type n) {
+        Super::debug_bound_check(n);
+        return this->m_data[n];
+    }
+
+    using Super::at;
+    inline reference at(size_type n) {
+        Super::bound_check(n);
+        return this->m_data[n];
+    }
+
+    using Super::front;
+    inline reference front() {
+        Super::debug_bound_check(0);
+        return this->m_data[0];
+    }
+
+    using Super::back;
+    inline reference back() {
+        Super::debug_bound_check(size() - 1);
+        return this->m_data[size() - 1];
+    }
+
+    using Super::data;
+    inline value_type* data() noexcept {
+        return this->m_data;
+    }
+
+    using Super::pop_back;
+    using Super::pop_front;
+
+    inline void swap(GenericView& other) {
+        Super::swap(other);
+    }
+
+    using Super::clear;
+
+    inline GenericView substr(size_type from, size_type to = npos) const {
+        return Super::substr(from, to);
+    }
+
+    using Super::remove_prefix;
+    using Super::remove_suffix;
+    inline bool starts_with(const T& other) const {
+        return Super::starts_with(other);
+    }
+    inline bool starts_with(const ConstGenericView<T> other) const {
+        return Super::starts_with(other);
+    }
+    inline bool ends_with(const T& other) const {
+        return Super::ends_with(other);
+    }
+    inline bool ends_with(const ConstGenericView<T> other) const {
+        return Super::ends_with(other);
+    }
+
+    template<class U>
+    friend bool operator==(const GenericView<U>& lhs, const GenericView<U>& rhs);
+    template<class U>
+    friend bool operator!=(const GenericView<U>& lhs, const GenericView<U>& rhs);
+    template<class U>
+    friend bool operator<(const GenericView<U>& lhs, const GenericView<U>& rhs);
+    template<class U>
+    friend bool operator<=(const GenericView<U>& lhs, const GenericView<U>& rhs);
+    template<class U>
+    friend bool operator>(const GenericView<U>& lhs, const GenericView<U>& rhs);
+    template<class U>
+    friend bool operator>=(const GenericView<U>& lhs, const GenericView<U>& rhs);
+    template<class U>
+    friend void swap(GenericView<U>& lhs, GenericView<U>& rhs);
+};
+
+template<class T>
+bool operator==(const GenericView<T>& lhs, const GenericView<T>& rhs) {
+    using Super = typename GenericView<T>::Super;
+    return (const Super&) lhs == (const Super&) rhs;
+}
+
+template<class T>
+bool operator!=(const GenericView<T>& lhs, const GenericView<T>& rhs) {
+    using Super = typename GenericView<T>::Super;
+    return (const Super&) lhs != (const Super&) rhs;
+}
+
+template<class T>
+bool operator<(const GenericView<T>& lhs, const GenericView<T>& rhs) {
+    using Super = typename GenericView<T>::Super;
+    return (const Super&) lhs < (const Super&) rhs;
+}
+
+template<class T>
+bool operator<=(const GenericView<T>& lhs, const GenericView<T>& rhs) {
+    using Super = typename GenericView<T>::Super;
+    return (const Super&) lhs <= (const Super&) rhs;
+}
+
+template<class T>
+bool operator>(const GenericView<T>& lhs, const GenericView<T>& rhs) {
+    using Super = typename GenericView<T>::Super;
+    return (const Super&) lhs > (const Super&) rhs;
+}
+
+template<class T>
+bool operator>=(const GenericView<T>& lhs, const GenericView<T>& rhs) {
+    using Super = typename GenericView<T>::Super;
+    return (const Super&) lhs >= (const Super&) rhs;
+}
+
+template<class T>
+void swap(GenericView<T>& lhs, GenericView<T>& rhs) {
+    using Super = typename GenericView<T>::Super;
+    return swap((Super&) lhs, (Super&) rhs);
+}
+
+/// A const view into a slice of memory.
+///
+/// This is an abstraction around a pointer to `T` and a size,
+/// and represents N elements of memory starting at that pointer.
+///
+/// Creating/Copying/Modifying a View will not copy any of the data it points at.
+/// Its API mostly mirrors that of a `std::vector<T>`.
+template<class T>
+class ConstGenericView: GenericViewBase<T, const T*> {
+    using Super = GenericViewBase<T, const T*>;
+    inline ConstGenericView(const Super& other): Super::GenericViewBase(other) {}
+    friend class GenericView<T>;
+public:
+    using value_type             = typename Super::value_type;
+    using const_reference        = typename Super::const_reference;
+    using const_pointer          = typename Super::const_pointer;
+    using const_iterator         = typename Super::const_iterator;
+    using const_reverse_iterator = typename Super::const_reverse_iterator;
+    using difference_type        = typename Super::difference_type;
+    using size_type              = typename Super::size_type;
+
+    static const size_type npos  = Super::npos;
+
+    /// Construct a empty View
+    inline ConstGenericView(): Super::GenericViewBase() {}
+
+    /// Construct a View pointing at `len` elements starting from `data`
+    inline ConstGenericView(const T* data, size_t len): Super::GenericViewBase(data, len) {}
+
+    /// Construct a View as a copy of `other`
+    inline ConstGenericView(const ConstGenericView& other):
+        Super::GenericViewBase(other) {}
+
+    /// Construct a View as a copy of `other`
+    inline ConstGenericView(const GenericView<T>& other):
+        ConstGenericView(other.data(), other.size()) {}
+
+    /// Construct a View pointing at the contents of a vector
+    inline ConstGenericView(const std::vector<T>& other):
+        Super::GenericViewBase(other.data(), other.size()) {}
+
+    /// Construct a vector with the contents of this View
+    using Super::operator std::vector<T>;
+
+    using Super::begin;
+    using Super::end;
+    using Super::rbegin;
+    using Super::rend;
+    using Super::cbegin;
+    using Super::cend;
+    using Super::crbegin;
+    using Super::crend;
+
+    using Super::size;
+    using Super::max_size;
+    using Super::empty;
+    using Super::operator[];
+    using Super::at;
+    using Super::front;
+    using Super::back;
+    using Super::data;
+    using Super::pop_back;
+    using Super::pop_front;
+
+    inline void swap(ConstGenericView& other) {
+        Super::swap(other);
+    }
+
+    using Super::clear;
+
+    inline ConstGenericView substr(size_type from, size_type to = npos) const {
+        return ConstGenericView(Super::substr(from, to));
+    }
+
+    using Super::remove_prefix;
+    using Super::remove_suffix;
+    inline bool starts_with(const T& other) const {
+        return Super::starts_with(other);
+    }
+    inline bool starts_with(const ConstGenericView<T> other) const {
+        return Super::starts_with(other);
+    }
+    inline bool ends_with(const T& other) const {
+        return Super::ends_with(other);
+    }
+    inline bool ends_with(const ConstGenericView<T> other) const {
+        return Super::ends_with(other);
+    }
+
+    template<class U>
+    friend bool operator==(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator!=(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator<(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator<=(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator>(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator>=(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend void swap(ConstGenericView<U>& lhs, ConstGenericView<U>& rhs);
+};
+
+template<class T>
+bool operator==(const ConstGenericView<T>& lhs, const ConstGenericView<T>& rhs) {
+    using Super = typename ConstGenericView<T>::Super;
+    return (const Super&) lhs == (const Super&) rhs;
+}
+
+template<class T>
+bool operator!=(const ConstGenericView<T>& lhs, const ConstGenericView<T>& rhs) {
+    using Super = typename ConstGenericView<T>::Super;
+    return (const Super&) lhs != (const Super&) rhs;
+}
+
+template<class T>
+bool operator<(const ConstGenericView<T>& lhs, const ConstGenericView<T>& rhs) {
+    using Super = typename ConstGenericView<T>::Super;
+    return (const Super&) lhs < (const Super&) rhs;
+}
+
+template<class T>
+bool operator<=(const ConstGenericView<T>& lhs, const ConstGenericView<T>& rhs) {
+    using Super = typename ConstGenericView<T>::Super;
+    return (const Super&) lhs <= (const Super&) rhs;
+}
+
+template<class T>
+bool operator>(const ConstGenericView<T>& lhs, const ConstGenericView<T>& rhs) {
+    using Super = typename ConstGenericView<T>::Super;
+    return (const Super&) lhs > (const Super&) rhs;
+}
+
+template<class T>
+bool operator>=(const ConstGenericView<T>& lhs, const ConstGenericView<T>& rhs) {
+    using Super = typename ConstGenericView<T>::Super;
+    return (const Super&) lhs >= (const Super&) rhs;
+}
+
+template<class T>
+void swap(ConstGenericView<T>& lhs, ConstGenericView<T>& rhs) {
+    using Super = typename ConstGenericView<T>::Super;
+    return swap((Super&) lhs, (Super&) rhs);
+}
+
+using ByteView = ConstGenericView<uint8_t>;
+
+/// A const view into a slice of memory.
+///
+/// This is an abstraction around a pointer to `T` and a size,
+/// and represents N elements of memory starting at that pointer.
+///
+/// Creating/Copying/Modifying a View will not copy any of the data it points at.
+/// Its API mostly mirrors that of a `std::vector<T>`.
+template<>
+class ConstGenericView<uint8_t>: GenericViewBase<uint8_t, const uint8_t*> {
+    using Super = GenericViewBase<uint8_t, const uint8_t*>;
+    inline ConstGenericView(const Super& other): Super::GenericViewBase(other) {}
+    friend class GenericView<uint8_t>;
+public:
+    using value_type             = typename Super::value_type;
+    using const_reference        = typename Super::const_reference;
+    using const_pointer          = typename Super::const_pointer;
+    using const_iterator         = typename Super::const_iterator;
+    using const_reverse_iterator = typename Super::const_reverse_iterator;
+    using difference_type        = typename Super::difference_type;
+    using size_type              = typename Super::size_type;
+
+    static const size_type npos  = Super::npos;
+
+    /// Construct a empty View
+    inline ConstGenericView(): Super::GenericViewBase() {}
+
+    /// Construct a View pointing at `len` elements starting from `data`
+    inline ConstGenericView(const uint8_t* data, size_t len): Super::GenericViewBase(data, len) {}
+
+    /// Construct a View as a copy of `other`
+    inline ConstGenericView(const ConstGenericView& other):
+        Super::GenericViewBase(other) {}
+
+    /// Construct a View as a copy of `other`
+    inline ConstGenericView(const GenericView<uint8_t>& other):
+        ConstGenericView(other.data(), other.size()) {}
+
+    /// Construct a View pointing at the contents of a vector
+    inline ConstGenericView(const std::vector<uint8_t>& other):
+        Super::GenericViewBase(other.data(), other.size()) {}
+
+    /// Construct a vector with the contents of this View
+    using Super::operator std::vector<uint8_t>;
+
+    using Super::begin;
+    using Super::end;
+    using Super::rbegin;
+    using Super::rend;
+    using Super::cbegin;
+    using Super::cend;
+    using Super::crbegin;
+    using Super::crend;
+
+    using Super::size;
+    using Super::max_size;
+    using Super::empty;
+    using Super::operator[];
+    using Super::at;
+    using Super::front;
+    using Super::back;
+    using Super::data;
+    using Super::pop_back;
+    using Super::pop_front;
+
+    inline void swap(ConstGenericView& other) {
+        Super::swap(other);
+    }
+
+    using Super::clear;
+
+    inline ConstGenericView substr(size_type from, size_type to = npos) const {
+        return ConstGenericView(Super::substr(from, to));
+    }
+
+    using Super::remove_prefix;
+    using Super::remove_suffix;
+    inline bool starts_with(const uint8_t& other) const {
+        return Super::starts_with(other);
+    }
+    inline bool starts_with(const ConstGenericView<uint8_t> other) const {
+        return Super::starts_with(other);
+    }
+    inline bool ends_with(const uint8_t& other) const {
+        return Super::ends_with(other);
+    }
+    inline bool ends_with(const ConstGenericView<uint8_t> other) const {
+        return Super::ends_with(other);
+    }
+
+    template<class U>
+    friend bool operator==(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator!=(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator<(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator<=(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator>(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend bool operator>=(const ConstGenericView<U>& lhs, const ConstGenericView<U>& rhs);
+    template<class U>
+    friend void swap(ConstGenericView<U>& lhs, ConstGenericView<U>& rhs);
+
+    // string extensions
+
+    /// Construct a View pointing at the contents of a string
+    inline ConstGenericView(const std::string& other):
+        ConstGenericView((const uint8_t*) other.data(), other.size()) {}
+
+    /// Construct a View pointing at a C-style null terminated string.
+    ///
+    /// Can be used to construct from a string literal
+    inline ConstGenericView(const char* other):
+        ConstGenericView((const uint8_t*) other, strlen(other)) {}
+
+    /// Construct a View pointing at `len` elements starting from `data`
+    inline ConstGenericView(const char* data, size_t len):
+        ConstGenericView((const uint8_t*) data, len) {}
+
+    /// Construct a string with the contents of this View
+    inline operator std::string() const {
+        return std::string(cbegin(), cend());
+    }
+};
+
+inline std::ostream& operator<<(std::ostream& os, const ByteView& v) {
+    os.write((const char*) v.data(), v.size());
+    return os;
+}
+
+inline ByteView operator "" _bv(const char* str, size_t n)
+{
+    return ByteView(str, n);
+}
+
+}
+
+#endif
