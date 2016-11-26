dafcbbec0bae7bb822f86be9387d10153dfc669a
Patrick Dinklage <pdinklag@gmail.com>
2016-11-26 15:02:04 +0100

    define extra range for factor lengths in LZSS coding

--- include/tudocomp/compressors/lzss/LZSSCoding.hpp
+++ include/tudocomp/compressors/lzss/LZSSCoding.hpp
@@ -19,16 +19,19 @@ inline void encode_text(coder_t& coder,
     assert(factors.is_sorted());
 
     auto n = text.size();
+    auto flen_max = factors.longest_factor();
 
     if(discard_null_terminator && text[n-1] == 0) {
         --n; // discard null terminator
     }
 
-    // encode text size
-    coder.encode(n, len_r);
-
     // define ranges
     Range text_r(n);
+    Range flen_r(flen_max);
+
+    // encode text size and longest factor
+    coder.encode(n, len_r);
+    coder.encode(flen_max, text_r);
 
     size_t p = 0;
     for(size_t i = 0; i < factors.size(); i++) {
@@ -45,7 +48,7 @@ inline void encode_text(coder_t& coder,
         // encode factor
         coder.encode(true, bit_r);
         coder.encode(f.src, text_r);
-        coder.encode(f.len, text_r);
+        coder.encode(f.len, flen_r);
 
         p += f.len;
     }
@@ -68,6 +71,10 @@ inline void decode_text_internal(coder_t& decoder, std::ostream& outs) {
     auto text_len = decoder.template decode<len_t>(len_r);
     Range text_r(text_len);
 
+    // decode longest factor
+    auto flen_max = decoder.template decode<len_t>(text_r);
+    Range flen_r(flen_max);
+
     // init decode buffer
     DecodeBuffer<dcb_strategy_t> buffer(text_len);
 
@@ -76,7 +83,7 @@ inline void decode_text_internal(coder_t& decoder, std::ostream& outs) {
         bool is_factor = decoder.template decode<bool>(bit_r);
         if(is_factor) {
             auto src = decoder.template decode<len_t>(text_r);
-            auto len = decoder.template decode<len_t>(text_r);
+            auto len = decoder.template decode<len_t>(flen_r);
 
             buffer.defact(src, len);
         } else {
