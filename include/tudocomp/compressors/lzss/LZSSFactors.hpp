f023b691883fa273d0c565fb55ce08053928f7ca
Patrick Dinklage <pdinklag@gmail.com>
2016-11-27 04:11:25 +0100

    Optimize LZSS coding for k-mer coding possibilities

--- include/tudocomp/compressors/lzss/LZSSFactors.hpp
+++ include/tudocomp/compressors/lzss/LZSSFactors.hpp
@@ -20,14 +20,22 @@ class FactorBuffer {
 private:
     std::vector<Factor> m_factors;
     bool m_sorted;
+
+    len_t m_shortest_factor;
     len_t m_longest_factor;
 
 public:
-    inline FactorBuffer() : m_sorted(true), m_longest_factor(0) {}
+    inline FactorBuffer() : m_sorted(true),
+                            m_shortest_factor(SIZE_MAX),
+                            m_longest_factor(0)
+    {
+    }
 
     inline void push_back(Factor f) {
         m_sorted = m_sorted && (m_factors.empty() || f.pos >= m_factors.back().pos);
         m_factors.push_back(f);
+
+        m_shortest_factor = std::min(m_shortest_factor, f.len);
         m_longest_factor = std::max(m_longest_factor, f.len);
     }
 
@@ -37,10 +45,6 @@ public:
 
     inline bool empty() const {
         return m_factors.empty();
-    }
-
-    inline size_t longest_factor() const {
-        return m_longest_factor;
     }
 
     inline size_t size() const {
@@ -58,6 +62,14 @@ public:
 
             m_sorted = true;
         }
+    }
+
+    inline size_t shortest_factor() const {
+        return m_shortest_factor;
+    }
+
+    inline size_t longest_factor() const {
+        return m_longest_factor;
     }
 };
 
