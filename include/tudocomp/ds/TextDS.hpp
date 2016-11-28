8d9b4d47811f6f25efc43e6d8b43fe0761f03c0b
Marvin Löbel <loebel.marvin@gmail.com>
2016-11-28 16:50:27 +0100

    fix tab -> space

--- include/tudocomp/ds/TextDS.hpp
+++ include/tudocomp/ds/TextDS.hpp
@@ -19,30 +19,30 @@ using io::InputView;
 
 /// Manages text related data structures.
 template<
-	class lcp_tt = LCPArray,
+    class lcp_tt = LCPArray,
     template<typename> class isa_tt = InverseSuffixArray>
 class TextDS {
 
 public:
-	typedef uint8_t value_type;
+    typedef uint8_t value_type;
     static const uint64_t SA = 0x01;
     static const uint64_t ISA = 0x02;
     static const uint64_t Phi = 0x04;
     static const uint64_t LCP = 0x08;
-	typedef TextDS<lcp_tt,isa_tt> this_t;
-	typedef typename lcp_tt::template construct_t<this_t> lcp_t;
-	typedef isa_tt<this_t> isa_t;
-	typedef SuffixArray<this_t> sa_t;
-	typedef PhiArray<this_t> phi_t;
+    typedef TextDS<lcp_tt,isa_tt> this_t;
+    typedef typename lcp_tt::template construct_t<this_t> lcp_t;
+    typedef isa_tt<this_t> isa_t;
+    typedef SuffixArray<this_t> sa_t;
+    typedef PhiArray<this_t> phi_t;
 
 private:
     size_t m_size;
     const value_type* m_text;
 
-	std::unique_ptr<sa_t>  m_sa;
-	std::unique_ptr<isa_t> m_isa;
-	std::unique_ptr<phi_t> m_phi;
-	std::unique_ptr<lcp_t> m_lcp;
+    std::unique_ptr<sa_t>  m_sa;
+    std::unique_ptr<isa_t> m_isa;
+    std::unique_ptr<phi_t> m_phi;
+    std::unique_ptr<lcp_t> m_lcp;
 
     class Literals {
     protected:
@@ -68,14 +68,14 @@ public:
     inline TextDS(const InputView& input)
         : m_size(input.size()), m_text((const value_type*)input.data())
     {
-		if(!input.is_terminal_null_ensured())
-		 	throw std::logic_error("Terminal Null is not ensured. Please invoke the method ensure_null_terminator() on your input before creating TextDS");
+        if(!input.is_terminal_null_ensured())
+             throw std::logic_error("Terminal Null is not ensured. Please invoke the method ensure_null_terminator() on your input before creating TextDS");
     }
 
     inline TextDS(const InputView& input, uint64_t flags) : TextDS(input)
     {
-		if(!input.is_terminal_null_ensured())
-		 	throw std::logic_error("Terminal Null is not ensured. Please invoke the method ensure_null_terminator() on your input before creating TextDS");
+        if(!input.is_terminal_null_ensured())
+             throw std::logic_error("Terminal Null is not ensured. Please invoke the method ensure_null_terminator() on your input before creating TextDS");
         require(flags);
     }
 
@@ -161,84 +161,84 @@ public:
 namespace tdc {
 
 template<
-	class lcp_tt,
+    class lcp_tt,
     template<typename> class isa_tt>
 inline void TextDS<lcp_tt,isa_tt>::print(std::ostream& out, size_t base) {
-	size_t w = std::max(6UL, (size_t)std::log10((double)m_size) + 1);
-	out << std::setfill(' ');
+    size_t w = std::max(6UL, (size_t)std::log10((double)m_size) + 1);
+    out << std::setfill(' ');
 
-	//Heading
-	out << std::setw(w) << "i" << " | ";
-	if(m_sa) out << std::setw(w) << "SA[i]" << " | ";
-	if(m_isa) out << std::setw(w) << "ISA[i]" << " | ";
-	if(m_phi) out << std::setw(w) << "Phi[i]" << " | ";
-	if(m_lcp) out << std::setw(w) << "LCP[i]" << " | ";
-	out << std::endl;
+    //Heading
+    out << std::setw(w) << "i" << " | ";
+    if(m_sa) out << std::setw(w) << "SA[i]" << " | ";
+    if(m_isa) out << std::setw(w) << "ISA[i]" << " | ";
+    if(m_phi) out << std::setw(w) << "Phi[i]" << " | ";
+    if(m_lcp) out << std::setw(w) << "LCP[i]" << " | ";
+    out << std::endl;
 
-	//Separator
-	out << std::setfill('-');
-	out << std::setw(w) << "" << "-|-";
-	if(m_sa) out << std::setw(w) << "" << "-|-";
-	if(m_isa) out << std::setw(w) << "" << "-|-";
-	if(m_phi) out << std::setw(w) << "" << "-|-";
-	if(m_lcp) out << std::setw(w) << "" << "-|-";
-	out << std::endl;
+    //Separator
+    out << std::setfill('-');
+    out << std::setw(w) << "" << "-|-";
+    if(m_sa) out << std::setw(w) << "" << "-|-";
+    if(m_isa) out << std::setw(w) << "" << "-|-";
+    if(m_phi) out << std::setw(w) << "" << "-|-";
+    if(m_lcp) out << std::setw(w) << "" << "-|-";
+    out << std::endl;
 
-	//Body
-	out << std::setfill(' ');
-	for(size_t i = 0; i < m_size + 1; i++) {
-		out << std::setw(w) << (i + base) << " | ";
-		if(m_sa) out << std::setw(w) << ((*m_sa)[i] + base) << " | ";
-		if(m_isa) out << std::setw(w) << ((*m_isa)[i] + base) << " | ";
-		if(m_phi) out << std::setw(w) << ((*m_phi)[i] + base) << " | ";
-		if(m_lcp) out << std::setw(w) << (*m_lcp)[i] << " | ";
-		out << std::endl;
-	}
+    //Body
+    out << std::setfill(' ');
+    for(size_t i = 0; i < m_size + 1; i++) {
+        out << std::setw(w) << (i + base) << " | ";
+        if(m_sa) out << std::setw(w) << ((*m_sa)[i] + base) << " | ";
+        if(m_isa) out << std::setw(w) << ((*m_isa)[i] + base) << " | ";
+        if(m_phi) out << std::setw(w) << ((*m_phi)[i] + base) << " | ";
+        if(m_lcp) out << std::setw(w) << (*m_lcp)[i] << " | ";
+        out << std::endl;
+    }
 }
 
 template<
-	class lcp_tt,
+    class lcp_tt,
     template<typename> class isa_tt>
 const SuffixArray<TextDS<lcp_tt,isa_tt>>& TextDS<lcp_tt,isa_tt>::require_sa() {
-	if(!m_sa) {
-		m_sa = std::unique_ptr<sa_t>(new sa_t());
-		m_sa->construct(*this);
-	}
-	return *m_sa;
+    if(!m_sa) {
+        m_sa = std::unique_ptr<sa_t>(new sa_t());
+        m_sa->construct(*this);
+    }
+    return *m_sa;
 }
 
 template<
-	class lcp_tt,
+    class lcp_tt,
     template<typename> class isa_tt>
 const PhiArray<TextDS<lcp_tt,isa_tt>>& TextDS<lcp_tt,isa_tt>::require_phi() {
-	if(!m_phi) {
-		m_phi = std::unique_ptr<phi_t>(new phi_t());
-		m_phi->construct(*this);
-	}
+    if(!m_phi) {
+        m_phi = std::unique_ptr<phi_t>(new phi_t());
+        m_phi->construct(*this);
+    }
 
-	return *m_phi;
+    return *m_phi;
 }
 
 template<
-	class lcp_tt,
+    class lcp_tt,
     template<typename> class isa_tt>
 const isa_tt<TextDS<lcp_tt,isa_tt>>& TextDS<lcp_tt,isa_tt>::require_isa() {
-	if(!m_isa) {
-		m_isa = std::unique_ptr<isa_t>(new isa_t());
-		m_isa->construct(*this);
-	}
+    if(!m_isa) {
+        m_isa = std::unique_ptr<isa_t>(new isa_t());
+        m_isa->construct(*this);
+    }
 
-	return *m_isa;
+    return *m_isa;
 }
 
 template<class lcp_tt,
     template<typename> class isa_tt>
 const typename TextDS<lcp_tt,isa_tt>::lcp_t& TextDS<lcp_tt,isa_tt>::require_lcp() {
-	if(!m_lcp) {
-		m_lcp = std::unique_ptr<lcp_t>(new lcp_t());
-		m_lcp->construct(*this);
-	}
-	return *m_lcp;
+    if(!m_lcp) {
+        m_lcp = std::unique_ptr<lcp_t>(new lcp_t());
+        m_lcp->construct(*this);
+    }
+    return *m_lcp;
 }
 
 //typedef TextDS<lcp_sada<TextDS<lcp_sada,InverseSuffixArray>, InverseSuffixArray<TextDS<lcp_sada,InverseSuffixArray>> TextDSI;
