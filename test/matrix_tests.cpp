7b5be773f4cb708b322b5cdc987913ade0fbe454
Marvin Löbel <loebel.marvin@gmail.com>
2016-12-07 00:34:16 +0100

    Add environment variable controls for matrix tests

--- test/matrix_tests.cpp
+++ test/matrix_tests.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include <glog/logging.h>
+#include <cstdlib>
 
 #include "test_util.hpp"
 #include <tudocomp_driver/Registry.hpp>
@@ -11,17 +12,47 @@
 using namespace tdc_algorithms;
 
 // TODO
-const std::vector<std::string> excluded_tests {
+const std::vector<std::string> EXCLUDED_TESTS {
     "chain",
 };
-const std::vector<std::string> additional_tests {
+const std::vector<std::string> ADDITIONAL_TESTS {
     "chain(chain(chain(chain(easyrle(\"1\"),bwt()),mtf()),easyrle()),encode(huff))",
     "chain(lz78, lzw)",
     "chain(lz78, chain(noop, lzw))",
 };
 
+std::vector<std::string> parse_scsv(const std::string& s) {
+    std::vector<std::string> r;
+    std::string remaining = s;
+
+    while(!remaining.empty()) {
+        auto next = remaining.find(";");
+        r.push_back(remaining.substr(0, next));
+        if (next == std::string::npos) {
+            remaining = "";
+        } else {
+            remaining = remaining.substr(next + 1);
+        }
+    }
+
+    return r;
+}
+
 TEST(TudocompDriver, roundtrip_matrix) {
     std::cout << "[ Generating list of test cases ]\n";
+
+    std::vector<std::string> excluded_tests = EXCLUDED_TESTS;
+    std::vector<std::string> additional_tests = ADDITIONAL_TESTS;
+    std::vector<std::string> pattern_tests;
+    bool has_pattern_tests = false;
+
+    auto env_exclude_p = std::getenv("MATRIX_EXCLUDE");
+    auto env_additional_p = std::getenv("MATRIX_ADDITIONAL");
+    auto env_pattern_p = std::getenv("MATRIX_PATTERN");
+
+    std::string env_exclude = (env_exclude_p != nullptr) ? env_exclude_p : "";
+    std::string env_additional = (env_additional_p != nullptr) ? env_additional_p : "";
+    std::string env_pattern = (env_pattern_p != nullptr) ? env_pattern_p : "";
 
     // Use cross product of all static arguments as base list to check
     std::vector<std::string> test_cases_pre_filter;
@@ -30,14 +61,31 @@ TEST(TudocompDriver, roundtrip_matrix) {
         test_cases_pre_filter.push_back(x.to_string(true));
     }
 
+    for (auto x : parse_scsv(env_exclude)) {
+        excluded_tests.push_back(x);
+    }
+    for (auto x : parse_scsv(env_pattern)) {
+        pattern_tests.push_back(x);
+        has_pattern_tests = true;
+    }
+    for (auto x : parse_scsv(env_additional)) {
+        additional_tests.push_back(x);
+    }
+
+    auto test_cases_contains = [](const std::string test_case,
+                                  const std::vector<std::string>& vs) {
+        return std::any_of(vs.begin(),
+                           vs.end(),
+                           [&](const std::string& s) {
+                            return test_case.find(s) != std::string::npos;
+                           });
+    };
+
     std::vector<std::string> test_cases;
     for (const auto& test_case: test_cases_pre_filter) {
-        if (!std::any_of(excluded_tests.begin(),
-                         excluded_tests.end(),
-                        [&](const std::string& excluded) {
-                            return test_case.find(excluded) == 0;
-                        }))
-        {
+        if (!test_cases_contains(test_case, excluded_tests)
+            && (!has_pattern_tests || test_cases_contains(test_case, pattern_tests))
+        ) {
             test_cases.push_back(test_case);
         }
     }
