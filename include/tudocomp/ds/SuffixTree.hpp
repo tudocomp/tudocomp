d521c78f19a5e842050df13482e1fc6c39e2e691
Ole Bergenholtz <ole.bergenholtz@tu-dortmund.de>
2016-12-08 18:05:54 +0100

    removed debug loggin from stree

--- include/tudocomp/ds/SuffixTree.hpp
+++ include/tudocomp/ds/SuffixTree.hpp
@@ -46,10 +46,6 @@ private:
     void add_sl(STNode* node){
 
         if(last_added_sl != root) {
-
-            DLOG(INFO)<< "sl from: " <<last_added_sl->start<<" "<< last_added_sl->end;
-
-            DLOG(INFO) << "sl to: " << node->start<<" " <<node->end;
             last_added_sl->suffix_link=node;
         }
         last_added_sl=node;
@@ -104,28 +100,16 @@ public:
         last_added_sl=root;
 
         while(remainder > 0){
-
-            //DLOG(INFO)<<"remainder: " << remainder;
-            //DLOG(INFO)<<"active length: " << active_length;
             if(active_length==0){
-
-                 DLOG(INFO)<<"switching active edge " << c;
                 active_edge = c;
             }
 
 
 
-            DLOG(INFO)<<"active edge: " << active_edge;
-
-           // DLOG(INFO)<<"remainder: " << remainder;
-           // DLOG(INFO)<<"active length: " << active_length;
-           // DLOG(INFO)<<"active edge: " << active_edge;
-
             //if the active node doesnt have the corresponding edge:
             auto next_it = active_node->child_nodes.find(active_edge);
             if(next_it==active_node->child_nodes.end()){
                 //insert new leaf
-                DLOG(INFO)<<"inserting new leaf rule 1: " << c;
 
                 STNode* new_leaf = new STNode(pos-1);
                 active_node->child_nodes[active_edge] = new_leaf;
@@ -134,41 +118,26 @@ public:
                 add_sl(active_node);
 
             } else {
-                //DLOG(INFO)<<"already in tree: " << c;
                 STNode* next = active_node->child_nodes[active_edge];
                 //if the active length is greater than the edge length:
                 //switch active node to that
-
-
-
-                DLOG(INFO)<<"active edge: " << active_edge;
-                DLOG(INFO)<<"active edge length: " << edge_length(next);
                 if(active_length>= edge_length(next)){
-                    DLOG(INFO)<<"switching active node down";
                     active_node = next;
                     active_length -= edge_length(next);
                     active_edge = Text[pos-active_length];
-
-                    DLOG(INFO)<<"active edge: " << active_edge;
                     continue;
                 }
 
                 //if that suffix is already in the tree::
                 if(Text[next->start +active_length] == c){
-                    DLOG(INFO)<<"char on active edge: " << c;
                     active_length++;
                     add_sl(active_node);
-
-                    DLOG(INFO)<<"active length: " << active_length;
                     break;
                 }
 
                 //now split edge if the edge is found
 
-                DLOG(INFO)<<"inserting split: " << c;
                 STNode* split = new STNode(next->start, next->start+active_length-1);
-                DLOG(INFO)<<"split s: " << next->start << " e: "<< next->start+active_length-1;
-                DLOG(INFO)<<"length of split: " << edge_length(split);
                 active_node->child_nodes[active_edge] = split;
                 STNode* leaf = new STNode(pos-1);
                 leaf->suffix=pos-1;
@@ -183,17 +152,12 @@ public:
             }
             remainder--;
             if(active_node==root && active_length>0){
-                DLOG(INFO)<<"setting new active edge: " << Text[pos-remainder];
                 active_length--;
                 active_edge = Text[pos-remainder];
             }else {
                 if(active_node->suffix_link != NULL){
-
-                    DLOG(INFO)<<"setting active node to sl";
                     active_node = active_node->suffix_link;
                 } else {
-
-                    DLOG(INFO)<<"setting active node root";
                     active_node = root;
                 }
 
@@ -203,11 +167,7 @@ public:
     }
     inline void add_string(std::string input){
         for(uint i = 0; i<input.length();i++){
-            DLOG(INFO)<<"-------------------------";
-            DLOG(INFO)<<"adding " << input[i];
             add_char(input[i]);
-
-            DLOG(INFO)<<"-------------------------";
         }
     }
 
