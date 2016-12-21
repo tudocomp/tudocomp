eeea10103cc1836506e224d54652a0605204276f
Marvin Löbel <loebel.marvin@gmail.com>
2016-12-21 14:40:35 +0100

    Reuse implementation of lz78 cics

---
+++ include/tudocomp/ds/st_util.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <sstream>
+
+#include "st.hpp"
+
+//read file fully
+inline std::string file_get_contents(const std::string& filename) {
+    std::ifstream stream(filename);
+    return std::string(
+            (std::istreambuf_iterator<char>(stream)),
+            std::istreambuf_iterator<char>());
+}
+
+//get suffix tree for text
+inline tdc::STLz78u suffix_tree(const std::string& text, tdc::STLz78u::cst_t& cst) {
+	construct_im(cst, text, 1);
+    return tdc::STLz78u(cst);
+}
