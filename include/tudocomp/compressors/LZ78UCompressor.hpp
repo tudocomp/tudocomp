1a7ad69109383aa5bf19fa40d479382a97a3712d
Marvin Löbel <loebel.marvin@gmail.com>
2016-12-21 11:38:21 +0100

    Second revision of paper

--- include/tudocomp/compressors/LZ78UCompressor.hpp
+++ include/tudocomp/compressors/LZ78UCompressor.hpp
@@ -3,6 +3,7 @@
 #include <tudocomp/Compressor.hpp>
 
 #include <sdsl/cst_fully.hpp>
+#include <sdsl/cst_sada.hpp>
 
 #include <tudocomp/Range.hpp>
 #include <tudocomp/ds/TextDS.hpp>
@@ -11,7 +12,7 @@ namespace tdc {
 
 class LZ78UCompressor: public Compressor {
 private:
-    using cst_t = sdsl::cst_sct3<>;
+    using cst_t = sdsl::cst_sada<>;
     using node_type = cst_t::node_type;
 
     static inline lz78::factorid_t select_size(Env& env, string_ref name) {
@@ -42,67 +43,86 @@ public:
     virtual void compress(Input& input, Output& out) override {
         env().begin_stat_phase("lz78u");
 
-        auto base_view = input.as_view();
+        auto T = input.as_view();
 
-        TextDS<> ds { base_view, env(), TextDS<>::ISA };
+        TextDS<> ds { T, env(), TextDS<>::ISA };
         auto isa_p = ds.release_isa();
         auto& ISA = *isa_p;
 
-        auto T = base_view.substr(0, base_view.size() - 1);
-
-        cst_t st;
+        cst_t ST;
         {
             env().begin_stat_phase("construct suffix tree");
 
             // TODO: Specialize sdsl template for less alloc here
-            std::string bad_copy_1 = T;
+            std::string bad_copy_1 = T.substr(0, T.size() - 1);
             std::cout << vec_to_debug_string(bad_copy_1) << "\n";
 
-            construct_im(st, bad_copy_1, 1);
+            construct_im(ST, bad_copy_1, 1);
 
             env().end_stat_phase();
         }
 
         std::vector<size_t> R;
-        R.reserve(st.nodes());
-        R.resize(st.nodes());
+        R.reserve(ST.nodes());
+        R.resize(ST.nodes());
 
         size_t pos = 1;
-        size_t parent_pos = 0;
         size_t z = 0;
-
-        auto node = st.root();
 
         // tx: a a a b a b a a a b a a b a b a $
         // sn:         5         10        15  16
 
+        auto lambda = [&ST, &pos, &T](const node_type& l1, const node_type& l2) -> View {
+            //auto size1 = ST.depth(l1);
+            //auto size2 = ST.depth(l2);
+            auto offset1 = T.size() - ST.depth(l1);
+            auto offset2 = T.size() - ST.depth(l2);
+
+            std::cout << "pos: " << pos << " lambda: " << offset1 << " vs " << offset2 << "\n";
+
+            //auto offset1 = ST.sn(l1);
+            //auto offset2 = ST.sn(l2);
+
+            if (offset1 < offset2) {
+                return T.substr(offset1, offset2);
+            } else {
+                throw std::runtime_error("asdf");
+            }
+        };
+
+        auto parent = [&ST](const node_type& n) {
+            return ST.parent(n);
+        };
+
+        auto leaf_select = [&ST](const size_t& pos) {
+            return ST.select_leaf(pos);
+        };
+
+        auto level_anc = [&ST](const node_type& n, size_t d) {
+            return ST.sl(n, d);
+        };
+
         while (pos <= T.size()) {
-            node = st.child(node, T[pos]);
+            auto l = leaf_select(ISA[pos]);
 
-            bool is_leaf = st.is_leaf(node);
-            if (is_leaf) {
-                std::cout << "pos: " << pos << ", sn: " << st.sn(node) << "\n";
-            }
-
-            pos++;
-
-            /*
-
-            size_t node_idx = node;
-
-            if (is_leaf) {
-                std::cout << "out l char: " << vec_to_debug_string(T.substr(pos, pos + 1)) << "\n";
-                std::cout << "out l idx:  " << 0 << "\n";
-                node = st.root();
-            } else if(R[node_idx] == 0) {
+            if (R[parent(l)] != 0) {
+                std::cout << "out l c: " << int(lambda(parent(l), l)[0]) << "\n";
+                std::cout << "out l r: " << int(0) << "\n";
+                pos++;
+            } else {
+                size_t d = 1;
+                while (level_anc(l, d) != 0) {
+                    d++;
+                    pos += lambda(level_anc(l, d - 1), level_anc(l, d)).size();
+                }
+                auto node = level_anc(l, d);
                 z++;
-                R[node_idx] = z;
-                std::cout << "out m char: " << vec_to_debug_string(T.substr(pos, pos + 1)) << "\n";
-                std::cout << "out m idx:  " << 0 << "\n";
+                R[node] = z;
 
+                std::cout << "out m s: " << vec_to_debug_string(lambda(parent(node), node)) << "\n";
+                std::cout << "out m r: " << int(R[parent(node)]) << "\n";
+                pos += lambda(parent(node), node).size();
             }
-
-            pos += is_leaf ? 1 :*/
         }
 
         env().end_stat_phase();
