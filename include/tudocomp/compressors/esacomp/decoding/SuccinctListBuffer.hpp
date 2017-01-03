8170ebe0607338ac27c86997883e41a1509f6030
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2017-01-03 11:20:33 +0100

    tuned SuccinctListBuffer

--- include/tudocomp/compressors/esacomp/decoding/SuccinctListBuffer.hpp
+++ include/tudocomp/compressors/esacomp/decoding/SuccinctListBuffer.hpp
@@ -22,30 +22,24 @@ public:
     }
 
 private:
-    std::vector<uliteral_t> m_buffer;
-    //typedef IntVector<dynamic_t> vector_t;
-//	typedef std::vector<len_t> vector_t;
     len_t** m_fwd;
-//    std::vector<std::vector<len_t>> m_fwd;
-    sdsl::bit_vector m_decoded;
 
     len_t m_cursor;
-    len_t m_longest_chain;
-    len_t m_current_chain;
+    tdc_stats(len_t m_longest_chain);
+    tdc_stats(len_t m_current_chain);
 
-    const len_t m_size;
+    IntVector<uliteral_t> m_buffer;
 
     inline void decode_literal_at(len_t pos, uliteral_t c) {
-        ++m_current_chain;
-        m_longest_chain = std::max(m_longest_chain, m_current_chain);
+		tdc_stats(++m_current_chain);
+        tdc_stats(m_longest_chain = std::max(m_longest_chain, m_current_chain));
 
         m_buffer[pos] = c;
-        m_decoded[pos] = 1;
+		DCHECK(c != 0 || pos == m_buffer.size()-1); // we assume that the text to restore does not contain a NULL-byte but at its very end
 
         if(m_fwd[pos] != nullptr) {
             const len_t*const& bucket = m_fwd[pos];
             for(size_t i = 1; i < bucket[0]; ++i) {
-                //len_t i = (dynamic_t)fwd;
                 if(bucket[i] == undef_len) break;
                 decode_literal_at(bucket[i], c); // recursion
             }
@@ -53,25 +47,24 @@ private:
             m_fwd[pos] = nullptr;
         }
 
-        --m_current_chain;
+        tdc_stats(--m_current_chain);
     }
 
 public:
     SuccinctListBuffer(SuccinctListBuffer&& other):
         Algorithm(std::move(*this)),
-        m_buffer(std::move(other.m_buffer)),
         m_fwd(std::move(other.m_fwd)),
         m_cursor(std::move(other.m_cursor)),
         m_longest_chain(std::move(other.m_longest_chain)),
         m_current_chain(std::move(other.m_current_chain)),
-        m_size(std::move(other.m_size))
+        m_buffer(std::move(other.m_buffer))
     {
         other.m_fwd = nullptr;
     }
 
     ~SuccinctListBuffer() {
         if(m_fwd != nullptr) {
-            for(size_t i = 0; i < m_size; ++i) {
+            for(size_t i = 0; i < m_buffer.size(); ++i) {
                 if(m_fwd[i] == nullptr) continue;
                 delete [] m_fwd[i];
             }
@@ -79,13 +72,10 @@ public:
         }
     }
     inline SuccinctListBuffer(Env&& env, len_t size)
-        : Algorithm(std::move(env)), m_cursor(0), m_longest_chain(0), m_current_chain(0), m_size(size) {
+        : Algorithm(std::move(env)), m_cursor(0), m_longest_chain(0), m_current_chain(0), m_buffer(size,0) {
 
-        m_buffer.resize(size, 0);
         m_fwd = new len_t*[size];
         std::fill(m_fwd,m_fwd+size,nullptr);
-//        m_fwd.resize(size, std::vector<len_t>());
-        m_decoded = sdsl::bit_vector(size, 0);
     }
 
     inline void decode_literal(uliteral_t c) {
@@ -96,30 +86,21 @@ public:
     inline void decode_factor(len_t pos, len_t num) {
         for(len_t i = 0; i < num; i++) {
             len_t src = pos+i;
-            if(m_decoded[src]) {
+            if(m_buffer[src]) {
                 decode_literal_at(m_cursor, m_buffer[src]);
             } else {
                 len_t*& bucket = m_fwd[src];
                 if(bucket == nullptr) {
-                    m_fwd[src] = new len_t[2];
+                    bucket = new len_t[2];
                     DCHECK(m_fwd[src] == bucket);
-                    std::fill(bucket,bucket+2, undef_len);
                     bucket[0] = 2;
-                    //m_fwd[src] = new vector_t(0,0, bits_for(m_size));
+					bucket[1] = m_cursor;
                 }
-                { // m_fwd[src]->push_back(m_cursor);
-                    len_t i = 1;
-                    while(i < bucket[0]) {
-                        if(bucket[i] == undef_len) break;
-                        ++i;
-                    }
-                    if(i == bucket[0]) {
-                        DCHECK(m_fwd[src] == bucket);
-                        bucket[0] = ++m_fwd[src][0]; //(bucket[0]*16)/10+1;
-                        bucket = (len_t*) realloc(bucket, sizeof(len_t)*bucket[0]);
-                        DCHECK(m_fwd[src] == bucket);
-                    }
-                    bucket[i] = m_cursor;
+				else
+                { // this block implements the call of m_fwd[src]->push_back(m_cursor);
+					++bucket[0]; // increase the size of a bucket only by one!
+					bucket = (len_t*) realloc(bucket, sizeof(len_t)*bucket[0]);
+                    bucket[bucket[0]-1] = m_cursor;
                 }
             }
 
@@ -131,7 +112,7 @@ public:
         return m_longest_chain;
     }
 
-    inline void write_to(std::ostream& out) {
+    inline void write_to(std::ostream& out) const {
         for(auto c : m_buffer) out << c;
     }
 };
