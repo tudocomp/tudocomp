4be28cac3ee1d84a6375ae46de1539f3db9823da
Marvin Löbel <marvin.loebel@tu-dortmund.de>
2017-01-23 14:17:27 +0100

    Add version generation script

---
+++ etc/genversion.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "usage: $0 [version.hpp]"
+
+WORKINGSETCHANGES=$(git status --porcelain | grep -Pv "^\?\?")
+
+DATE=$(date "+%Y.%m")
+COMMITNO=$(git rev-list --count HEAD)
+COMMITREV=$(git show HEAD | head -n 1 | cut -c8-)
+
+VERSION=`if [ -z "$WORKINGSETCHANGES" ]; then
+    printf "0.%s.%s (%s)" $DATE $COMMITNO $COMMITREV
+else
+    printf "0.%s.%s-modified (uncommited changes based on %s)" $DATE $COMMITNO $COMMITREV
+fi`
+
+cat << EOF > $1
+#pragma once
+#include <string>
+
+namespace tdc {
+    const std::string VERSION = "$VERSION";
+}
+EOF
