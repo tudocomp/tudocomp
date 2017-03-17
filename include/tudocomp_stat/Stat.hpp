00b30c443131076a99a9316b6f5d1df7719c917f
Patrick Dinklage <patrick.dinklage@uni-dortmund.de>
2017-03-17 13:57:27 +0100

    Custom memory management in Stat class

--- include/tudocomp_stat/Stat.hpp
+++ include/tudocomp_stat/Stat.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <cstring>
 #include <ctime>
-#include <list>
 
 #include <tudocomp_stat/Json.hpp>
 
@@ -21,8 +21,26 @@ private:
     class Data {
         friend class Stat;
 
+    public:
+        static constexpr size_t STR_BUFFER_SIZE = 64;
+
     private:
-        std::string title;
+        struct keyval {
+            keyval* next;
+            char key[STR_BUFFER_SIZE];
+            char val[STR_BUFFER_SIZE];
+
+            inline keyval() : next(nullptr) {
+            }
+
+            ~keyval() {
+                if(next) delete next;
+            }
+        };
+
+    public: //TODO private
+
+        char title[STR_BUFFER_SIZE];
 
         unsigned long time_start;
         unsigned long time_end;
@@ -30,16 +48,45 @@ private:
         ssize_t mem_current;
         ssize_t mem_peak;
 
-        json::Array stats;
+        keyval* first_stat;
 
-        std::list<Data> children;
+        Data* first_child;
+        Data* next_sibling;
+
+        inline Data()
+            : first_stat(nullptr),
+              first_child(nullptr),
+              next_sibling(nullptr) {
+        }
+
+        ~Data() {
+            if(first_stat) delete first_stat;
+            if(first_child) delete first_child;
+            if(next_sibling) delete next_sibling;
+        }
 
         template<typename T>
-        inline void log_stat(const std::string& key, const T& value) {
-            json::Object pair;
-            pair.set("key", key);
-            pair.set("value", value);
-            stats.add(pair);
+        inline void log_stat(const char* key, const T& value) {
+            keyval* kv = new keyval();
+
+            {
+                json::TValue<T> t(value);
+                std::stringstream ss;
+                t.str(ss);
+
+                strncpy(kv->key, key, STR_BUFFER_SIZE);
+                strncpy(kv->val, ss.str().c_str(), STR_BUFFER_SIZE);
+            }
+
+            if(first_stat) {
+                keyval* last = first_stat;
+                while(last->next) {
+                    last = last->next;
+                }
+                last->next = kv;
+            } else {
+                first_stat = kv;
+            }
         }
 
     public:
@@ -51,12 +98,26 @@ private:
             obj.set("memOff",    mem_off);
             obj.set("memPeak",   mem_peak);
             obj.set("memFinal",  mem_current);
-            obj.set("stats",     stats);
+
+            json::Array stats;
+            keyval* kv = first_stat;
+            while(kv) {
+                json::Object pair;
+                pair.set("key", std::string(kv->key));
+                pair.set("value", std::string(kv->val));
+                stats.add(pair);
+                kv = kv->next;
+            }
+            obj.set("stats", stats);
 
             json::Array sub;
-            for(auto it = children.begin(); it != children.end(); ++it) {
-                sub.add(it->to_json());
+
+            Data* child = first_child;
+            while(child) {
+                sub.add(child->to_json());
+                child = child->next_sibling;
             }
+
             obj.set("sub", sub);
 
             return obj;
@@ -64,77 +125,101 @@ private:
     };
 
     Stat* m_parent;
-    Data  m_data;
+    Data* m_data;
 
     bool  m_track_memory;
 
+    inline void append_child(Data* data) {
+        if(m_data->first_child) {
+            Data* last = data->first_child;
+            while(last->next_sibling) {
+                last = last->next_sibling;
+            }
+            last->next_sibling = data;
+        } else {
+            m_data->first_child = data;
+        }
+    }
+
     inline void track_alloc_internal(size_t bytes) {
         if(m_track_memory) {
-            m_data.mem_current += bytes;
-            m_data.mem_peak = std::max(m_data.mem_peak, m_data.mem_current);
+            m_data->mem_current += bytes;
+            m_data->mem_peak = std::max(m_data->mem_peak, m_data->mem_current);
             if(m_parent) m_parent->track_alloc_internal(bytes);
         }
     }
 
     inline void track_free_internal(size_t bytes) {
         if(m_track_memory) {
-            m_data.mem_current -= bytes;
+            m_data->mem_current -= bytes;
             if(m_parent) m_parent->track_free_internal(bytes);
         }
     }
 
 public:
     inline static void track_alloc(size_t bytes) {
-        if(s_current) s_current->track_alloc_internal(bytes);
+        if(s_current && s_current->m_track_memory) {
+            printf("[%s] alloc: %zu\n", s_current->m_data->title, bytes);
+            s_current->track_alloc_internal(bytes);
+        }
     }
 
     inline static void track_free(size_t bytes) {
-        if(s_current) s_current->track_free_internal(bytes);
+        if(s_current && s_current->m_track_memory) {
+            printf("[%s]  free: %zu\n", s_current->m_data->title, bytes);
+            s_current->track_free_internal(bytes);
+        }
     }
 
-    inline Stat(const std::string& title) {
+    inline Stat(const char* title) {
         m_parent = s_current;
+
+        if(m_parent) m_parent->m_track_memory = false;
+        m_data = new Data();
+        if(m_parent) m_parent->m_track_memory = true;
+        strncpy(m_data->title, title, Data::STR_BUFFER_SIZE);
+
         s_current = this;
-
-        m_data.title = title;
-
-        m_data.mem_off = m_parent ? m_parent->m_data.mem_current : 0;
-        m_data.mem_current = 0;
-        m_data.mem_peak = 0;
-
-        m_data.time_end = 0;
-        m_data.time_start = current_time_millis();
         m_track_memory = true;
 
-        // TODO: artificially un-track self?
+        m_data->mem_off = m_parent ? m_parent->m_data->mem_current : 0;
+        m_data->mem_current = 0;
+        m_data->mem_peak = 0;
+
+        m_data->time_end = 0;
+        m_data->time_start = current_time_millis();
+        m_track_memory = true;
     }
 
     ~Stat() {
         // finish
-        m_data.time_end = current_time_millis();
         m_track_memory = false;
+        m_data->time_end = current_time_millis();
 
-        // add data to parent's data
         if(m_parent) {
-            m_parent->m_data.children.push_back(m_data);
+            // add data to parent's data
+            m_parent->append_child(m_data);
+        } else {
+            // if this was the root, delete data
+            delete m_data;
         }
 
         // pop parent
         s_current = m_parent;
-
-        // TODO: artificially re-track self?
     }
 
     template<typename T>
-    inline void log_stat(const std::string& key, const T& value) {
-        m_track_memory = false; // TODO: ?
-        m_data.log_stat(key, value);
-        m_track_memory = true; // TODO: ?
+    inline void log_stat(const char* key, const T& value) {
+        m_track_memory = false;
+        m_data->log_stat(key, value);
+        m_track_memory = true;
     }
 
-    inline Data& data() {
-        m_data.time_end = current_time_millis();
-        return m_data;
+    inline void to_json(std::ostream& out) {
+        m_data->time_end = current_time_millis();
+        m_track_memory = false;
+        m_data->to_json().str(out);
+        m_track_memory = true;
     }
 };
 
