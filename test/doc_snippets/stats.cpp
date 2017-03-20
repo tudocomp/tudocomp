292836baa64bd7e6d6ca0680618a3c88028a61b5
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2017-03-20 12:51:18 +0100

    Update stat tracking doc snippet

--- test/doc_snippets/stats.cpp
+++ test/doc_snippets/stats.cpp
@@ -10,55 +10,68 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 
-#include <tudocomp/tudocomp.hpp>
+#include <tudocomp/ds/IntVector.hpp>
+#include <tudocomp_stat/StatPhase.hpp>
 
 #include <chrono>
 #include <thread>
 
 using namespace tdc;
 
-using ms = std::chrono::milliseconds;
-
 TEST(stats, example) {
-    Env env = create_env(Meta("undisclosed", "test", "Test environment"));
+    StatPhase root("Root");
 
     // Phase 1
-    env.begin_stat_phase("Phase 1");
-    {
+    StatPhase::wrap("Phase 1", []{
         char* alloc1 = new char[2048];
-        std::this_thread::sleep_for(ms(30));
+        std::this_thread::sleep_for(std::chrono::milliseconds(30));
         delete[] alloc1;
-    }
-    env.end_stat_phase();
+    });
 
     // Phase 2
-    env.begin_stat_phase("Phase 2");
-    {
+    StatPhase::wrap("Phase 2", []{
         char* alloc2 = new char[3072];
 
         // Phase 2.1
-        env.begin_stat_phase("Phase 2.1");
-        {
-            std::this_thread::sleep_for(ms(30));
-            env.log_stat("A statistic", 147);
-            env.log_stat("Another statistic", 0.5);
-        }
-        env.end_stat_phase();
+        StatPhase::wrap("Phase 2.1", []{
+            std::this_thread::sleep_for(std::chrono::milliseconds(30));
+
+            StatPhase::log("A statistic", 147);
+            StatPhase::log("Another statistic", 0.5);
+        });
 
         // Phase 2.2
-        env.begin_stat_phase("Phase 2.2");
-        {
+        StatPhase::wrap("Phase 2.2", []{
             char* alloc2_2 = new char[1024];
-            std::this_thread::sleep_for(ms(40));
+            std::this_thread::sleep_for(std::chrono::milliseconds(40));
             delete[] alloc2_2;
-        }
-        env.end_stat_phase();
+        });
 
         delete[] alloc2;
-    }
-    env.end_stat_phase();
+    });
+
+    // Phase 3.X (looping)
+    StatPhase::wrap("Phase 3", []{
+        IntVector<uint32_t> vec(512, 0);
+        
+        StatPhase sub_phase("Init");
+        int count = 0;
+        while(!vec.empty()) {
+            vec.pop_back();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+            if((++count % 128) == 0) {
+                vec.shrink_to_fit();
+
+                if(!vec.empty()) {
+                    sub_phase.split(std::string("@") + std::to_string(vec.size()));
+                    sub_phase.log_stat("remaining", vec.size());
+                }
+            }
+        };
+    });
 
     // Conclude tracking and print JSON to stdout
-    env.finish_stats().to_json().str(std::cout);
+    root.to_json().str(std::cout);
 }
 
