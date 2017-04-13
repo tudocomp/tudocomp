4753356993167b4381a3826d29a77ac43624143a
Patrick Dinklage <patrick.dinklage@uni-dortmund.de>
2017-04-13 11:40:19 +0200

    Implement mixable type lists for compile-time mapping of integers to types

---
+++ include/tudocomp/util/type_list.hpp
@@ -0,0 +1,178 @@
+#pragma once
+
+namespace tdc {
+
+/// \brief Contains meta-programming utilities for type lists.
+namespace type_list {
+
+/// \brief Meta-type for a list of types.
+template<typename... Ts> struct type_list;
+
+/// \brief An empty type list.
+using mt = type_list<>;
+
+/// \brief Represents "no" type.
+struct None;
+
+/// \brief Represents an unidentifiable, ambiguous type.
+struct Ambiguous;
+
+/// \brief Produces a type list by prepending \ref None exactly N times.
+///
+/// This is intended for initialization of a type list with N elements.
+/// The resulting type list can be obtained via the \c list member.
+///
+/// \tparam N  the amount of \ref None items to prepend
+/// \tparam Tl the initial list (e.g. \ref mt)
+template<size_t N, typename Tl = mt> struct init;
+
+/// \cond INTERNAL
+
+// recursive case (N > 0): prepend None and continue with N - 1
+template<size_t N, typename... Ts> struct init<N, type_list<Ts...>> {
+    using list = typename init<N - 1, type_list<None, Ts...>>::list;
+};
+
+// trivial case (N = 0): "don't prepend none"
+template<typename... Ts> struct init<0, type_list<Ts...>> {
+    using list = type_list<Ts...>;
+};
+/// \endcond
+
+/// \brief Gets i-th type in a type list.
+///
+/// The result type can be obtained via the \c type member.
+///
+/// \tparam I  the index in the type list
+/// \tparam Tl the type list in question
+template<size_t I, typename Tl> struct get_type;
+
+/// \cond INTERNAL
+
+// recursive case (I > 0): cut off head and continue with I - 1
+template<size_t I, typename Head, typename... Tail>
+struct get_type<I, type_list<Head, Tail...>>
+: get_type<I - 1, type_list<Tail...>>{};
+
+// trivial case (I = 0): yield head
+template<typename Head, typename... Tail>
+struct get_type<0, type_list<Head, Tail...>> {
+    using type = Head;
+};
+/// \endcond
+
+/// \brief Prepends a type to a type list.
+///
+/// The resulting type list can be obtained via the \c list member.
+///
+/// \tparam T  the type to prepend
+/// \tparam Tl the type list to modify
+template<typename T, typename Tl> struct prepend;
+
+/// \cond INTERNAL
+
+// impl
+template<typename T, typename... Ts>
+struct prepend<T, type_list<Ts...>> {
+    using list = type_list<T, Ts...>;
+};
+/// \endcond
+
+/// \brief Mixes two type lists.
+///
+/// The type lists must be equally long.
+///
+/// For each pair of types \c T1 and \c T2 in the two lists, the result list
+/// will be determined using the following rules:
+///
+/// * \ref None if both \c T1 and \c T2 are \ref None.
+/// * \c T1 if \c T1 is not \ref None and \c T2 is \ref None.
+/// * \c T2 if \c T1 is \ref None and \c T2 is not \ref None.
+/// * \ref Ambiguous if neither \c T1 or \c T2 are \ref None.
+///
+/// The resulting type list can be obtained via the \c list member.
+///
+/// \tparam Tl1 the fist type list
+/// \tparam Tl2 the second type list
+template<typename Tl1, typename Tl2> struct mix;
+
+/// \cond INTERNAL
+
+// case 1: Head1 != None, Head2 != None => Ambiguous
+template<typename Head1, typename... Tail1, typename Head2, typename... Tail2>
+struct mix<type_list<Head1, Tail1...>, type_list<Head2, Tail2...>> {
+    using list = typename prepend<Ambiguous,
+        typename mix<
+            type_list<Tail1...>,
+            type_list<Tail2...>
+        >::list
+    >::list;
+};
+
+// case 2: Head1 != None, Head2 == None => Head1
+template<typename Head1, typename... Tail1, typename... Tail2>
+struct mix<type_list<Head1, Tail1...>, type_list<None, Tail2...>> {
+    using list = typename prepend<Head1,
+        typename mix<
+            type_list<Tail1...>,
+            type_list<Tail2...>
+        >::list
+    >::list;
+};
+
+// case 3: Head1 == None, Head2 != None => Head2
+template<typename... Tail1, typename Head2, typename... Tail2>
+struct mix<type_list<None, Tail1...>, type_list<Head2, Tail2...>> {
+    using list = typename prepend<Head2,
+        typename mix<
+            type_list<Tail1...>,
+            type_list<Tail2...>
+        >::list
+    >::list;
+};
+
+// case 4: Head1 == None, Head2 == None => None
+template<typename... Tail1, typename... Tail2>
+struct mix<type_list<None, Tail1...>, type_list<None, Tail2...>> {
+    using list = typename prepend<None,
+        typename mix<
+            type_list<Tail1...>,
+            type_list<Tail2...>
+        >::list
+    >::list;
+};
+
+// trivial case: mixing two empty lists yields an empty list
+template<> struct mix<mt, mt> {
+    using list = mt;
+};
+/// \endcond
+
+/// \brief Mixes multiple lists.
+///
+/// Applies \ref mix to each consecutive pair of type lists.
+/// The resulting type list can be obtained via the \c list member.
+///
+/// tparam Tls the lists to mix
+template<typename... Tls> struct multimix;
+
+/// \cond INTERNAL
+
+// recursive case: mix first two in list, then continue with that and remainder
+template<typename Tl1, typename Tl2, typename... Tls>
+struct multimix<Tl1, Tl2, Tls...> {
+    using list =
+        typename multimix<
+            typename mix<Tl1, Tl2>::list,
+            Tls...
+        >::list;
+};
+
+// trivial case: multimixing one list yields the same list
+template<typename Tl>
+struct multimix<Tl> {
+    using list = Tl;
+};
+/// \endcond
+
+}} //ns
---
+++ test/type_list_test.cpp
@@ -0,0 +1,69 @@
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include <tudocomp/util/type_list.hpp>
+
+/*
+    TODO:
+    - allow "mix" of lists of different sizes
+    - sugar syntax for "provides" lists below
+*/
+
+// providers
+struct A {
+    using provides = tdc::type_list::type_list<
+        /*0*/ A,
+        /*1*/ tdc::type_list::None,
+        /*2*/ A,
+        /*3*/ tdc::type_list::None,
+        /*4*/ tdc::type_list::None
+    >;
+
+    static constexpr int test_id = 0xA;
+};
+
+struct B {
+    using provides = tdc::type_list::type_list<
+        /*0*/ tdc::type_list::None,
+        /*1*/ tdc::type_list::None,
+        /*2*/ tdc::type_list::None,
+        /*3*/ B,
+        /*4*/ tdc::type_list::None
+    >;
+
+    static constexpr int test_id = 0xB;
+};
+
+struct C {
+    using provides = tdc::type_list::type_list<
+        /*0*/ tdc::type_list::None,
+        /*1*/ C,
+        /*2*/ tdc::type_list::None,
+        /*3*/ tdc::type_list::None,
+        /*4*/ C
+    >;
+
+    static constexpr int test_id = 0xC;
+};
+
+// manager
+using dsid_t = size_t;
+
+template<typename... Providers>
+struct manager {
+    using providers = typename tdc::type_list::multimix<
+        typename Providers::provides...>::list;
+
+    template<dsid_t Id>
+    using get_provider = typename tdc::type_list::get_type<Id, providers>::type;
+};
+
+// instance
+using manager_t = manager<B, C, A>;
+
+// test mapping at compile time
+static_assert(manager_t::get_provider<0>::test_id == 0xA, "something's wrong");
+static_assert(manager_t::get_provider<1>::test_id == 0xC, "something's wrong");
+static_assert(manager_t::get_provider<2>::test_id == 0xA, "something's wrong");
+static_assert(manager_t::get_provider<3>::test_id == 0xB, "something's wrong");
+static_assert(manager_t::get_provider<4>::test_id == 0xC, "something's wrong");
