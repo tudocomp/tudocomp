fd19e5138d42d2291a024ed0df5969b9af644ad7
Patrick Dinklage <patrick.dinklage@uni-dortmund.de>
2017-04-17 10:32:02 +0200

    Restructure type_list code

--- include/tudocomp/util/type_list.hpp
+++ include/tudocomp/util/type_list.hpp
@@ -18,43 +18,22 @@ struct None;
 struct Ambiguous;
 
 /// \cond INTERNAL
+// declarations
+// see public using declarations below for descriptions
 
-// decl
 template<size_t I, typename Tl> struct _get;
-
-// recursive case (I > 0): cut off head and continue with I - 1
-template<size_t I, typename Head, typename... Tail>
-struct _get<I, type_list<Head, Tail...>>
-: _get<I - 1, type_list<Tail...>>{};
-
-// trivial case (I = 0): yield head
-template<typename Head, typename... Tail>
-struct _get<0, type_list<Head, Tail...>> {
-    using type = Head;
-};
+template<typename T, typename Tl> struct _prepend;
+template<size_t I, typename T> struct _set;
+template<typename Tl1, typename Tl2> struct _mix;
+template<typename... Tls> struct _multimix;
 /// \endcond
 
 /// \brief Gets the i-th type in a type list.
-///
-/// The result type can be obtained via the \c type member.
 ///
 /// \tparam I  the index in the type list
 /// \tparam Tl the type list in question
 template<size_t I, typename Tl>
 using get = typename _get<I, Tl>::type;
-
-/// \cond INTERNAL
-
-// decl
-template<typename T, typename Tl> struct _prepend;
-
-// impl
-template<typename T, typename... Ts>
-struct _prepend<T, type_list<Ts...>> {
-    using list = type_list<T, Ts...>;
-};
-
-/// \endcond
 
 /// \brief Prepends a type to a type list.
 ///
@@ -65,21 +44,6 @@ struct _prepend<T, type_list<Ts...>> {
 template<typename T, typename Tl>
 using prepend = typename _prepend<T, Tl>::list;
 
-/// \cond INTERNAL
-
-// recursive case (I > 0):
-// produce a type list of None and append set with I-1
-template<size_t I, typename T> struct _set {
-    using list = prepend<None, typename _set<I - 1, T>::list>;
-};
-
-// trivial case (I = 0): produce a type list of just T
-template<typename T> struct _set<0, T> {
-    using list = type_list<T>;
-};
-
-/// \endcond
-
 /// \brief Produces a type list where the i-th type is the specified type
 ///        and all other types are None.
 ///
@@ -89,74 +53,6 @@ template<typename T> struct _set<0, T> {
 /// \tparam T  the type to set at the specified index
 template<size_t I, typename T>
 using set = typename _set<I, T>::list;
-
-/// \cond INTERNAL
-
-// decl
-template<typename Tl1, typename Tl2> struct _mix;
-
-// case 1: Head1 != None, Head2 != None => Ambiguous
-template<typename Head1, typename... Tail1, typename Head2, typename... Tail2>
-struct _mix<type_list<Head1, Tail1...>, type_list<Head2, Tail2...>> {
-    using list = prepend<Ambiguous,
-        typename _mix<
-            type_list<Tail1...>,
-            type_list<Tail2...>
-        >::list
-    >;
-};
-
-// case 2: Head1 != None, Head2 == None => Head1
-template<typename Head1, typename... Tail1, typename... Tail2>
-struct _mix<type_list<Head1, Tail1...>, type_list<None, Tail2...>> {
-    using list = prepend<Head1,
-        typename _mix<
-            type_list<Tail1...>,
-            type_list<Tail2...>
-        >::list
-    >;
-};
-
-// case 3: Head1 == None, Head2 != None => Head2
-template<typename... Tail1, typename Head2, typename... Tail2>
-struct _mix<type_list<None, Tail1...>, type_list<Head2, Tail2...>> {
-    using list = prepend<Head2,
-        typename _mix<
-            type_list<Tail1...>,
-            type_list<Tail2...>
-        >::list
-    >;
-};
-
-// case 4: Head1 == None, Head2 == None => None
-template<typename... Tail1, typename... Tail2>
-struct _mix<type_list<None, Tail1...>, type_list<None, Tail2...>> {
-    using list = prepend<None,
-        typename _mix<
-            type_list<Tail1...>,
-            type_list<Tail2...>
-        >::list
-    >;
-};
-
-// trivial case: mix empty list with a non-empty list
-template<typename Head1, typename... Tail1>
-struct _mix<type_list<Head1, Tail1...>, mt> {
-    using list = prepend<Head1,
-        typename _mix<type_list<Tail1...>, mt>::list>;
-};
-
-template<typename Head2, typename... Tail2>
-struct _mix<mt, type_list<Head2, Tail2...>> {
-    using list = prepend<Head2,
-        typename _mix<mt, type_list<Tail2...>>::list>;
-};
-
-// trivial case: mixing two empty lists yields an empty list
-template<> struct _mix<mt, mt> {
-    using list = mt;
-};
-/// \endcond
 
 /// \brief Mixes two type lists.
 ///
@@ -175,27 +71,6 @@ template<> struct _mix<mt, mt> {
 template<typename Tl1, typename Tl2>
 using mix = typename _mix<Tl1, Tl2>::list;
 
-/// \cond INTERNAL
-// decl
-template<typename... Tls> struct _multimix;
-
-// recursive case: mix first two in list, then continue with that and remainder
-template<typename Tl1, typename Tl2, typename... Tls>
-struct _multimix<Tl1, Tl2, Tls...> {
-    using list =
-        typename _multimix<
-            mix<Tl1, Tl2>,
-            Tls...
-        >::list;
-};
-
-// trivial case: multimixing one list yields the same list
-template<typename Tl>
-struct _multimix<Tl> {
-    using list = Tl;
-};
-/// \endcond
-
 /// \brief Mixes multiple lists.
 ///
 /// Applies \ref mix to each consecutive pair of type lists.
@@ -204,5 +79,108 @@ struct _multimix<Tl> {
 /// tparam Tls the lists to mix
 template<typename... Tls>
 using multimix = typename _multimix<Tls...>::list;
+
+/// \cond INTERNAL
+// implementations
+
+// get - recursive case (I > 0):
+// cut off head and continue with I - 1
+template<size_t I, typename Head, typename... Tail>
+struct _get<I, type_list<Head, Tail...>>
+: _get<I - 1, type_list<Tail...>>{};
+
+// get - trivial case (I = 0):
+// simply yield head
+template<typename Head, typename... Tail>
+struct _get<0, type_list<Head, Tail...>> {
+    using type = Head;
+};
+
+// prepend - all cases
+template<typename T, typename... Ts>
+struct _prepend<T, type_list<Ts...>> {
+    using list = type_list<T, Ts...>;
+};
+
+// set - recursive case (I > 0):
+// produce a type list of None and append set with I-1
+template<size_t I, typename T> struct _set {
+    using list = prepend<None, set<I - 1, T>>;
+};
+
+// set - trivial case (I = 0):
+// produce a type list of just T
+template<typename T> struct _set<0, T> {
+    using list = type_list<T>;
+};
+
+// mix - case 1: Head1 != None, Head2 != None
+// => yield Ambiguous
+template<typename Head1, typename... Tail1, typename Head2, typename... Tail2>
+struct _mix<type_list<Head1, Tail1...>, type_list<Head2, Tail2...>> {
+    using list = prepend<Ambiguous,
+        mix<type_list<Tail1...>, type_list<Tail2...>>
+    >;
+};
+
+// mix - case 2: Head1 != None, Head2 == None
+// => yield Head1
+template<typename Head1, typename... Tail1, typename... Tail2>
+struct _mix<type_list<Head1, Tail1...>, type_list<None, Tail2...>> {
+    using list = prepend<Head1,
+        mix<type_list<Tail1...>, type_list<Tail2...>>
+    >;
+};
+
+// mix - case 3: Head1 == None, Head2 != None
+// => yield Head2
+template<typename... Tail1, typename Head2, typename... Tail2>
+struct _mix<type_list<None, Tail1...>, type_list<Head2, Tail2...>> {
+    using list = prepend<Head2,
+        mix<type_list<Tail1...>, type_list<Tail2...>>
+    >;
+};
+
+// mix - case 4: Head1 == None, Head2 == None
+// => yield None
+template<typename... Tail1, typename... Tail2>
+struct _mix<type_list<None, Tail1...>, type_list<None, Tail2...>> {
+    using list = prepend<None,
+        mix<type_list<Tail1...>, type_list<Tail2...>>
+    >;
+};
+
+// mix - trivial case: mix empty list (right) with a non-empty list (left)
+template<typename Head1, typename... Tail1>
+struct _mix<type_list<Head1, Tail1...>, mt> {
+    using list = prepend<Head1, mix<type_list<Tail1...>, mt>>;
+};
+
+// mix - trivial case: mix empty list (left) with a non-empty list (right)
+template<typename Head2, typename... Tail2>
+struct _mix<mt, type_list<Head2, Tail2...>> {
+    using list = prepend<Head2, mix<mt, type_list<Tail2...>>>;
+};
+
+// mix - trivial case: mix two empty lists
+template<> struct _mix<mt, mt> {
+    using list = mt;
+};
+
+// multimix - recursive case:
+// mix first two in list, then continue with that and remainder
+template<typename Tl1, typename Tl2, typename... Tls>
+struct _multimix<Tl1, Tl2, Tls...> {
+    using list = multimix<mix<Tl1, Tl2>, Tls...>;
+};
+
+// trivial case:
+// multimixing one list yields the same list
+template<typename Tl>
+struct _multimix<Tl> {
+    using list = Tl;
+};
+
+/// \endcond
 
 }} //ns
