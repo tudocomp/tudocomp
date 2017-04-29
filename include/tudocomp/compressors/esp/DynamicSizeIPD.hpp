a62b894161083495ef9524955617992aff40f2d2
Marvin Löbel <marvin.loebel@tu-dortmund.de>
2017-04-29 13:15:35 +0200

    Seemingly implement DynamicSize Map logic

--- include/tudocomp/compressors/esp/DynamicSizeIPD.hpp
+++ include/tudocomp/compressors/esp/DynamicSizeIPD.hpp
@@ -13,6 +13,42 @@ namespace tdc {namespace esp {
     struct SizeAdjust<size_t> {
         template<size_t N>
         using Type = uint_t<N>;
+
+        template<size_t N>
+        inline uint_t<N> cast_to(size_t val) {
+            return val;
+        }
+
+        template<size_t N>
+        inline size_t cast_from(uint_t<N> val) {
+            return val;
+        }
+    };
+
+    template<size_t M>
+    struct SizeAdjust<Array<M, size_t>> {
+        template<size_t N>
+        using Type = Array<M, uint_t<N>>;
+
+        template<size_t N>
+        inline Array<M, uint_t<N>> cast_to(Array<M, size_t> val) {
+            Array<M, uint_t<N>> ret;
+            for(size_t i = 0; i < ret.size(); i++) {
+                ret[i] = val[i];
+            }
+
+            return val;
+        }
+
+        template<size_t N>
+        inline Array<M, size_t> cast_from(Array<M, uint_t<N>> val) {
+            Array<M, size_t> ret;
+            for(size_t i = 0; i < ret.size(); i++) {
+                ret[i] = val[i];
+            }
+
+            return val;
+        }
     };
 
     template<typename ipd_t>
@@ -37,29 +73,101 @@ namespace tdc {namespace esp {
                 virtual U access(const Array<N, T>& key, Updater updater) = 0;
                 virtual size_t size() const = 0;
                 virtual void for_all(ForEach f) const = 0;
+                virtual bool can_fit_value(U new_val) const = 0;
+                virtual bool can_fit_key(const Array<N, T>& key) const = 0;
             };
             template<size_t M, size_t O>
             struct DynamicMapOf: DynamicMap {
+                using Updater = typename DynamicMap::Updater;
+                using ForEach = typename DynamicMap::ForEach;
                 using MappedT = typename SizeAdjust<T>::template Type<M>;
                 using MappedU = typename SizeAdjust<U>::template Type<O>;
 
+                Array<N, MappedT> m_empty;
                 typename ipd_t::template IPDMap<Array<N, MappedT>, MappedU> m_map;
+
+                inline DynamicMapOf(size_t bucket_count, const Array<N, T>& empty):
+                    m_empty(typename SizeAdjust<Array<N, T>>::cast_to(empty)),
+                    m_map(bucket_count, m_empty) {}
+
+                inline virtual size_t size() const override final {
+                    return m_map.size();
+                }
+
+                inline virtual void for_all(ForEach f) const override final {
+                    for(auto& kv : m_map) {
+                        const auto& key = kv.first;
+                        const auto& val = kv.second;
+
+                        const auto& key2 = typename SizeAdjust<Array<N, T>>::cast_to(key);
+                        const auto& val2 = typename SizeAdjust<T>::cast_to(val);
+
+                        f(key.as_view(), val);
+                    }
+                }
+
+                inline virtual bool can_fit_value(U val) const override final {
+                    return bits_for(val) <= O;
+                }
+
+                inline virtual bool can_fit_key(const Array<N, T>& key) const override final {
+                    if (typename SizeAdjust<Array<N, T>>::cast_to(key) == m_empty) {
+                        return false;
+                    }
+
+                    size_t val = 0;
+                    for (auto x : key.as_view()) {
+                        val = std::max(val, x);
+                    }
+                    return bits_for(val) <= M;
+                }
+
+                inline virtual U access(const Array<N, T>& key, Updater updater) const override final {
+                    Array<N, MappedT> actual_key = typename SizeAdjust<Array<N, T>>::cast_to(key);
+                    return m_map.access(actual_key, [&](MappedU& val) {
+                        U val2 = typename SizeAdjust<T>::cast_from(val);
+                        updater(val2);
+                        val = typename SizeAdjust<T>::cast_to(val2);
+                    });
+                }
             };
 
-            std::unordered_map<Array<N, T>, U> m_map;
+            std::unique_ptr<DynamicMap> m_map;
 
         public:
-            inline IPDMap(size_t bucket_count, const Array<N, T>& empty)
-                {}
+            inline IPDMap(size_t bucket_count, const Array<N, T>& empty) {
+                m_map = std::make_unique<DynamicMapOf<64, 64>>(bucket_count, empty);
+            }
 
             template<typename Updater>
             inline U access(const Array<N, T>& key, Updater updater) {
-                auto& val = m_map[key];
+                if (!m_map->can_fit_key(key)) {
+                    // RESIZE
+                }
 
-                U val2 = val;
-                updater(val2);
-                val = val2;
-                return val2;
+                auto copy = U();
+                bool value_resize = false;
+
+                U ret = m_map->access(key, [&](U& val) {
+                    copy = val;
+                    updater(copy);
+                    if (m_map->can_fit_value(copy)) {
+                        val = copy;
+                    } else {
+                        value_resize = true;
+                    }
+                });
+
+                if (value_resize) {
+                    // RESIZE
+                    ret = m_map->access(key, [&](U& val) {
+                        val = copy;
+                    });
+                }
+
+                DCHECK_EQ(ret, copy);
+
+                return copy;
             }
 
             inline size_t size() const {
