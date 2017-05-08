d62856cb0e72dfef880b30bd424cf3185f9c2a45
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2017-05-08 14:07:41 +0200

    Switch to C++14

--- include/tudocomp/util.hpp
+++ include/tudocomp/util.hpp
@@ -385,40 +385,6 @@ inline std::string make_table(const std::vector<std::string>& data,
 
 }
 
-/// \cond INTERNAL
-// this codebase is using c++11 but would really like to use this function...
-namespace std {
-    template<class T> struct _Unique_if {
-        typedef unique_ptr<T> _Single_object;
-    };
-
-    template<class T> struct _Unique_if<T[]> {
-        typedef unique_ptr<T[]> _Unknown_bound;
-    };
-
-    template<class T, size_t N> struct _Unique_if<T[N]> {
-        typedef void _Known_bound;
-    };
-
-    template<class T, class... Args>
-        typename _Unique_if<T>::_Single_object
-        make_unique(Args&&... args) {
-            return unique_ptr<T>(new T(std::forward<Args>(args)...));
-        }
-
-    template<class T>
-        typename _Unique_if<T>::_Unknown_bound
-        make_unique(size_t n) {
-            typedef typename remove_extent<T>::type U;
-            return unique_ptr<T>(new U[n]());
-        }
-
-    template<class T, class... Args>
-        typename _Unique_if<T>::_Known_bound
-        make_unique(Args&&...) = delete;
-}
-
-/// \endcond
 //Check that p is a permutation of [0..n-1]
 
 #if defined(DEBUG) && defined(PARANOID) //functions that cost more than constant time to check
