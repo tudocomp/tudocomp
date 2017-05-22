75abc55781adf76e99cbf3182b2973e97670a109
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2017-05-22 15:55:27 +0200

    comment to etc/continuous_test.sh

--- etc/continuous_test.sh
+++ etc/continuous_test.sh
@@ -1,4 +1,25 @@
 #!/bin/zsh
+# Run continous random tests on the compressor specified in the function check
+# Aborts when the decompression does not restore the original input
+# change kSize to the number of generated characters
+
+##### EDIT
+local -r kSize=1000
+function check {
+#./tdc -a 'lcpcomp(ascii,comp=plcppeaks,dec=compact)' dna.random -o dna.random.tdc --force && 
+#./tdc -a 'lcpcomp(ascii,comp=heap,dec=compact)' dna.random -o dna.random.tdc --force && 
+#./tdc -a 'lcpcomp(ascii,comp=plcp,dec=compact)' dna.random -o dna.random.tdc --force && 
+#./tdc -a 'lz78(ascii,monte(hash_roller = rk))' dna.random -o dna.random.tdc --force && 
+./tdc -a 'lz78(ascii,ternary)' dna.random -o dna.random.tdc --force && 
+#./tdc -a 'lz78(ascii,myhash)' dna.random -o dna.random.tdc --force && 
+#./tdc -a 'lz78u(streaming(ascii),ascii)' dna.random -o dna.random.tdc --force && 
+./tdc --decompress dna.random.tdc --output dna.random.orig --force && 
+diff -q dna.random dna.random.orig ||
+die "Sizes differ"
+#echo "terminate called after throwing an instance"
+}
+##### EDIT END
+
 
 function die {
 echo "$1"
@@ -17,19 +38,6 @@ function genDNA {
 
 
 
-function check {
-#./tdc -a 'lcpcomp(ascii,comp=plcppeaks,dec=compact)' dna.random -o dna.random.tdc --force && 
-#./tdc -a 'lcpcomp(ascii,comp=heap,dec=compact)' dna.random -o dna.random.tdc --force && 
-#./tdc -a 'lcpcomp(ascii,comp=plcp,dec=compact)' dna.random -o dna.random.tdc --force && 
-#./tdc -a 'lz78(ascii,monte(hash_roller = rk))' dna.random -o dna.random.tdc --force && 
-./tdc -a 'lz78(ascii,ternary)' dna.random -o dna.random.tdc --force && 
-#./tdc -a 'lz78(ascii,myhash)' dna.random -o dna.random.tdc --force && 
-#./tdc -a 'lz78u(streaming(ascii),ascii)' dna.random -o dna.random.tdc --force && 
-./tdc --decompress dna.random.tdc --output dna.random.orig --force && 
-diff -q dna.random dna.random.orig ||
-die "Sizes differ"
-#echo "terminate called after throwing an instance"
-}
 
 wheel="_/\\|"
 wheeli=1
@@ -38,7 +46,7 @@ it=1
 
 while [ 1 ]; do 
 make -s tudocomp_driver >/dev/null&&
-./genDNA.sh 1000 > dna.random &&
+./genDNA.sh $kSize > dna.random &&
 timeA=$(date +%s%N | cut -b1-13)
 check dna.random 
 timeB=$(date +%s%N | cut -b1-13)
