3dfe9c7f2a7e370ba7091170242c7314758cc286
Patrick Dinklage <patrick.dinklage@uni-dortmund.de>
2017-05-28 10:27:15 +0200

    Improved AST definition and parsing

--- test/meta_tests.cpp
+++ test/meta_tests.cpp
@@ -4,13 +4,13 @@
 #include <tudocomp/util/meta/DeclAlgorithm.hpp>
 #include <tudocomp/util/meta/DeclParam.hpp>
 
-#include <tudocomp/util/meta/ASTNode.hpp>
+#include <tudocomp/util/meta/ASTDef.hpp>
 #include <tudocomp/util/meta/ASTParser.hpp>
 
 using namespace tdc::meta;
 
 TEST(Sandbox, example) {
-    auto lz77 = decl::Algorithm("lz77", "compressor", "LZ77 online compressor.");
+    /*auto lz77 = decl::Algorithm("lz77", "compressor", "LZ77 online compressor.");
     lz77.add_param(decl::Param("window"));
     lz77.add_param(decl::Param("coder", false, false, "coder"));
     //DLOG(INFO) << lz77.str();
@@ -21,11 +21,11 @@ TEST(Sandbox, example) {
     // registry
     decl::AlgorithmMap map;
     map.emplace("lz77", std::move(lz77));
-    map.emplace("binary", std::move(binary));
+    map.emplace("binary", std::move(binary));*/
 
     // parse
     DLOG(INFO) << "parse...";
-    auto node = ast::Parser::parse("lz77(window=10,coder=binary(x=-3,q=sub(name='arf')))");
-    DLOG(INFO) << node.str();
+    auto v = ast::Parser::parse("lz77(coder=binary,window=10,values=[1,2,3,4,5])");
+    DLOG(INFO) << v->str();
 }
 
