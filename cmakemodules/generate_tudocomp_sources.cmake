1628e68e86803315220184d3781f1000389e8f82
Marvin Löbel <marvin.loebel@tu-dortmund.de>
2017-06-02 16:18:13 +0200

    Move registry generation script to own cmake directory

--- cmakemodules/generate_tudocomp_sources.cmake
+++ cmakemodules/generate_tudocomp_sources.cmake
@@ -2,13 +2,6 @@ file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/include/tudocomp)
 
 CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/include/tudocomp/config.h.in ${CMAKE_BINARY_DIR}/include/tudocomp/config.h)
 
-add_custom_command(
-    OUTPUT ${CMAKE_BINARY_DIR}/tudocomp_algorithms.cpp
-    COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/etc/genregistry.py ${CMAKE_BINARY_DIR}/include/tudocomp/config.h "tudocomp_algorithms.cpp" ${CMAKE_BINARY_DIR}/tudocomp_algorithms.cpp
-    COMMENT "Generating tudocomp_algorithms.cpp..."
-    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/etc/genregistry.py ${CMAKE_BINARY_DIR}/include/tudocomp/config.h
-)
-
 add_custom_target(
     generate_version
     COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/etc/genversion.sh ${CMAKE_BINARY_DIR}/include/tudocomp/version.hpp ${VERSION}
