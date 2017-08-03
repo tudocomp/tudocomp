2c586aa1ec2d9315270a7db2f9e709a73cf333a5
Marvin Löbel <marvin.loebel@tu-dortmund.de>
2017-08-03 17:38:39 +0200

    Mention large datasets in the docs

--- etc/large_datasets_config.py
+++ etc/large_datasets_config.py
@@ -10,6 +10,7 @@ download_and_extract("large", sizes, [
     DACIT_URL + "/rlzap_dlcp_female",
 
     # http://www.internationalgenome.org/data-portal/sample/NA19000
+    # (Alphabet: "GTCA", all "N" filtered out)
     DACIT_URL + "/1000Genome_JPT_NA19000_SRR099528_1.fastq.filtered4_no_N.dna",
 
     # https://panthema.net/2012/1119-eSAIS-Inducing-Suffix-and-LCP-Arrays-in-External-Memory/
