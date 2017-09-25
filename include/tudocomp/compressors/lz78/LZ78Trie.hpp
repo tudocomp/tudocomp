21282e72998dc56209be01e354efa0b65492cacd
Marvin Löbel <marvin.loebel@tu-dortmund.de>
2017-09-25 11:55:45 +0200

    Remove unwanted virtual interface of lz78 trie

--- include/tudocomp/compressors/lz78/LZ78Trie.hpp
+++ include/tudocomp/compressors/lz78/LZ78Trie.hpp
@@ -33,43 +33,51 @@ public:
 
 
 #define LZ78_DICT_SIZE_DESC \
-			"`dict_size` has to either be 0 (unlimited), or a positive integer,\n" \
-			"and determines the maximum size of the backing storage of\n" \
-			"the dictionary before it gets reset."
+            "`dict_size` has to either be 0 (unlimited), or a positive integer,\n" \
+            "and determines the maximum size of the backing storage of\n" \
+            "the dictionary before it gets reset."
 
 template<typename search_pos>
 class LZ78Trie {
 public:
     using node_t = TrieNode<search_pos>;
 private:
-	const size_t m_n;
-	const size_t& m_remaining_characters;
+    const size_t m_n;
+    const size_t& m_remaining_characters;
 protected:
-	LZ78Trie(const size_t n, const size_t& remaining_characters)
-		: m_n(n), m_remaining_characters(remaining_characters) {}
+    LZ78Trie(const size_t n, const size_t& remaining_characters)
+        : m_n(n), m_remaining_characters(remaining_characters) {}
 
     inline size_t expected_number_of_remaining_elements(const size_t z) const {
         return lz78_expected_number_of_remaining_elements(z, m_n, m_remaining_characters);
     }
 
-	/**
-	 * The dictionary can store multiple root nodes
-	 * For LZ78, we use a root node with the id = c = 0.
-	 * For LZW, we add for each possible literal value a root node.
-	 * The compressor has to add these nodes.
-	 */
-	virtual node_t add_rootnode(uliteral_t c) = 0;
+    /**
+     * The dictionary can store multiple root nodes
+     * For LZ78, we use a root node with the id = c = 0.
+     * For LZW, we add for each possible literal value a root node.
+     * The compressor has to add these nodes.
+     */
+    inline node_t add_rootnode(uliteral_t c) {
+        CHECK(false) << "This needs to be implemented by a inheriting class";
+        return 0;
+    }
 
     /**
      * Returns the root node corresponding to literal c
      */
-    virtual node_t get_rootnode(uliteral_t c) = 0;
+    inline node_t get_rootnode(uliteral_t c) {
+        CHECK(false) << "This needs to be implemented by a inheriting class";
+        return 0;
+    }
 
-	/**
-	 * Erases the contents of the dictionary.
-	 * Used by compressors with limited dictionary size.
-	 */
-	virtual void clear() = 0;
+    /**
+     * Erases the contents of the dictionary.
+     * Used by compressors with limited dictionary size.
+     */
+    inline void clear() {
+        CHECK(false) << "This needs to be implemented by a inheriting class";
+    }
 
     /** Searches a pair (`parent`, `c`). If there is no node below `parent` on an edge labeled with `c`, a new leaf of the `parent` will be constructed.
       * @param parent  the parent node's id
@@ -77,12 +85,18 @@ protected:
       * @return the index of the respective child, if it was found.
       * @retval undef_id   if a new leaf was inserted
       **/
-    virtual node_t find_or_insert(const node_t& parent, uliteral_t c) = 0;
+    inline node_t find_or_insert(const node_t& parent, uliteral_t c) {
+        CHECK(false) << "This needs to be implemented by a inheriting class";
+        return 0;
+    }
 
-	/**
-	 * Returns the number of entries, plus the number of rootnodes
-	 */
-    virtual factorid_t size() const = 0;
+    /**
+     * Returns the number of entries, plus the number of rootnodes
+     */
+    inline factorid_t size() const {
+        CHECK(false) << "This needs to be implemented by a inheriting class";
+        return 0;
+    }
 
 };
 
