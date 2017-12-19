8a632914bbc9eaa3c2bbe4725ae8281e58d4217d
Marvin Löbel <marvin.loebel@tu-dortmund.de>
2017-12-19 10:43:39 +0100

    migrate dataset download to dolomit server

--- etc/datasets_config.py
+++ etc/datasets_config.py
@@ -1,48 +1,48 @@
 small_sizes = [200, 100, 50, 10, 1]
 
 # HashTag Datasets, see http://acube.di.unipi.it/datasets/
-URL = "http://acube.di.unipi.it/repo/he-graph"
+ACUBE_URL = "http://acube.di.unipi.it/repo/he-graph"
 download_and_extract("hashtag", small_sizes, [
-    URL + "/graph/he-graph.txt.gz",
-    URL + "/relatedness/pairs.tsv",
-    URL + "/classification/crowdflower.tsv",
+    ACUBE_URL + "/graph/he-graph.txt.gz",
+    ACUBE_URL + "/relatedness/pairs.tsv",
+    ACUBE_URL + "/classification/crowdflower.tsv",
 ])
 
-DACIT_URL = "dacit.cs.uni-dortmund.de/datasets"
+TDC_URL = "dolomit.cs.uni-dortmund.de/"
 
 # ASCII-only, no inner null commoncrawl.org extract
 download_and_extract("cc", small_sizes, [
-    DACIT_URL + "/commoncrawl.ascii"
+    TDC_URL + "/commoncrawl.ascii"
 ])
 
 # Pizza&Chili Corpus text collection
 download_and_extract("pc", small_sizes, [
-    DACIT_URL + "/code/sources.gz",
-    DACIT_URL + "/music/pitches.gz",
-    DACIT_URL + "/protein/proteins.gz",
-    DACIT_URL + "/dna/dna.gz",
-    DACIT_URL + "/nlang/english.gz",
-    DACIT_URL + "/xml/dblp.xml.gz",
+    TDC_URL + "/code/sources.gz",
+    TDC_URL + "/music/pitches.gz",
+    TDC_URL + "/protein/proteins.gz",
+    TDC_URL + "/dna/dna.gz",
+    TDC_URL + "/nlang/english.gz",
+    TDC_URL + "/xml/dblp.xml.gz",
 ])
 download_and_extract("pcr", small_sizes, [
-    DACIT_URL + "/pc-pseudo-real/english.001.2.7z",
-    DACIT_URL + "/pc-pseudo-real/dblp.xml.00001.1.7z",
-    DACIT_URL + "/pc-pseudo-real/dblp.xml.00001.2.7z",
-    DACIT_URL + "/pc-pseudo-real/dblp.xml.0001.1.7z",
-    DACIT_URL + "/pc-pseudo-real/dblp.xml.0001.2.7z",
-    DACIT_URL + "/pc-pseudo-real/dna.001.1.7z",
-    DACIT_URL + "/pc-pseudo-real/english.001.2.7z",
-    DACIT_URL + "/pc-pseudo-real/proteins.001.1.7z",
-    DACIT_URL + "/pc-pseudo-real/sources.001.2.7z",
-    DACIT_URL + "/pc-real/cere.7z",
-    DACIT_URL + "/pc-real/coreutils.7z",
-    DACIT_URL + "/pc-real/einstein.de.txt.7z",
-    DACIT_URL + "/pc-real/einstein.en.txt.7z",
-    DACIT_URL + "/pc-real/Escherichia_Coli.7z",
-    DACIT_URL + "/pc-real/influenza.7z",
-    DACIT_URL + "/pc-real/kernel.7z",
-    DACIT_URL + "/pc-real/para.7z",
-    DACIT_URL + "/pc-real/world_leaders.7z",
+    TDC_URL + "/pc-pseudo-real/english.001.2.7z",
+    TDC_URL + "/pc-pseudo-real/dblp.xml.00001.1.7z",
+    TDC_URL + "/pc-pseudo-real/dblp.xml.00001.2.7z",
+    TDC_URL + "/pc-pseudo-real/dblp.xml.0001.1.7z",
+    TDC_URL + "/pc-pseudo-real/dblp.xml.0001.2.7z",
+    TDC_URL + "/pc-pseudo-real/dna.001.1.7z",
+    TDC_URL + "/pc-pseudo-real/english.001.2.7z",
+    TDC_URL + "/pc-pseudo-real/proteins.001.1.7z",
+    TDC_URL + "/pc-pseudo-real/sources.001.2.7z",
+    TDC_URL + "/pc-real/cere.7z",
+    TDC_URL + "/pc-real/coreutils.7z",
+    TDC_URL + "/pc-real/einstein.de.txt.7z",
+    TDC_URL + "/pc-real/einstein.en.txt.7z",
+    TDC_URL + "/pc-real/Escherichia_Coli.7z",
+    TDC_URL + "/pc-real/influenza.7z",
+    TDC_URL + "/pc-real/kernel.7z",
+    TDC_URL + "/pc-real/para.7z",
+    TDC_URL + "/pc-real/world_leaders.7z",
     "http://pizzachili.dcc.uchile.cl/repcorpus/artificial/fib41.7z",
     "http://pizzachili.dcc.uchile.cl/repcorpus/artificial/rs.13.7z",
     "http://pizzachili.dcc.uchile.cl/repcorpus/artificial/tm29.7z",
@@ -50,17 +50,17 @@ download_and_extract("pcr", small_sizes, [
 
 # TAGME Datasets, see http://acube.di.unipi.it/datasets/
 download_and_extract("tagme", small_sizes, [
-    DACIT_URL + "/wiki-annot30.gz",
-    DACIT_URL + "/wiki-disamb30.gz",
+    TDC_URL + "/wiki-annot30.gz",
+    TDC_URL + "/wiki-disamb30.gz",
 ])
 
 # TagMyNews Datasets, see http://acube.di.unipi.it/datasets/
 download_and_extract("tagmynews", small_sizes, [
-    DACIT_URL + "/news.gz",
-    DACIT_URL + "/data-web-snippets.tar.gz",
+    TDC_URL + "/news.gz",
+    TDC_URL + "/data-web-snippets.tar.gz",
 ])
 
 # ASCII-only Wikipedia excerpt from the "10.000 most vital articles" project
 download_and_extract("wiki", small_sizes, [
-    DACIT_URL + "/all_vital.txt.gz",
+    TDC_URL + "/all_vital.txt.gz",
 ])
--- etc/large_datasets_config.py
+++ etc/large_datasets_config.py
@@ -1,19 +1,19 @@
 sizes = [1, 10240]
 
-DACIT_URL = "dacit.cs.uni-dortmund.de/datasets"
+TDC_URL = "dolomit.cs.uni-dortmund.de/datasets"
 
 download_and_extract("large", sizes, [
-    DACIT_URL + "/commoncrawl_10240.ascii",
+    TDC_URL + "/commoncrawl_10240.ascii",
 
     # http://acube.di.unipi.it/rlzap-dataset/
     # (array of 32bit values)
-    DACIT_URL + "/rlzap_dlcp_female",
+    TDC_URL + "/rlzap_dlcp_female",
 
     # http://www.internationalgenome.org/data-portal/sample/NA19000
     # (Alphabet: "GTCA", all "N" filtered out)
-    DACIT_URL + "/1000Genome_JPT_NA19000_SRR099528_1.fastq.filtered4_no_N.dna",
+    TDC_URL + "/1000Genome_JPT_NA19000_SRR099528_1.fastq.filtered4_no_N.dna",
 
     # https://panthema.net/2012/1119-eSAIS-Inducing-Suffix-and-LCP-Arrays-in-External-Memory/
     # Gutenberg Concatenation from September 2012
-    DACIT_URL + "/gutenberg-201209.24090588160.xz",
+    TDC_URL + "/gutenberg-201209.24090588160.xz",
 ])
