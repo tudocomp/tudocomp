273aebc312d84ae0f3e3bfaa1cd78a3ccecacc40
Marvin Löbel <loebel.marvin@gmail.com>
2018-02-07 23:41:04 +0100

    Add helper for updating git submoduls

--- cmakemodules/git_submodule_subdirectories.cmake
+++ cmakemodules/git_submodule_subdirectories.cmake
@@ -4,3 +4,12 @@ execute_process(COMMAND git submodule update --init -- ${dep_name}
 
 add_subdirectory(${dep_name})
 endmacro()
+
+# NB: Could add ALL specifier to ensure its up-to-date on each build
+# Maybe add VERBATIM to output?
+add_custom_target(
+    update_submodules
+    COMMAND git submodule update --recursive --remote
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMENT "Updating GIT submodules..."
+)
