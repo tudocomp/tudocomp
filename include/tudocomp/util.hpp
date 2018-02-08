92af1128670d5a9bdd92897f345c14c4e47f5976
Marvin Löbel <loebel.marvin@gmail.com>
2018-02-08 11:21:46 +0100

    move compact_sparse_hash into own repo

--- include/tudocomp/util.hpp
+++ include/tudocomp/util.hpp
@@ -171,30 +171,6 @@ inline bool parse_number_until_other(std::istream& inp, char& last, size_t& out)
     return more;
 }
 
-/// \brief Computes the highest set bit in an integer variable
-inline constexpr uint_fast8_t bits_hi(uint64_t x) {
-	return x == 0 ? 0 : 64 - __builtin_clzll(x);
-}
-
-/// \brief Computes the number of bits required to store the given integer
-/// value.
-///
-/// This is equivalent to the binary logarithm rounded up to the next integer.
-///
-/// Examples:
-/// - `bits_for(0b0) == 1`
-/// - `bits_for(0b1) == 1`
-/// - `bits_for(0b10) == 2`
-/// - `bits_for(0b11) == 2`
-/// - `bits_for(0b100) == 3`
-///
-/// \param n The integer to be stored.
-/// \return The amount of bits required to store the value (guaranteed to be
-/// greater than zero).
-inline constexpr uint_fast8_t bits_for(size_t n) {
-    return n == 0 ? 1U : bits_hi(n);
-}
-
 /// \brief Performs an integer division with the result rounded up to the
 /// next integer.
 ///
