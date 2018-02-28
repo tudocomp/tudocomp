aa8e693c96121ac26f4db36605a599fdd403810d
Patrick Dinklage <pdinklag@gmail.com>
2018-02-28 15:35:43 +0100

    BufferedWrite for even faster writing

--- src/plcp_main.cpp
+++ src/plcp_main.cpp
@@ -12,9 +12,38 @@ namespace tdc { namespace lcpcomp {
     constexpr len_t M = 1024 * 1024;
     using uint40_t = uint_t<40>;
 
-    inline void write_uint40(std::ostream& outs, uint40_t v) {
-        outs.write((const char*)&v, sizeof(uint40_t));
-    }
+    template<typename T>
+    class BufferedWriter {
+        private:
+            std::ostream* m_outs;
+
+            T* m_buf;
+            size_t m_bufsize;
+
+            size_t m_num;
+
+            inline void flush() {
+                m_outs->write((const char*)m_buf, m_num * sizeof(T));
+                m_num = 0;
+            }
+
+        public:
+            inline BufferedWriter(std::ostream& outs, size_t bufsize)
+                : m_outs(&outs), m_bufsize(bufsize), m_num(0) {
+
+                m_buf = new T[bufsize];
+            }
+
+            inline ~BufferedWriter() {
+                flush();
+                delete[] m_buf;
+            }
+
+            inline void write(const T& value) {
+                m_buf[m_num++] = value;
+                if(m_num >= m_bufsize) flush();
+            }
+    };
 
     inline void skip_bytes(std::istream& ins, size_t num) {
         while(num--) {
@@ -55,27 +84,31 @@ namespace tdc { namespace lcpcomp {
             // walk over factors
             size_t num_replaced = 0;
 
-            size_t p = 0; //! current text position
-            for(auto& factor : refs) {
-                // encode literals until cursor reaches factor
-                while(p++ < factor.pos) {
-                    write_uint40(outs, ins.get());
-                    write_uint40(outs, 0);
+            {
+                BufferedWriter<uint40_t> bw(outs, 400);
+
+                size_t p = 0; //! current text position
+                for(auto& factor : refs) {
+                    // encode literals until cursor reaches factor
+                    while(p++ < factor.pos) {
+                        bw.write(ins.get());
+                        bw.write(0);
+                    }
+
+                    // encode factor
+                    bw.write(factor.src);
+                    bw.write(factor.len);
+
+                    p += size_t(factor.len);
+                    skip_bytes(ins, factor.len);
+                    num_replaced += factor.len;
                 }
 
-                // encode factor
-                write_uint40(outs, factor.src);
-                write_uint40(outs, factor.len);
-
-                p += size_t(factor.len);
-                skip_bytes(ins, factor.len);
-                num_replaced += factor.len;
-            }
-
-            while(!ins.eof())  {
-                // encode remaining literals
-                write_uint40(outs, ins.get());
-                write_uint40(outs, 0);
+                while(!ins.eof())  {
+                    // encode remaining literals
+                    bw.write(ins.get());
+                    bw.write(0);
+                }
             }
 
             StatPhase::log("num_replaced", num_replaced);
