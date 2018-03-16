479b6f6e1b9a97b0399acb42f62fa3ace5375000
Jonas Ellert <jonas.ellert@tu-dortmund.de>
2018-03-16 10:55:45 +0100

    Decrease unstructured IOs in PLCP-decomp

--- include/tudocomp/compressors/lcpcomp/decompress/PLCPDecomp.hpp
+++ include/tudocomp/compressors/lcpcomp/decompress/PLCPDecomp.hpp
@@ -16,6 +16,18 @@ namespace tdc { namespace lcpcomp {
 
 
 template <typename unsigned_t>
+struct literal_t {
+ public:
+    unsigned_t copyTo;
+    char character;
+    literal_t() {}        
+    literal_t(unsigned_t copyTo, char character) {
+        this->copyTo = copyTo;
+        this->character = character;
+    }
+};
+
+template <typename unsigned_t>
 struct factor_t {
  public:
     unsigned_t copyFrom, length;
@@ -47,36 +59,40 @@ struct reference_t : public factor_t<unsigned_t> {
     unsigned_t copyToEnd() const {
         return copyTo + factor_t<unsigned_t>::length;
     }
+    
+    template<typename unsigned_other_t>
+    reference_t<unsigned_other_t> convert() {
+        return reference_t<unsigned_other_t>(
+            uint64_t(copyTo), 
+            uint64_t(factor_t<unsigned_t>::copyFrom), 
+            uint64_t(factor_t<unsigned_t>::length));
+    }
 };
 
-template <typename unsigned_t>
+template <unsigned bitsPerUInt, unsigned bitsPerUIntInitial>
 class PLCPDecomp {
     friend class PLCPDecompGenerator;
  public:
+    typedef uint_t<bitsPerUIntInitial> unsigned_initial_t;
+    typedef reference_t<unsigned_initial_t> ref_initial_t;
+    typedef typename stxxl::VECTOR_GENERATOR<ref_initial_t>::result vector_of_ref_initial_t;
+    typedef typename vector_of_ref_initial_t::bufwriter_type vector_of_ref_writer_initial_t;
+    typedef typename vector_of_ref_initial_t::bufreader_type vector_of_ref_reader_initial_t;
+    
+    typedef uint_t<bitsPerUInt> unsigned_t;    
     typedef reference_t<unsigned_t> ref_t;
+    typedef literal_t<unsigned_t> lit_t;
     typedef typename stxxl::VECTOR_GENERATOR<ref_t>::result vector_of_ref_t;
+    typedef typename vector_of_ref_t::bufwriter_type vector_of_ref_writer_t;
+    typedef typename vector_of_ref_t::bufreader_type vector_of_ref_reader_t;
+    typedef typename stxxl::VECTOR_GENERATOR<lit_t>::result vector_of_lit_t;
+    typedef typename vector_of_lit_t::bufwriter_type vector_of_lit_writer_t;
+    typedef typename vector_of_lit_t::bufreader_type vector_of_lit_reader_t;
     typedef typename stxxl::VECTOR_GENERATOR<char>::result vector_of_char_t;
+    typedef typename vector_of_char_t::bufwriter_type vector_of_char_writer_t;
+    typedef typename vector_of_char_t::bufreader_type vector_of_char_reader_t;
     
  private:
- 
-    void print(vector_of_ref_t vector) {
-        for (unsigned i = 0; i < vector.size(); i++)
-        {
-            std::cout << "(" << vector[i].copyTo << ", " << vector[i].copyFrom << ", " << vector[i].length << "), ";
-        }
-        std::cout << std::endl;
-    }
-    
-    void print(vector_of_char_t vector) {
-        for (unsigned i = 0; i < vector.size(); i++)
-        {
-            if(vector[i] == '\0') std::cout << "_";
-            else std::cout << vector[i];
-        }
-        std::cout << std::endl;
-    }
-
-    // Comparison structs for stxxl sort
 
     struct reference_limits_t {
         const unsigned_t min_unsigned = std::numeric_limits<unsigned_t>::min();
@@ -96,107 +112,166 @@ class PLCPDecomp {
         bool operator()(const ref_t &a, const ref_t &b) const 
         { return a.copyFrom < b.copyFrom; }
     } compare_by_copyFrom;
- 
-    len_t bytes_memory;
     
-    vector_of_char_t textBuffer;
+    struct literal_limits_t {
+        const unsigned_t min_unsigned = std::numeric_limits<unsigned_t>::min();
+        const unsigned_t max_unsigned = std::numeric_limits<unsigned_t>::max();
+        const lit_t min_literal = lit_t(min_unsigned, '\0');
+        const lit_t max_literal = lit_t(max_unsigned, '\0');
+        lit_t min_value() const { return literal_limits_t::min_literal; }
+        lit_t max_value() const { return literal_limits_t::max_literal; }
+    };
+    
+    struct compare_by_copyTo_lit_t : literal_limits_t {
+        bool operator()(const lit_t &a, const lit_t &b) const 
+        { return a.copyTo < b.copyTo; }
+    } compare_by_copyTo_lit;
+    
+    vector_of_char_t &textBuffer;
+    
     vector_of_ref_t * byCopyToV;
     vector_of_ref_t * byCopyToV_new;
-    vector_of_ref_t * byCopyFromV;
+    vector_of_ref_writer_t * byCopyToV_writer;
     
+    vector_of_ref_t resolvedV;
+    vector_of_ref_writer_t * resolvedV_writer;
     
-    PLCPDecomp() {
-        byCopyToV = new vector_of_ref_t();
-        byCopyToV_new = new vector_of_ref_t();
-        byCopyFromV = new vector_of_ref_t();
-    }
+    len_t bytes_memory;
     
     ~PLCPDecomp() {
         delete byCopyToV;
         delete byCopyToV_new;
-        delete byCopyFromV;
+        delete byCopyToV_writer;
+        delete resolvedV_writer;
     }
     
     void scan();
-    bool resolve(
-        typename vector_of_ref_t::iterator &fromIter, 
-        typename vector_of_ref_t::const_iterator &toIter);
-    void restore(ref_t &factor);
+    bool resolve(ref_t &fromFactor, const ref_t &toFactor);
+    void restore();
+    
+    void count() {
+        uint64_t count = 0;
+        for (auto c : textBuffer)
+        {
+            if(c != '\0') count++;
+        }
+        std::cout << "Resolved:   " << count << std::endl;
+        uint64_t count2 = 0;
+        for (auto f : *byCopyToV)
+        {
+            count2 += 0 + f.length;
+        }
+        std::cout << "Unresolved: " << count2 << std::endl;
+        std::cout << "All:        " << count + count2 << std::endl;
+    }
     
  public:
+    
+    PLCPDecomp(
+            vector_of_char_t &textBuffer, 
+            vector_of_ref_initial_t &factors, len_t bytes_memory) :
+            textBuffer(textBuffer),
+            bytes_memory(bytes_memory) {
+        
+        byCopyToV = new vector_of_ref_t();
+        byCopyToV_new = new vector_of_ref_t();
+        byCopyToV_writer = new vector_of_ref_writer_t(*byCopyToV_new);
+        resolvedV_writer = new vector_of_ref_writer_t(resolvedV);
+        
+        std::cout 
+            << "Converting references from " 
+            << bitsPerUIntInitial << " bits to " 
+            << bitsPerUInt << " bits..." << std::endl;
+            
+        vector_of_ref_writer_t toWriter(*byCopyToV);
+        for (auto factor : vector_of_ref_reader_initial_t(factors))
+            toWriter << factor.template convert<unsigned_t>();
+    }
+ 
     vector_of_char_t decompress();
 };
 
         
-template <typename unsigned_t>
-typename PLCPDecomp<unsigned_t>::vector_of_char_t PLCPDecomp<unsigned_t>::decompress() {
-
+template <unsigned bitsPerUInt, unsigned bitsPerUIntInitial>
+typename PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::vector_of_char_t PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::decompress() {
     std::cout << "Decompressing. Type: " << typeid(unsigned_t).name() << std::endl;
     unsigned scanCount = 0;
     while(byCopyToV->size() > 0) {
         tdc::StatPhase scanPhase("Scan " + std::to_string(++scanCount) + " (Factors: " + std::to_string(byCopyToV->size()) + ")");
+        std::cout << std::endl;
+        std::cout << "Scan " + std::to_string(scanCount) + " (Factors: " + std::to_string(byCopyToV->size()) + ")" << std::endl;
         scan();
     }
-    //~ print(textBuffer);
-    
     return textBuffer;
 }
 
-template <typename unsigned_t>
-void PLCPDecomp<unsigned_t>::scan() {
-    std::cout << std::endl;
+template <unsigned bitsPerUInt, unsigned bitsPerUIntInitial>
+void PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::scan() {
+    //~ count();
     std::cout << "SCAN..." << std::endl;
+    
     std::cout << "  Copy from CopyToV to CopyFromV..." << std::endl;
-    (*byCopyFromV) = (*byCopyToV);
-    std::cout << "  Sort CopyFromV..." << std::endl;
-    stxxl::sort(byCopyFromV->begin(), byCopyFromV->end(), compare_by_copyFrom, bytes_memory);
-    std::cout << "  Add guard to CopyToV..." << std::endl;
+    vector_of_ref_t byCopyFromV = *byCopyToV;
+
     byCopyToV->push_back(compare_by_copyTo.max_value());
     
-    //~ print(textBuffer);
-    //~ print(*byCopyToV);
-    //~ print(*byCopyFromV);
+    std::cout << "  Sort CopyFromV..." << std::endl;
+    stxxl::sort(byCopyFromV.begin(), byCopyFromV.end(), compare_by_copyFrom, bytes_memory);
+    
     
     std::cout << "  Resolve & Jump..." << std::endl;
-    auto toIter = byCopyToV->cbegin();
-    auto fromIter = byCopyFromV->begin();
-    auto fromEnd = byCopyFromV->cend();
+    auto toIter = byCopyToV->cbegin();    
+    vector_of_ref_reader_t fromReader(byCopyFromV);
     
-    for (; fromIter != fromEnd; fromIter++)
+    //~ uint64_t cmax = 0;
+    for (; !fromReader.empty(); ++fromReader)
     {
-        while((*toIter).copyToEnd() <= (*fromIter).copyFrom)
+        auto fromFactor = *fromReader;
+        while((*toIter).copyToEnd() <= fromFactor.copyFrom)
             toIter++;
         
-        if(!resolve(fromIter, toIter)) {
+        if(!resolve(fromFactor, *toIter)) {
+            //~ uint64_t c = 1;
             auto nextToIter = toIter + 1;
-            while(!resolve(fromIter, nextToIter)) {
+            while(!resolve(fromFactor, *nextToIter)) {
                 nextToIter++;
+                //~ c++;
             }
+            //~ if(c > cmax) {
+                //~ std::cout << c << " " << std::flush;
+                //~ cmax = c;
+            //~ }
         };
     }
+    std::cout << std::endl;
+    byCopyToV_writer->finish();
+    resolvedV_writer->finish();
     
     std::cout << "  Update CopyToV..." << std::endl;
     vector_of_ref_t * swap = byCopyToV;
     byCopyToV = byCopyToV_new;
     byCopyToV_new = swap;
     byCopyToV_new->clear();
+    delete byCopyToV_writer;
+    byCopyToV_writer = new vector_of_ref_writer_t(*byCopyToV_new);
     
     std::cout << "  Sort CopyToV..." << std::endl;
     stxxl::sort(byCopyToV->begin(), byCopyToV->end(), compare_by_copyTo, bytes_memory);
+    
+    restore();
+    resolvedV.clear();
+    delete resolvedV_writer;
+    resolvedV_writer = new vector_of_ref_writer_t(resolvedV);
+    
+    //~ count();
 }
 
-template <typename unsigned_t>
-bool PLCPDecomp<unsigned_t>::resolve(
-        typename vector_of_ref_t::iterator &fromIter, 
-        typename vector_of_ref_t::const_iterator &toIter) {
-    
-    auto const &toFactor = (*toIter);
-    auto &fromFactor = (*fromIter);
+template <unsigned bitsPerUInt, unsigned bitsPerUIntInitial>
+bool PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::resolve(ref_t &fromFactor, const ref_t &toFactor) {
     
     //~ std::cout << "(" << fromFactor.copyTo << ", " << fromFactor.copyFrom << ", " << fromFactor.length << "), ";
     //~ std::cout << "(" << toFactor.copyTo << ", " << toFactor.copyFrom << ", " << toFactor.length << ")";
     //~ std::cout << "   ";
-    
     
     bool wasResolve = false;
     unsigned_t prefixLen;    
@@ -205,14 +280,15 @@ bool PLCPDecomp<unsigned_t>::resolve(
         prefixLen = std::min(fromFactor.length, unsigned_t(toFactor.copyToEnd() - fromFactor.copyFrom));
         auto jumpOffset = fromFactor.copyFrom - toFactor.copyTo;
         ref_t jumped(fromFactor.copyTo, toFactor.copyFrom + jumpOffset, prefixLen);
-        byCopyToV_new->push_back(jumped);
+        (*byCopyToV_writer) << jumped;
         //~ std::cout << "JUMPED:  (" << jumped.copyTo << ", " << jumped.copyFrom << ", " << jumped.length << ")";
         //~ std::cout << "   ";
     } else {
         // prefix of fromFactor can be resolved
         prefixLen = std::min(fromFactor.length, unsigned_t(toFactor.copyTo - fromFactor.copyFrom));
         ref_t resolved(fromFactor.copyTo, fromFactor.copyFrom, prefixLen);
-        restore(resolved);
+        //restore(resolved);
+        (*resolvedV_writer) << resolved;
         wasResolve = true;
         //~ std::cout << "RESOLVED:  (" << resolved.copyTo << ", " << resolved.copyFrom << ", " << resolved.length << ")";
         //~ std::cout << "   ";
@@ -226,20 +302,35 @@ bool PLCPDecomp<unsigned_t>::resolve(
         fromFactor.copyFrom += prefixLen;
         fromFactor.length -= prefixLen;
         //~ std::cout << "REMAINS: (" << fromFactor.copyTo << ", " << fromFactor.copyFrom << ", " << fromFactor.length << ")" << std::endl;
-        if(wasResolve) return resolve(fromIter, toIter);
+        if(wasResolve) return resolve(fromFactor, toFactor);
         else return false;
     }    
 }
 
-template <typename unsigned_t>
-void PLCPDecomp<unsigned_t>::restore(ref_t &reference) {
-    auto fromIter = textBuffer.cbegin() + reference.copyFrom - 1;
-    auto toIter = textBuffer.begin() + reference.copyTo - 1;
+template <unsigned bitsPerUInt, unsigned bitsPerUIntInitial>
+void PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::restore() {
+    std::cout << "  Sort ResolvedV by copyFrom..." << std::endl;
+    stxxl::sort(resolvedV.begin(), resolvedV.end(), compare_by_copyFrom, bytes_memory);
     
-    for (unsigned_t i = 0; i < reference.length; i++)
-    {
-        (*toIter++) = (*fromIter++);
-    }    
+    vector_of_lit_t resolvedLiteralsV;
+    vector_of_lit_writer_t literalWriter(resolvedLiteralsV);
+    
+    for (auto factor : resolvedV) {
+        auto textIt = textBuffer.cbegin() + factor.copyFrom - 1;
+        for (unsigned_t i = 0; i < factor.length; i++)
+        {
+            literalWriter << lit_t(factor.copyTo + i, *textIt);
+            textIt++;
+        } 
+    }
+    literalWriter.finish();
+    
+    std::cout << "  Sort ResolvedLiteralsV by copyTo..." << std::endl;
+    stxxl::sort(resolvedLiteralsV.begin(), resolvedLiteralsV.end(), compare_by_copyTo_lit, bytes_memory);
+    
+    for (auto literal : resolvedLiteralsV) {
+        textBuffer[literal.copyTo - 1] = literal.character;
+    }
 }
 
 }} //ns
--- include/tudocomp/compressors/lcpcomp/decompress/PLCPDecompGenerator.hpp
+++ include/tudocomp/compressors/lcpcomp/decompress/PLCPDecompGenerator.hpp
@@ -14,11 +14,6 @@
 #include <stxxl/bits/io/iostats.h>
 
 namespace tdc { namespace lcpcomp {
-    
-struct PLCPDecompSpecs {
-    len_t mbMem;
-    uint_t<40> nFactors, nLiterals, nReferences, bitsPerInt;
-};
 
 class PLCPDecompGenerator {
  public:
@@ -27,56 +22,70 @@ class PLCPDecompGenerator {
     template<typename unsigned_t>
     inline static unsigned_t swapBytes(uint_t<40> value);
     
-    template<typename unsigned_t>
+    template<unsigned bitsPerUInt, unsigned bitsPerUIntInitial>
     static stxxl_vector_of_char_t decompressInternal(
-        IntegerFileArray<uint_t<40>> &factors, 
-        PLCPDecompSpecs specs);
+        typename PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::vector_of_char_t &textBuffer,
+        typename PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::vector_of_ref_initial_t &factors, 
+        len_t bytes_memory);
     
  public:
- 
-    static PLCPDecompSpecs getSpecs(
-        IntegerFileArray<uint_t<40>> &factors, 
-        len_t megabytes_memory);
         
     static stxxl_vector_of_char_t decompress(
         IntegerFileArray<uint_t<40>> &factors, 
         len_t megabytes_memory);
 };
 
-
-
-PLCPDecompSpecs PLCPDecompGenerator::getSpecs(IntegerFileArray<uint_t<40>> &factors, len_t megabytes_memory){
-
-    std::cout << "Calculating PLCP decomp specifications. Memory for STXXL (in MB): " << megabytes_memory << std::endl;
-
-    PLCPDecompSpecs result;
-    result.mbMem = megabytes_memory;
-    result.nFactors = factors.size() / 2;
-    result.nReferences = 0;
-    result.nLiterals = 0;
-
-    // count literals and number of characters contained in references
-    for (uint_t<40> i = 0; i < result.nFactors; i++) {
-        uint_t<40> len = swapBytes<uint_t<40>>(factors[2 * i + 1]);
-        if(len == uint_t<40>(0)) result.nLiterals += 1;
-        else result.nReferences += len;
-    }
-    
-    result.bitsPerInt = 1 + (int) std::log2((uint64_t)(result.nReferences + result.nLiterals + 1));
-    
-    std::cout << "Factors:              " << result.nFactors << std::endl;
-    std::cout << "Literal characters:   " << result.nLiterals << std::endl;
-    std::cout << "Reference characters: " << result.nReferences << std::endl;
-    std::cout << "Total characters:     " << result.nReferences + result.nLiterals << std::endl;
-    std::cout << "Bits per integer:     " << result.bitsPerInt << std::endl;
-
-    return result;
-}
-
 PLCPDecompGenerator::stxxl_vector_of_char_t PLCPDecompGenerator::decompress
         (IntegerFileArray<uint_t<40>> &factors, len_t megabytes_memory){
+    
+    using compressor_t = PLCPDecomp<40, 40>;
+    
+    std::cout << "Preparing decompression..." << std::endl;
 
-    PLCPDecompSpecs specs = getSpecs(factors, megabytes_memory);
+    len_t nBytesMemory = megabytes_memory * 1000 * 1000;
+    uint_t<40> nFactors = factors.size() / 2;
+    uint_t<40> nReferences = 0;
+    uint_t<40> nLiterals = 0;
+    
+    compressor_t::vector_of_ref_initial_t factorsForCompressor;
+    compressor_t::vector_of_ref_writer_initial_t factorWriter(factorsForCompressor);
+    
+    compressor_t::vector_of_char_t textBuffer;
+    compressor_t::vector_of_char_writer_t textWriter(textBuffer);
+
+    std::cout << "    Filling initial vectors..." << std::endl;
+
+    uint_t<40> textPosition = 1;
+    for (uint_t<40> i = 0; i < nFactors; i++) {
+       
+        //~ uint_t<40> target(swapBytes<uint_t<40>>(factors[2 * i]) + 1);
+        //~ uint_t<40> len(swapBytes<uint_t<40>>(factors[2 * i + 1]));
+        uint_t<40> target(factors[i * 2] + 1);
+        uint_t<40> len(factors[i * 2 + 1]);
+        
+        if(len == uint_t<40>(0)) {
+            textWriter << char(target - 1);
+            textPosition++;
+            nLiterals++;
+        } 
+        else {
+            factorWriter << compressor_t::ref_initial_t(textPosition, target, len);
+            for (uint_t<40> j = 0; j < len; j++) textWriter << '\0';
+            textPosition += len;
+            nReferences += len;
+        }
+    }
+    factorWriter.finish();
+    textWriter.finish();
+    
+    unsigned bitsPerInt = 1 + (int) std::log2((uint64_t)(nReferences + nLiterals + 1));
+    
+    std::cout << "    Factors:                " << nFactors << std::endl;
+    std::cout << "    Literal characters:     " << nLiterals << std::endl;
+    std::cout << "    Reference characters:   " << nReferences << std::endl;
+    std::cout << "    Total characters:       " << nReferences + nLiterals << std::endl;
+    std::cout << "    Bits per integer:       " << bitsPerInt << std::endl;
+    std::cout << "    Allowed memory (bytes): " << nBytesMemory << std::endl;
     
     //~ if(specs.bitsPerInt <= 4) return decompressInternal<uint_t<4>>(factors, specs);
     //~ else if(specs.bitsPerInt <= 8) return decompressInternal<uint_t<8>>(factors, specs);
@@ -87,52 +96,17 @@ PLCPDecompGenerator::stxxl_vector_of_char_t PLCPDecompGenerator::decompress
     //~ else if(specs.bitsPerInt <= 28) return decompressInternal<uint_t<28>>(factors, specs);
     //~ else if(specs.bitsPerInt <= 32) return decompressInternal<uint_t<32>>(factors, specs);
     //~ else if(specs.bitsPerInt <= 36) return decompressInternal<uint_t<36>>(factors, specs);
-    /*else /*if(specs.bitsPerInt <= 40)*/ return decompressInternal<uint_t<40>>(factors, specs);
+    /*else /*if(specs.bitsPerInt <= 40)*/ return decompressInternal<34, 40>(textBuffer, factorsForCompressor, nBytesMemory);
 }
 
-template<typename unsigned_t>
-PLCPDecompGenerator::stxxl_vector_of_char_t PLCPDecompGenerator::decompressInternal
-        (IntegerFileArray<uint_t<40>> &factors, PLCPDecompSpecs specs){
+template<unsigned bitsPerUInt, unsigned bitsPerUIntInitial>
+PLCPDecompGenerator::stxxl_vector_of_char_t PLCPDecompGenerator::decompressInternal(
+        typename PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::vector_of_char_t &textBuffer,
+        typename PLCPDecomp<bitsPerUInt, bitsPerUIntInitial>::vector_of_ref_initial_t &factors, 
+        len_t bytes_memory) {
 
-    std::cout << "\nStarting decompression..." << std::endl;
-    
-    PLCPDecomp<unsigned_t> decompressor;
-    
-    std::cout << "Reserving space for vectors..." << std::endl;
-    decompressor.byCopyToV->reserve(specs.nReferences);
-    decompressor.byCopyToV_new->reserve(specs.nReferences);
-    decompressor.byCopyFromV->reserve(specs.nReferences);
-    decompressor.textBuffer.resize(specs.nReferences + specs.nLiterals);
-    
-    decompressor.bytes_memory = specs.mbMem * 1024 * 1024;
-    
-    std::cout << "Parsing unresolved factors and literals..." << std::endl;
-    std::cout << "    (fill vector of unresolved factors in copyTo order)" << std::endl;
-    std::cout << "    (fill vector of restored characters with literals)" << std::endl;
-    
-    // everything here is 1-based
-    // (necessary for stxxl sort)
-    unsigned_t textPosition = 1;
-    for (uint_t<40> i = 0; i < specs.nFactors; i++) {
-        // read current factor
-        unsigned_t target(swapBytes<unsigned_t>(factors[i * 2]) + 1);
-        unsigned_t len(swapBytes<unsigned_t>(factors[i * 2 + 1]));
-        if(len == unsigned_t(0)) {
-            // restore literal factors immediatly
-            // (naturally in textpos order -> single scan)
-            decompressor.textBuffer[textPosition - 1] = char(target - 1);
-            textPosition++;
-        } 
-        else {
-            // fill one of the factor vectors
-            // (other one will get copied)
-            auto reference = reference_t<unsigned_t>(textPosition, target, len);
-            decompressor.byCopyToV->push_back(reference);
-            textPosition += len;
-        }
-    }
-    
-    return decompressor.decompress();
+    PLCPDecomp<bitsPerUInt, bitsPerUIntInitial> compressor(textBuffer, factors, bytes_memory);
+    return compressor.decompress();
 }
 
 template<typename unsigned_t>
