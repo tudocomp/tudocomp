2c12a59dc16abf7ea5af29269d56b3c619076e98
Patrick Dinklage <pdinklag@gmail.com>
2018-03-27 12:44:44 +0200

    Verify factor set

--- src/plcp_main.cpp
+++ src/plcp_main.cpp
@@ -1,5 +1,8 @@
 #include <algorithm>
+#include <forward_list>
 #include <iostream>
+#include <unordered_map>
+
 #include <tudocomp_stat/StatPhase.hpp>
 #include <tudocomp/CreateAlgorithm.hpp>
 #include <tudocomp/compressors/lcpcomp/compress/PLCPStrategy.hpp>
@@ -11,6 +14,68 @@
 #include <stxxl/bits/io/syscall_file.h>
 
 namespace tdc { namespace lcpcomp {
+    template<typename node_t>
+    class ReferenceGraph {
+    private:
+        std::vector<node_t> m_path;
+
+        std::unordered_map<node_t, node_t> m_edges;
+        size_t m_longest_path;
+        size_t m_shortcuts;
+
+        inline node_t follow_path(const node_t start, const node_t avoid) {
+            auto v = start;
+
+            // follow and store path
+            auto it = m_edges.find(v);
+            while(it != m_edges.end()) {
+                m_path.emplace_back(v);
+
+                v = it->second;
+                CHECK(v != start) << "cycle detected";
+                CHECK(v != avoid) << "cycle detected";
+
+                it = m_edges.find(v);
+            }
+
+            if(m_path.size() > 1) {
+                // create shortcuts
+                m_shortcuts += m_path.size() - 1;
+                for(auto u : m_path) {
+                    m_edges[u] = v;
+                }
+            }
+
+            m_longest_path = std::max(m_longest_path, m_path.size());
+            m_path.clear(); // clear, but keep allocated
+
+            return v;
+        }
+
+    public:
+        inline ReferenceGraph() : m_longest_path(0), m_shortcuts(0) {
+        }
+
+        inline size_t longest_path() const {
+            return m_longest_path;
+        }
+
+        inline size_t num_shortcuts() const {
+            return m_shortcuts;
+        }
+
+        inline size_t num_edges() const {
+            return m_edges.size();
+        }
+
+        // insert an edge, throw error if a cycle is detected or if u already
+        // has an outgoing edge
+        inline void insert_edge(node_t u, node_t v) {
+            CHECK(m_edges.find(u) == m_edges.end()) << "duplicate edge detected";
+            m_edges[u] = follow_path(v, u);
+        }
+    };
+
     constexpr len_t M = 1024 * 1024;
     using uint40_t = uint_t<40>;
 
@@ -86,6 +151,40 @@ namespace tdc { namespace lcpcomp {
 			refStrategy.factorize(refs);
             StatPhase::log("num_factors", refs.size());
     	});
+
+        LOG(INFO) << "Start verification... factors: " << refs.size();
+        StatPhase::wrap("Verify", [&]{
+            ReferenceGraph<size_t> g;
+            const size_t n = sa.size();
+            size_t num_replaced = 0;
+            size_t next_unreplaced = 0;
+
+            decltype(refs)::backing_vector_type::bufreader_type reader(refs.factors);
+            for(auto& factor : reader) {
+                const size_t fpos = factor.pos;
+                const size_t fsrc = factor.src;
+                const size_t flen = factor.len;
+                num_replaced += flen;
+
+                CHECK_LT(num_replaced, n) << "more symbols replaced than input has symbols";
+                CHECK_GE(fpos, next_unreplaced) << "factors are interleaving";
+                CHECK_LT(fsrc, n) << "factor start out of bounds";
+                CHECK_LT(fsrc + flen - 1, n) << "factor end out of bounds";
+
+                next_unreplaced = fpos + flen;
+
+                for(size_t k = 0; k < flen; k++) {
+                    g.insert_edge(fpos + k, fsrc + k);
+                }
+
+
+            }
+
+            StatPhase::log("longest_path", g.longest_path());
+            StatPhase::log("num_edges", g.num_edges());
+            StatPhase::log("num_shortcuts", g.num_shortcuts());
+        });
+        LOG(INFO) << "Done!";
 
         StatPhase::wrap("Encode Factors", [&]{
             // Open input file
