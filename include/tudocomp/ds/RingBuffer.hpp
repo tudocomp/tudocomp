b78b0e9ae9dcb427fb3cab330050d099ec81fd89
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-04-16 09:28:35 +0200

    RingBuffer: Fixes and additional functionality

--- include/tudocomp/ds/RingBuffer.hpp
+++ include/tudocomp/ds/RingBuffer.hpp
@@ -22,7 +22,7 @@ namespace tdc {
         // simulates "i++", but also applies the modulo for the buffer size
         inline size_t post_inc(size_t& i) const {
             const size_t r = i;
-            i = (i+1) % m_buffer.size();
+            i = (i+1) % capacity();
             return r;
         }
 
@@ -69,9 +69,25 @@ namespace tdc {
             }
         }
 
+        /// \brief Gets the item at the start of the buffer without removing.
+        inline T peek_front() const {
+            if(tdc_unlikely(m_empty)) {
+                // fail if empty
+                throw std::runtime_error(
+                    "attempt to pop from an empty ring buffer");
+            } else {
+                return m_buffer[m_start];
+            }
+        }
+
         /// \brief Tells whether the buffer is empty.
         inline bool empty() const {
             return m_empty;
+        }
+
+        /// \brief Tells whether the ring buffer is filled.
+        inline bool full() const {
+            return !m_empty && (m_start == m_end);
         }
 
         /// \brief Gets the ring buffer's capacity.
@@ -81,25 +97,46 @@ namespace tdc {
 
         /// \brief Gets the ring buffer's current amount of entries.
         inline size_t size() const {
+            //TODO test
             if(m_start == m_end) {
                 return m_empty ? 0 : capacity();
             } else if(m_start < m_end) {
                 return m_end - m_start;
             } else { // m_first > m_last
-                return (capacity() - m_start + 1) + m_end; //TODO test
+                return (capacity() - m_start + 1) + m_end;
             }
         }
 
         /// \brief Constant access iterator.
         class const_iterator {
-        protected:
+        private:
             const RingBuffer* m_rb;
             size_t m_pos;
             bool m_end;
 
+            inline const_iterator(
+                const RingBuffer& rb, size_t pos, bool end = false)
+                : m_rb(&rb), m_pos(pos), m_end(end || rb.m_empty)
+            {
+            }
+
+            inline size_t offset() const {
+                //TODO test
+                if(m_end) {
+                    return m_rb->size(); // pointing beyond end
+                } else {
+                    // calculate the current actual offset from the start
+                    if(m_pos >= m_rb->m_start) {
+                        return m_pos - m_rb->m_start;
+                    } else { // m_pos < m_rb->m_start
+                        return (m_rb->capacity() - m_rb->m_start + 1) + m_pos;
+                    }
+                }
+            }
+
         public:
-            inline const_iterator(const RingBuffer& rb, bool end = false) :
-                m_rb(&rb), m_pos(rb.m_start), m_end(end || rb.m_empty)
+            inline const_iterator(const RingBuffer& rb, bool end = false)
+                : const_iterator(rb, rb.m_start, end || rb.m_empty)
             {
             }
 
@@ -116,9 +153,7 @@ namespace tdc {
             }
 
             bool operator==(const_iterator other) const {
-                return m_rb == other.m_rb && (
-                            m_end == other.m_end ||
-                            m_pos == other.m_pos);
+                return m_rb == other.m_rb && offset() == other.offset();
             }
 
             bool operator!=(const_iterator other) const {
@@ -131,6 +166,12 @@ namespace tdc {
                 } else {
                     return m_rb->m_buffer[m_pos];
                 }
+            }
+
+            const_iterator operator+(size_t i) const {
+                auto pos = (m_pos + i) % m_rb->capacity();
+                return const_iterator(
+                    *m_rb, pos, (offset() + i) >= m_rb->size());
             }
         };
 
--- test/ringbuffer_tests.cpp
+++ test/ringbuffer_tests.cpp
@@ -11,6 +11,7 @@ TEST(ringbuffer, init) {
     RingBuffer<size_t> r(N);
 
     ASSERT_TRUE(r.empty());
+    ASSERT_FALSE(r.full());
     ASSERT_EQ(0, r.size());
     ASSERT_EQ(N, r.capacity());
     ASSERT_EQ(r.begin(), r.end());
@@ -21,15 +22,18 @@ TEST(ringbuffer, push) {
     RingBuffer<size_t> r(N);
 
     for(size_t i = 0; i < N; i++) {
+        ASSERT_FALSE(r.full());
         r.push_back(i);
         ASSERT_EQ(i+1, r.size());
         ASSERT_FALSE(r.empty());
     }
+    ASSERT_TRUE(r.full());
 }
 
 TEST(ringbuffer, pop) {
     RingBuffer<size_t> r(N);
     for(size_t i = 0; i < N; i++) r.push_back(i);
+    ASSERT_TRUE(r.full());
     for(size_t i = 0; i < N; i++) ASSERT_EQ(i, r.pop_front());
     ASSERT_TRUE(r.empty());
 }
@@ -49,6 +53,13 @@ TEST(ringbuffer, iterator) {
     
     size_t k = 0;
     for(auto x : r) ASSERT_EQ(k++, x);
+}
+
+TEST(ringbuffer, iterator_offset) {
+    RingBuffer<size_t> r(N);
+    for(size_t i = 0; i < N; i++) r.push_back(i);
+    for(size_t k = 0; k < N; k++) ASSERT_EQ(k, *(r.begin() + k));
+    ASSERT_EQ(r.end(), r.begin() + r.size());
 }
 
 TEST(ringbuffer, circular) {
