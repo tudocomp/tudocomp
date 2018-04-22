54ddd9f9a140c463c338f6b98344920494ae0fb8
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2018-04-22 13:35:42 +0200

    Merge StreamingCoder parts into one file

--- include/tudocomp/compressors/lzss/StreamingCoder.hpp
+++ include/tudocomp/compressors/lzss/StreamingCoder.hpp
@@ -2,9 +2,6 @@
 
 #include <tudocomp/compressors/lzss/LZSSCoder.hpp>
 
-#include <tudocomp/compressors/lzss/StreamingEncoder.hpp>
-#include <tudocomp/compressors/lzss/StreamingDecoder.hpp>
-
 namespace tdc {
 namespace lzss {
 
@@ -26,13 +23,98 @@ public:
 
     using super_t::LZSSCoder;
 
+    template<typename refc_t, typename lenc_t, typename litc_t>
+    class Encoder {
+    protected:
+        std::unique_ptr<refc_t> m_refc;
+        std::unique_ptr<lenc_t> m_lenc;
+        std::unique_ptr<litc_t> m_litc;
+        Range m_flen_r;
+
+    public:
+        /// \brief Constructor.
+        inline Encoder(
+            std::unique_ptr<refc_t>&& refc,
+            std::unique_ptr<lenc_t>&& lenc,
+            std::unique_ptr<litc_t>&& litc,
+            Range flen_r = TypeRange<len_compact_t>())
+            : m_refc(std::move(refc)),
+              m_lenc(std::move(lenc)),
+              m_litc(std::move(litc)),
+              m_flen_r(flen_r)
+        {
+        }
+
+        inline void encode_factor(Factor f) {
+            if(tdc_unlikely(f.pos < f.src)) {
+                throw std::runtime_error(
+                    "forward references are not allowed");
+            }
+
+            m_litc->encode(true, bit_r); // 1-bit to indicate factor
+            m_litc->flush(); // context switch
+            m_refc->encode(f.pos - f.src, Range(1, f.pos)); // delta
+            m_refc->flush(); // context switch
+            m_lenc->encode(f.len, m_flen_r);
+            m_lenc->flush(); // context switch
+        }
+
+        inline void encode_literal(uliteral_t c) {
+            m_litc->encode(false, bit_r); // 0-bit to indicate literal
+            m_litc->encode(c, literal_r);
+            // no context switch here - make good use of potential runs!
+        }
+    };
+
+    template<typename refd_t, typename lend_t, typename litd_t>
+    class Decoder {
+    protected:
+        std::unique_ptr<refd_t> m_refd;
+        std::unique_ptr<lend_t> m_lend;
+        std::unique_ptr<litd_t> m_litd;
+        Range m_flen_r;
+
+    public:
+        /// \brief Constructor.
+        inline Decoder(
+            std::unique_ptr<refd_t>&& refd,
+            std::unique_ptr<lend_t>&& lend,
+            std::unique_ptr<litd_t>&& litd,
+            Range flen_r = TypeRange<len_compact_t>())
+            : m_refd(std::move(refd)),
+              m_lend(std::move(lend)),
+              m_litd(std::move(litd)),
+              m_flen_r(flen_r)
+        {
+        }
+
+        template<typename decomp_t>
+        inline void decode(decomp_t& decomp) {
+            size_t p = 0;
+            while(!m_litd->eof()) {
+                auto is_factor = m_litd->template decode<bool>(bit_r);
+                if(is_factor) {
+                    size_t fsrc = p - m_refd->template decode<size_t>(Range(1, p));
+                    size_t flen = m_lend->template decode<size_t>(m_flen_r);
+
+                    decomp.decode_factor(fsrc, flen);
+                    p += flen;
+                } else {
+                    auto c = m_litd->template decode<uliteral_t>(literal_r);
+                    decomp.decode_literal(c);
+                    ++p;
+                }
+            }
+        }
+    };
+
     template<typename literals_t>
     inline auto encoder(Output& output, literals_t&& literals, Range len_r) {
-        return super_t::template encoder<StreamingEncoder>(output, std::move(literals), len_r);
+        return super_t::template encoder<Encoder>(output, std::move(literals), len_r);
     }
 
     inline auto decoder(Input& input, Range len_r) {
-        return super_t::template decoder<StreamingDecoder>(input, len_r);
+        return super_t::template decoder<Decoder>(input, len_r);
     }
 };
 
