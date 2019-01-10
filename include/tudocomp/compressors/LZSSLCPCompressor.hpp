1428a12d8f179a7a14204e15722c95463cac2f24
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2019-01-10 12:51:40 +0100

    Port LZSSLCPCompressor to DSManager

--- include/tudocomp/compressors/LZSSLCPCompressor.hpp
+++ include/tudocomp/compressors/LZSSLCPCompressor.hpp
@@ -15,7 +15,12 @@
 
 #include <tudocomp/decompressors/LZSSDecompressor.hpp>
 
-#include <tudocomp/ds/TextDS.hpp>
+#include <tudocomp/ds/DSManager.hpp>
+#include <tudocomp/ds/providers/DivSufSort.hpp>
+#include <tudocomp/ds/providers/ISAFromSA.hpp>
+#include <tudocomp/ds/providers/PhiAlgorithm.hpp>
+#include <tudocomp/ds/providers/PhiFromSA.hpp>
+#include <tudocomp/ds/providers/LCPFromPLCP.hpp>
 
 #include <tudocomp_stat/StatPhase.hpp>
 
@@ -23,7 +28,7 @@ namespace tdc {
 
 /// Computes the LZ77 factorization of the input using its suffix array and
 /// LCP table.
-template<typename lzss_coder_t, typename text_t = TextDS<>>
+template<typename lzss_coder_t, typename ds_t = DSManager<DivSufSort, PhiFromSA, PhiAlgorithm, LCPFromPLCP, ISAFromSA>>
 class LZSSLCPCompressor : public Compressor {
 public:
     inline static Meta meta() {
@@ -32,10 +37,10 @@ public:
             "suffix and LCP array.");
         m.param("coder", "The output encoder.")
             .strategy<lzss_coder_t>(TypeDesc("lzss_coder"));
-        m.param("textds", "The text data structure provider.")
-            .strategy<text_t>(TypeDesc("textds"), Meta::Default<TextDS<>>());
+        m.param("ds", "The text data structure provider.")
+            .strategy<ds_t>(ds::type(), Meta::Default<DSManager<DivSufSort, PhiFromSA, PhiAlgorithm, LCPFromPLCP, ISAFromSA>>());
         m.param("threshold", "The minimum factor length.").primitive(2);
-        m.inherit_tag<text_t>(tags::require_sentinel);
+        m.inherit_tag<ds_t>(tags::require_sentinel);
         m.inherit_tag<lzss_coder_t>(tags::lossy);
         return m;
     }
@@ -46,17 +51,20 @@ public:
         auto view = input.as_view();
 
         // Construct text data structures
-        text_t text = StatPhase::wrap("Construct Text DS", [&]{
-            return text_t(config().sub_config("textds"), view,
-                    text_t::SA | text_t::ISA | text_t::LCP);
+        ds_t ds(config().sub_config("ds"), view);
+        StatPhase::wrap("Construct Text DS", [&]{
+            ds.template construct<
+                ds::SUFFIX_ARRAY,
+                ds::LCP_ARRAY,
+                ds::INVERSE_SUFFIX_ARRAY>();
         });
 
-        auto& sa = text.require_sa();
-        auto& isa = text.require_isa();
-        auto& lcp = text.require_lcp();
+        auto& sa = ds.template get<ds::SUFFIX_ARRAY>();
+        auto& isa = ds.template get<ds::INVERSE_SUFFIX_ARRAY>();
+        auto& lcp = ds.template get<ds::LCP_ARRAY>();
 
         // Factorize
-        const len_t text_length = text.size();
+        const len_t text_length = view.size();
         lzss::FactorBufferRAM factors;
 
         StatPhase::wrap("Factorize", [&]{
@@ -115,16 +123,16 @@ public:
 
         // statistics
         IF_STATS({
-            lzss::FactorizationStats stats(factors, text.size());
+            lzss::FactorizationStats stats(factors, view.size());
             stats.log();
         })
 
         // encode
         StatPhase::wrap("Encode", [&]{
             auto coder = lzss_coder_t(config().sub_config("coder")).encoder(
-                output, lzss::UnreplacedLiterals<text_t, decltype(factors)>(text, factors));
+                output, lzss::UnreplacedLiterals<decltype(view), decltype(factors)>(view, factors));
 
-            coder.encode_text(text, factors);
+            coder.encode_text(view, factors);
         });
     }
 
