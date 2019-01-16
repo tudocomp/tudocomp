26bdc0d9c0772af244453a7254a8547404140751
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2019-01-16 13:12:44 +0100

    Port lcpsada tests to DSManager and fix test order

--- test/lcpsada_tests.cpp
+++ test/lcpsada_tests.cpp
@@ -6,26 +6,37 @@
 #include <sdsl/int_vector.hpp>
 
 #include <tudocomp/io.hpp>
-#include <tudocomp/ds/TextDS.hpp>
 #include <tudocomp/ds/uint_t.hpp>
 #include <tudocomp/ds/bwt.hpp>
 #include "test/util.hpp"
 #include <tudocomp/ds/LCPSada.hpp>
 
+#include <tudocomp/ds/DSManager.hpp>
+#include <tudocomp/ds/providers/DivSufSort.hpp>
+#include <tudocomp/ds/providers/PhiFromSA.hpp>
+#include <tudocomp/ds/providers/PhiAlgorithm.hpp>
+#include <tudocomp/ds/providers/LCPFromPLCP.hpp>
+
 using namespace tdc;
 using io::InputView;
 
+template<typename textds_t>
+void test_lcpsada(textds_t& t) {
+    t.template construct<
+        ds::SUFFIX_ARRAY,
+        ds::PHI_ARRAY,
+        ds::PLCP_ARRAY,
+        ds::LCP_ARRAY>();
 
-void test_lcpsada(TextDS<>& t) {
-	auto& sa = t.require_sa();
-	typedef std::remove_reference<decltype(sa)>::type sa_t;
+	auto& sa = t.template get<ds::SUFFIX_ARRAY>();
+	typedef typename std::remove_reference<decltype(sa)>::type sa_t;
 	typedef DynamicIntVector phi_t;
 	phi_t phi { construct_phi_array<phi_t,sa_t>(sa) };
-	test::assert_eq_sequence(phi, t.require_phi());
-	auto lcp = construct_lcp_sada(sa, t);
-	test::assert_eq_sequence(lcp, t.require_lcp());
-	LCPForwardIterator plcp { construct_plcp_bitvector(sa, t) };
-	auto& plcp_ds = t.require_plcp();
+	test::assert_eq_sequence(phi, t.template get<ds::PHI_ARRAY>());
+	auto lcp = construct_lcp_sada(sa, t.input);
+	test::assert_eq_sequence(lcp, t.template get<ds::LCP_ARRAY>());
+	LCPForwardIterator plcp { construct_plcp_bitvector(sa, t.input) };
+	auto& plcp_ds = t.template get<ds::PLCP_ARRAY>();
 	if(sa.size() > 1)
     for (size_t i = 0; i < sa.size()-1; i++) {
 		DCHECK_EQ(plcp.index(),i);
@@ -52,9 +63,10 @@ class RunTestDS {
 	}
 };
 
+using textds_t = DSManager<DivSufSort, PhiFromSA, PhiAlgorithm, LCPFromPLCP>;
 
 #define TEST_DS_STRINGCOLLECTION(func) \
-	RunTestDS<TextDS<>> runner(func); \
+	RunTestDS<textds_t> runner(func); \
 	test::roundtrip_batch(runner); \
 	test::on_string_generators(runner,14);
 TEST(ds, lcpsada)          { TEST_DS_STRINGCOLLECTION(test_lcpsada); }
