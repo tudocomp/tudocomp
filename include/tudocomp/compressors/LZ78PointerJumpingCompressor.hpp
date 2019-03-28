283da5c2bbaf31e5d5f119ec327d79752ffed16b
Marvin Löbel <marvin.loebel@tu-dortmund.de>
2019-03-28 12:07:30 +0100

    move FixedBufferPointerJumping to own file

--- include/tudocomp/compressors/LZ78PointerJumpingCompressor.hpp
+++ include/tudocomp/compressors/LZ78PointerJumpingCompressor.hpp
@@ -16,142 +16,9 @@
 #include <tudocomp/compressors/lz_trie/TernaryTrie.hpp>
 #include <tudocomp/coders/BinaryCoder.hpp>
 
+#include <tudocomp/compressors/lz_pointer_jumping/FixedBufferPointerJumping.hpp>
+
 namespace tdc {
-
-template<typename node_t, typename value_type>
-class FixedBufferPointerJumping {
-public:
-    static const size_t MAX_JUMP_WIDTH = 17;
-private:
-
-    struct alignas(uint64_t) SmallFixedString {
-        std::array<uliteral_t, MAX_JUMP_WIDTH> m_data;
-        lz_trie::factorid_t m_node;
-
-        inline SmallFixedString(uliteral_t const* ptr,
-                                size_t size,
-                                lz_trie::factorid_t node) {
-            DCHECK_LE(size, m_data.size());
-            for (size_t i = 0; i < size; i++) {
-                m_data[i] = ptr[i];
-            }
-            for (size_t i = size; i < m_data.size(); i++) {
-                m_data[i] = 0;
-            }
-            m_node = node;
-        }
-        inline SmallFixedString(): SmallFixedString(nullptr, 0, 0) {}
-        inline uliteral_t const& operator[](size_t i) const {
-            return m_data[i];
-        }
-        inline uliteral_t& operator[](size_t i) {
-            return m_data[i];
-        }
-        inline lz_trie::factorid_t node() const { return m_node; }
-        inline void node(lz_trie::factorid_t n) { m_node = n; }
-    };
-    struct SmallFixedStringEq {
-        size_t m_actual_size = -1;
-
-        inline bool operator()(SmallFixedString const& a,
-                               SmallFixedString const& b) const {
-            DCHECK_LE(m_actual_size, MAX_JUMP_WIDTH);
-            DCHECK_EQ(sizeof(uint64_t) % sizeof(uliteral_t), 0);
-            size_t const elem_in_word = sizeof(uint64_t) / sizeof(uliteral_t);
-
-            if (a.node() != b.node()) return false;
-
-            // compare elements word-wise to speed things up
-            size_t i = 0;
-            while(i + elem_in_word <= m_actual_size) {
-                std::cout << "word compare at i=" << i << std::endl;
-                auto pa = (uint64_t const*) &a[i];
-                auto pb = (uint64_t const*) &b[i];
-                if (*pa != *pb) return false;
-                i += elem_in_word;
-            }
-            for(; i < m_actual_size; i++) {
-                std::cout << "single compare at i=" << i << std::endl;
-                if (a[i] != b[i]) return false;
-            }
-
-            return true;
-        }
-    };
-    struct SmallFixedStringHash {
-        size_t m_actual_size = -1;
-
-        inline size_t operator()(SmallFixedString const& a) const {
-            DCHECK_LE(m_actual_size, MAX_JUMP_WIDTH);
-            DCHECK_EQ(sizeof(uint64_t) % sizeof(uliteral_t), 0);
-            size_t const elem_in_word = sizeof(uint64_t) / sizeof(uliteral_t);
-
-            size_t seed = 0;
-            auto hash_step = [&](uint64_t v) {
-                // hash combine formular stolen from boost
-                size_t hsh = std::hash<uint64_t>()(v);
-                seed ^= hsh + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-            };
-
-            hash_step(a.node());
-
-            // hash elements word-wise to speed things up
-            size_t i = 0;
-            while(i + elem_in_word <= m_actual_size) {
-                std::cout << "word hash at i=" << i << std::endl;
-                auto pa = (uint64_t const*) &a[i];
-                hash_step(*pa);
-                i += elem_in_word;
-            }
-            for(; i < m_actual_size; i++) {
-                std::cout << "single hash at i=" << i << std::endl;
-                hash_step(a[i]);
-            }
-
-            return seed;
-        }
-    };
-    std::unordered_map<
-        SmallFixedString,
-        value_type,
-        SmallFixedStringHash,
-        SmallFixedStringEq
-    > m_jump_pointer_map;
-
-public:
-    using jump_buffer_handle = SmallFixedString;
-
-    FixedBufferPointerJumping(size_t jump_width):
-        m_jump_pointer_map(0, SmallFixedStringHash{jump_width}, SmallFixedStringEq{jump_width}) {}
-
-    inline void set_parent_node(jump_buffer_handle& handle, node_t const& node) {
-        handle.node(node.id());
-    }
-
-    inline uliteral_t* get_buffer(jump_buffer_handle& handle) {
-        return &handle[0];
-    }
-
-    struct result_t {
-        decltype(m_jump_pointer_map)& m_map;
-        typename decltype(m_jump_pointer_map)::const_iterator m_iter;
-
-        inline bool found() const {
-            return m_iter != m_map.end();
-        }
-
-        inline value_type const& get() {
-            DCHECK(found());
-            return m_iter->second;
-        }
-    };
-    inline result_t find(jump_buffer_handle const& key) {
-        return result_t { m_jump_pointer_map, m_jump_pointer_map.find(key) };
-    }
-    inline void insert(jump_buffer_handle const& key, value_type const& val) {
-        m_jump_pointer_map.insert({key, val});
-    }
-};
 
 template <typename coder_t, typename dict_t>
 class LZ78PointerJumpingCompressor: public Compressor {
@@ -161,7 +28,7 @@ private:
         node_t parent;
         node_t node;
     };
-    using pointer_jumping_t = FixedBufferPointerJumping<node_t, NodePair>;
+    using pointer_jumping_t = lz_pointer_jumping::FixedBufferPointerJumping<node_t, NodePair>;
 
     /// Max dictionary size before reset, 0 == unlimited
     const lz_trie::factorid_t m_dict_max_size {0};
