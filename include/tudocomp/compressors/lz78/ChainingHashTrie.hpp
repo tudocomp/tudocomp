85818ef48131a6eb828cf6599b0befd42bdd8be7
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2020-03-07 17:11:25 +0900

    split by key_width

--- include/tudocomp/compressors/lz78/ChainingHashTrie.hpp
+++ include/tudocomp/compressors/lz78/ChainingHashTrie.hpp
@@ -19,15 +19,23 @@ namespace lz78 {
 
 class ChainingHashTrie : public Algorithm, public LZ78Trie<> {
 	static_assert(std::numeric_limits<squeeze_node_t>::radix == 2, "needs radix to be 2");
+	static_assert(std::numeric_limits<factorid_t>::radix == 2, "needs radix to be 2");
 
 	typedef separate_chaining::group::group_chaining_table<> table_t;
     // typedef std::unordered_map<squeeze_node_t, factorid_t, _VignaHasher> table_t;
 
-	table_t m_table;
+	
+	static constexpr uint_fast8_t key_max_width = std::numeric_limits<factorid_t>::digits;
+	static constexpr uint_fast8_t value_max_width = std::numeric_limits<squeeze_node_t>::digits;
+
 
 	static constexpr size_t m_arraysize = 256;
 	uint64_t m_array[m_arraysize];
 	size_t m_elements = 0;
+
+	static constexpr uint_fast8_t m_matrixrows = key_max_width - bits_for(m_arraysize);
+	table_t* m_matrix[m_matrixrows];
+	// table_t m_table;
 
 public:
     inline static Meta meta() {
@@ -35,23 +43,28 @@ public:
         return m;
     }
 
-    inline ChainingHashTrie(Config&& cfg, const size_t n, const size_t& remaining_characters, factorid_t reserve = 0)
+    inline ChainingHashTrie(Config&& cfg, const size_t n, const size_t& remaining_characters, [[maybe_unused]] factorid_t reserve = 0)
         : Algorithm(std::move(cfg))
         , LZ78Trie(n, remaining_characters)
-		, m_table(64, std::numeric_limits<squeeze_node_t>::digits)
+		// , m_table(64, std::numeric_limits<squeeze_node_t>::digits)
     {
-        if(reserve > 0) { m_table.reserve(reserve); }
+        // if(reserve > 0) { m_table.reserve(reserve); }
 		std::fill(m_array, m_array+m_arraysize, -1ULL);
+		std::fill(m_matrix, m_matrix+m_matrixrows, nullptr);
+
     }
     IF_STATS(
         MoveGuard m_guard;
-        inline ~ChainingHashTrie() {
-            if (m_guard) {
-			StatPhase logPhase;
-			m_table.print_stats(logPhase);
-            }
-        }
     )
+		inline ~ChainingHashTrie() {
+			clear();
+    IF_STATS(
+            if (m_guard) {
+			// StatPhase logPhase;
+			// m_table.print_stats(logPhase);
+            }
+    )
+		}
     ChainingHashTrie(ChainingHashTrie&& other) = default;
     ChainingHashTrie& operator=(ChainingHashTrie&& other) = default;
 
@@ -68,9 +81,15 @@ public:
     }
 
     inline void clear() {
-        m_table.clear();
-		m_elements = 0;
+		for(size_t i = 0; i < m_matrixrows; ++i) { 
+			if(m_matrix[i] != nullptr) { 
+				m_matrix[i]->clear(); 
+				delete m_matrix[i];
+			}
+		}
+		std::fill(m_matrix, m_matrix+m_matrixrows, nullptr);
 		std::fill(m_array, m_array+m_arraysize, -1ULL);
+		m_elements = 0;
     }
 
     inline node_t find_or_insert(const node_t& parent_w, uliteral_t c) {
@@ -86,14 +105,27 @@ public:
 			}
 			return node_t(m_array[key], false); // return the factor id of that node
 		}
-		// bits_for(key-m_arraysize)
 
-		const size_t tablesize = m_table.size();
-        auto it = m_table[create_node(parent+1,c)];
+		const squeeze_node_t large_key = key-m_arraysize;
 
-		DCHECK(tablesize == m_table.size() || tablesize+1 == m_table.size());
+		const uint_fast8_t large_key_width = bits_for(large_key);
+		DCHECK_LE(large_key_width, m_matrixrows);
+		if(m_matrix[large_key_width] == nullptr) {
+			m_matrix[large_key_width] = new table_t(large_key_width-1, value_max_width);
+		}
+		const uint64_t subtract_key = (-1ULL>>(64-large_key_width+1));
+		auto& table = *m_matrix[large_key_width];
+		DCHECK_LT(subtract_key, large_key);
+		DCHECK_LT(bits_for(large_key - subtract_key), large_key_width);
+		const squeeze_node_t remaining_key = large_key - subtract_key;
 
-        if(tablesize+1 == m_table.size()) { // not found -> add a new node
+
+		const size_t tablesize = table.size();
+        auto it = table[remaining_key];
+
+		DCHECK(tablesize == table.size() || tablesize+1 == table.size());
+
+        if(tablesize+1 == table.size()) { // not found -> add a new node
 			++m_elements;
 			it = newleaf_id;
 			DCHECK_EQ(it.value(), newleaf_id);
