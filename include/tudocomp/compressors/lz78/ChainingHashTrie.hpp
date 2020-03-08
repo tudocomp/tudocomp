88730367966cce06104f5245d1af1e5b678813d1
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2020-03-08 17:13:12 +0900

    add row solution

--- include/tudocomp/compressors/lz78/ChainingHashTrie.hpp
+++ include/tudocomp/compressors/lz78/ChainingHashTrie.hpp
@@ -25,17 +25,22 @@ class ChainingHashTrie : public Algorithm, public LZ78Trie<> {
     // typedef std::unordered_map<squeeze_node_t, factorid_t, _VignaHasher> table_t;
 
 	
-	static constexpr uint_fast8_t key_max_width = std::numeric_limits<factorid_t>::digits;
-	static constexpr uint_fast8_t value_max_width = std::numeric_limits<squeeze_node_t>::digits;
+	static constexpr uint_fast8_t value_max_width = std::numeric_limits<factorid_t>::digits;
+	static constexpr uint_fast8_t key_max_width = std::numeric_limits<squeeze_node_t>::digits;
 
 
 	static constexpr size_t m_arraysize = 256;
-	uint64_t m_array[m_arraysize];
+	factorid_t m_array[m_arraysize];
 	size_t m_elements = 0;
 
-	static constexpr uint_fast8_t m_matrixrows = key_max_width - bits_for(m_arraysize);
-	table_t* m_matrix[m_matrixrows];
-	// table_t m_table;
+	static constexpr size_t m_smalltable_key_width = 9;
+	table_t m_smalltable;
+
+	static constexpr uint_fast8_t m_rowlength = key_max_width - m_smalltable_key_width - bits_for(m_arraysize);
+	table_t* m_rows[m_rowlength];
+
+	// static constexpr uint_fast8_t m_matrixlength = key_max_width - bits_for(m_arraysize);
+	// table_t** m_matrix[m_rowlength];
 
 public:
     inline static Meta meta() {
@@ -46,24 +51,40 @@ public:
     inline ChainingHashTrie(Config&& cfg, const size_t n, const size_t& remaining_characters, [[maybe_unused]] factorid_t reserve = 0)
         : Algorithm(std::move(cfg))
         , LZ78Trie(n, remaining_characters)
-		// , m_table(64, std::numeric_limits<squeeze_node_t>::digits)
+		, m_smalltable(m_smalltable_key_width, value_max_width)
     {
         // if(reserve > 0) { m_table.reserve(reserve); }
 		std::fill(m_array, m_array+m_arraysize, -1ULL);
-		std::fill(m_matrix, m_matrix+m_matrixrows, nullptr);
+		std::fill(m_rows, m_rows+m_rowlength, nullptr);
 
     }
     IF_STATS(
         MoveGuard m_guard;
     )
 		inline ~ChainingHashTrie() {
-			clear();
-    IF_STATS(
+    // IF_STATS(
             if (m_guard) {
+				for(size_t i = 0; i < m_rowlength; ++i) {
+					if(m_rows[i] == nullptr) { continue; }
+					uint64_t value_widths[65];
+					std::fill(value_widths, value_widths+65, 0);
+					for(auto it = m_rows[i]->begin(); it != m_rows[i]->end(); ++it) {
+						++value_widths[bits_for(it.value())];
+					}
+					for(size_t k = 0; k <= 64; ++k) {
+						std::stringstream s;
+						s << "key_width_" << std::setw(2) << i << "value_width_" << std::setw(2) << k;
+						std::string strkey = s.str();
+						if(value_widths[k] > 0) {
+							StatPhase::log(std::move(strkey), value_widths[k]);
+						}
+					}
+				}
 			// StatPhase logPhase;
 			// m_table.print_stats(logPhase);
             }
-    )
+    // )//STATS
+			clear();
 		}
     ChainingHashTrie(ChainingHashTrie&& other) = default;
     ChainingHashTrie& operator=(ChainingHashTrie&& other) = default;
@@ -81,15 +102,17 @@ public:
     }
 
     inline void clear() {
-		for(size_t i = 0; i < m_matrixrows; ++i) { 
-			if(m_matrix[i] != nullptr) { 
-				m_matrix[i]->clear(); 
-				delete m_matrix[i];
+		for(size_t i = 0; i < m_rowlength; ++i) { 
+			if(m_rows[i] != nullptr) { 
+				m_rows[i]->clear(); 
+				delete m_rows[i];
+				m_rows[i] = nullptr;
 			}
 		}
-		std::fill(m_matrix, m_matrix+m_matrixrows, nullptr);
+		std::fill(m_rows, m_rows+m_rowlength, nullptr);
 		std::fill(m_array, m_array+m_arraysize, -1ULL);
 		m_elements = 0;
+		m_smalltable.clear();
     }
 
     inline node_t find_or_insert(const node_t& parent_w, uliteral_t c) {
@@ -98,44 +121,70 @@ public:
 
 		const squeeze_node_t key = create_node(parent+1,c);
 		if(key < m_arraysize) {
-			if(m_array[key] == -1ULL) { // not found -> add a new node
+			if(m_array[key] == static_cast<factorid_t>(-1ULL)) { //! not found -> add a new node
 				++m_elements;
 				m_array[key] = newleaf_id;
 				return node_t(size() - 1, true); 
 			}
-			return node_t(m_array[key], false); // return the factor id of that node
+			return node_t(m_array[key], false); //! return the factor id of that node
 		}
 
-		const squeeze_node_t large_key = key-m_arraysize;
-
+		const squeeze_node_t large_key = key-m_arraysize; //! since we know that the key is larger than `m_arraysize`, we can subtract this value from the key we want to store
 		const uint_fast8_t large_key_width = bits_for(large_key);
-		DCHECK_LE(large_key_width, m_matrixrows);
-		if(m_matrix[large_key_width] == nullptr) {
-			m_matrix[large_key_width] = new table_t(large_key_width-1, value_max_width);
+		if(large_key_width < m_smalltable_key_width) { //! use the small table
+			const size_t tablesize = m_smalltable.size();
+			DCHECK_LE(large_key_width, m_smalltable.key_width());
+			auto it = m_smalltable[large_key];
+			DCHECK(tablesize == m_smalltable.size() || tablesize+1 == m_smalltable.size());
+			if(tablesize+1 == m_smalltable.size()) { //! not found -> add a new node
+				++m_elements;
+				it = newleaf_id;
+				DCHECK_EQ(it.value(), newleaf_id);
+				return node_t(size() - 1, true); 
+			}
+			return node_t(it.value(), false); //! return the factor id of that node
 		}
-		const uint64_t subtract_key = (-1ULL>>(64-large_key_width+1));
-		auto& table = *m_matrix[large_key_width];
-		DCHECK_LT(subtract_key, large_key);
+
+		// DCHECK_LT(large_key_width, m_rowlength);
+		// if(m_rows[large_key_width] == nullptr) {
+		// 	m_rows[large_key_width] = new table_t(large_key_width, value_max_width);
+		// }
+		// auto& table = *m_rows[large_key_width];
+		// const size_t tablesize = table.size();
+        // auto it = table[large_key];
+		// DCHECK(tablesize == table.size() || tablesize+1 == table.size());
+
+		// //TODO: shift large_key_width by m_smalltable_key_width
+		
+		const size_t rowindex = large_key_width - m_smalltable_key_width; //! index in m_rows for this key
+		DCHECK_LE(rowindex, m_rowlength); 
+		if(m_rows[rowindex] == nullptr) {
+			m_rows[rowindex] = new table_t(large_key_width-1, value_max_width);
+		}
+		const uint64_t subtract_key = 1ULL<<(large_key_width-1); //(-1ULL>>(64-large_key_width+1)); 
+		auto& table = *m_rows[rowindex];
+		DCHECK_LE(subtract_key, large_key);
 		DCHECK_LT(bits_for(large_key - subtract_key), large_key_width);
 		const squeeze_node_t remaining_key = large_key - subtract_key;
-
+		DCHECK_GE(table.key_width(), bits_for(remaining_key));
 
 		const size_t tablesize = table.size();
         auto it = table[remaining_key];
-
 		DCHECK(tablesize == table.size() || tablesize+1 == table.size());
 
-        if(tablesize+1 == table.size()) { // not found -> add a new node
+        if(tablesize+1 == table.size()) { //! not found -> add a new node
 			++m_elements;
+			// DCHECK_LT(newleaf_id, key);
 			it = newleaf_id;
 			DCHECK_EQ(it.value(), newleaf_id);
             return node_t(size() - 1, true); 
         }
 
-        return node_t(it.value(), false); // return the factor id of that node
+        return node_t(it.value(), false); //! return the factor id of that node
     }
 
     inline size_t size() const {
+		DCHECK_LT(m_elements, std::numeric_limits<factorid_t>::max());
         return m_elements;
     }
 };
