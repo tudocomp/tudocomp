9f23b3f16e15967d8529928e438c34acefeccdae
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2020-03-10 18:03:56 +0900

    fixed ChainingHashTrie.hpp

--- include/tudocomp/compressors/lz78/ChainingHashTrie.hpp
+++ include/tudocomp/compressors/lz78/ChainingHashTrie.hpp
@@ -25,13 +25,15 @@ class ChainingHashTrie : public Algorithm, public LZ78Trie<> {
     // typedef std::unordered_map<squeeze_node_t, factorid_t, _VignaHasher> table_t;
 
 	
-	static constexpr uint_fast8_t value_max_width = std::numeric_limits<factorid_t>::digits;
-	static constexpr uint_fast8_t key_max_width = std::numeric_limits<squeeze_node_t>::digits;
+	static constexpr uint_fast8_t value_max_width = std::min<uint_fast8_t>( bits_for(std::numeric_limits<len_t>::max() * ALPHABET_BITS / std::numeric_limits<len_t>::digits)  ,  std::numeric_limits<factorid_t>::digits);
+	static constexpr uint_fast8_t key_max_width = std::min<uint_fast8_t>(value_max_width+ALPHABET_BITS, std::numeric_limits<squeeze_node_t>::digits);
 
+	factorid_t m_max_rootnode = 0; //! the highest node ID of a node added with `add_rootnode`
 
+	//! TODO: m_array is useless for LZW -> can we templatize this approach such that LZW does not use `m_array` at all?
 	static constexpr size_t m_arraysize = 256;
 	factorid_t m_array[m_arraysize];
-	size_t m_elements = 0;
+	size_t m_elements = 0; //! number of stored elementsn including `m_max_rootnode`
 
 	static constexpr size_t m_smalltable_key_width = 9;
 	table_t m_smalltable;
@@ -39,8 +41,18 @@ class ChainingHashTrie : public Algorithm, public LZ78Trie<> {
 	static constexpr uint_fast8_t m_rowlength = 10; 
 	table_t* m_rows[m_rowlength];
 
-	static constexpr uint_fast8_t m_matrixlength = key_max_width - m_rowlength - m_smalltable_key_width - bits_for(m_arraysize);
-	table_t** m_matrix[m_rowlength];
+
+	static constexpr uint_fast8_t m_matrixlength = 1 + std::max<int32_t>(0, static_cast<int32_t>(key_max_width) - m_rowlength - m_smalltable_key_width);
+	table_t** m_matrix[m_matrixlength];
+	uint8_t m_matrix_allocated[m_matrixlength]; //! stores the largest index of an allocated table in a row of m_matrix
+
+	constexpr size_t matrixrow_offset(const size_t rowindex)  {
+		return rowindex+m_smalltable_key_width+m_rowlength- ALPHABET_BITS;
+	}
+
+	constexpr size_t matrixrow_width(const size_t rowindex)  {
+		return 1 + value_max_width - matrixrow_offset(rowindex);
+	}
 
 public:
     inline static Meta meta() {
@@ -57,14 +69,18 @@ public:
 		std::fill(m_array, m_array+m_arraysize, -1ULL);
 		std::fill(m_rows, m_rows+m_rowlength, nullptr);
 		std::fill(m_matrix, m_matrix+m_matrixlength, nullptr);
-
+		std::fill(m_matrix_allocated, m_matrix_allocated+m_matrixlength, 0);
     }
     IF_STATS(
         MoveGuard m_guard;
     )
 		inline ~ChainingHashTrie() {
-    // IF_STATS(
-            // if (m_guard) {
+    IF_STATS(
+			StatPhase::wrap("chain stats", [&]{
+            if (m_guard) {
+			StatPhase::log("value_max_width", value_max_width);
+			StatPhase::log("key_max_width", key_max_width);
+
 				size_t array_filled = 0;
 				for(size_t i = 0; i < m_arraysize; ++i) {
 					if(m_array[i] == static_cast<factorid_t>(-1ULL)) { continue; }
@@ -93,29 +109,31 @@ public:
 				}
 				for(size_t i = 0; i < m_matrixlength; ++i) {
 					if(m_matrix[i] == nullptr) { continue; }
-					for(size_t k = 0; k < value_max_width; ++k) {
+					for(size_t k = 0; k < matrixrow_width(i); ++k) {
 						if(m_matrix[i][k] == nullptr) { continue; }
 						std::stringstream s;
-						s << "m[" << std::setw(2) << i << "][" << std::setw(2) <<  k << "]";
+						s << "m[" << std::setw(2) << i << "][" << std::setw(2) << (k+matrixrow_offset(i)) << "]";
 						std::string strkey = s.str();
 						StatPhase::log(std::move(strkey), m_matrix[i][k]->size());
 					}
 				}
 			// StatPhase logPhase;
 			// m_table.print_stats(logPhase);
-            // }
-    // )//STATS
+            }//guard
+			});//statphase
+    )//STATS
 			clear();
 		}
     ChainingHashTrie(ChainingHashTrie&& other) = default;
     ChainingHashTrie& operator=(ChainingHashTrie&& other) = default;
 
     inline node_t add_rootnode(uliteral_t c) { //TODO: optimize -> seems that we do not need to store anything in m_array!
-		DCHECK_LT(create_node(0, c), m_arraysize);
-		m_array[create_node(0, c)] = size();
+		m_max_rootnode = c;
+		// DCHECK_LT(create_node(0, c), m_arraysize);
+		// m_array[create_node(0, c)] = size();
 		++m_elements;
 		// m_table[create_node(0, c)] = size();
-        return node_t(size() - 1, true);
+        return get_rootnode(c); 
     }
 
     inline node_t get_rootnode(uliteral_t c) const {
@@ -125,7 +143,7 @@ public:
     inline void clear() {
 		for(size_t i = 0; i < m_matrixlength; ++i) { 
 			if(m_matrix[i] == nullptr) { continue; }
-			for(size_t j = 0; j < value_max_width; ++j) {
+			for(size_t j = 0; j < matrixrow_width(i); ++j) {
 				if(m_matrix[i][j] != nullptr) {
 					delete m_matrix[i][j];
 				}
@@ -142,6 +160,7 @@ public:
 		std::fill(m_matrix, m_matrix+m_matrixlength, nullptr);
 		std::fill(m_rows, m_rows+m_rowlength, nullptr);
 		std::fill(m_array, m_array+m_arraysize, -1ULL);
+		std::fill(m_matrix_allocated, m_matrix_allocated+m_matrixlength, 0);
 		m_elements = 0;
 		m_smalltable.clear();
     }
@@ -150,7 +169,13 @@ public:
         auto parent = parent_w.id();
         const factorid_t newleaf_id = size(); //! if we add a new node, its index will be equal to the current size of the dictionary
 
-		const squeeze_node_t key = create_node(parent+1,c);
+		squeeze_node_t key = create_node(parent,c);
+		// DCHECK_NE(key, 0);
+		// if(key <= m_max_rootnode) {
+		// 	return node_t(key, false); //! return the factor id of that node
+		// }
+		// key -= m_max_rootnode;
+
 		if(key < m_arraysize) {
 			if(m_array[key] == static_cast<factorid_t>(-1ULL)) { //! not found -> add a new node
 				++m_elements;
@@ -218,8 +243,8 @@ public:
 		const size_t rowindex = large_key_width - m_smalltable_key_width - m_rowlength; //! index in m_matrix for this key
 		DCHECK_LT(rowindex, m_matrixlength);
 		if(m_matrix[rowindex] == nullptr) {
-			m_matrix[rowindex] = new table_t*[value_max_width];
-			std::fill(m_matrix[rowindex], m_matrix[rowindex]+value_max_width, nullptr);
+			m_matrix[rowindex] = new table_t*[matrixrow_width(rowindex)];
+			std::fill(m_matrix[rowindex], m_matrix[rowindex]+matrixrow_width(rowindex), nullptr);
 		}
 		
 		const uint64_t subtract_key = 1ULL<<(large_key_width-1); //(-1ULL>>(64-large_key_width+1)); 
@@ -228,22 +253,41 @@ public:
 		const squeeze_node_t remaining_key = large_key - subtract_key;
 		table_t**const rowtable = m_matrix[rowindex];
 
-		for(size_t k = 0; k < value_max_width; ++k) {
+		DCHECK_LT(m_matrix_allocated[rowindex], matrixrow_width(rowindex));
+		ON_DEBUG(
+			for(size_t l = m_matrix_allocated[rowindex]+1; l < matrixrow_width(rowindex); ++l) {
+				DCHECK(rowtable[l] == nullptr);
+			} 
+		)
+		for(size_t k = 0; k <= m_matrix_allocated[rowindex]; ++k) { //! check whether the key is in one of the hash tables of `rowtable`
 			if(rowtable[k] != nullptr) {
-				DCHECK_GE(k, rowindex+m_smalltable_key_width+m_rowlength- ALPHABET_BITS); //TODO: use for optimization
 				auto& table = *rowtable[k];
 				const auto ret = table.locate(remaining_key);
 				if(ret.second != -1ULL) { //! found node
-					return node_t(table.value_at(ret.first, ret.second), false); //! return the factor id of that node
+					const uint_fast8_t value_width = k + matrixrow_offset(rowindex); //! have to restore the value by adding the subtracted amount when inserting it
+					const factorid_t remaining_value = table.value_at(ret.first, ret.second);
+					const factorid_t value = remaining_value + (1ULL<<(value_width-1));
+
+					return node_t(value, false); //! return the factor id of that node
 				}
 			}
+
+			//TODO: if rowtable == nullptr => all other are also nullptr? then `m_matrix_allocated` is not needed.
 		}
-		const uint_fast8_t columnindex = bits_for(newleaf_id);
-		DCHECK_GE(columnindex, rowindex+m_smalltable_key_width+m_rowlength - ALPHABET_BITS); //TODO: use for optimization
+		DCHECK_LE(matrixrow_offset(rowindex), bits_for(newleaf_id));
+		const uint_fast8_t value_width = bits_for(newleaf_id);
+		const uint_fast8_t columnindex = value_width - matrixrow_offset(rowindex);
+		DCHECK_LT(columnindex, matrixrow_width(rowindex));
 		if(rowtable[columnindex] == nullptr) {
-			rowtable[columnindex] = new table_t(large_key_width-1, columnindex);
+			rowtable[columnindex] = new table_t(large_key_width-1, value_width-1); //! we save one value-bit by subtracting `subtract_value` in the following
+			m_matrix_allocated[rowindex] = std::max<uint8_t>(m_matrix_allocated[rowindex], columnindex);
 		}
-		rowtable[columnindex]->find_or_insert(remaining_key, newleaf_id);
+		const uint64_t subtract_value = 1ULL<<(value_width-1); //(-1ULL>>(64-large_value_width+1)); 
+		DCHECK_LE(subtract_value, newleaf_id);
+		DCHECK_LT(bits_for(newleaf_id - subtract_value), value_width);
+		const factorid_t remaining_value = newleaf_id - subtract_value;
+
+		rowtable[columnindex]->find_or_insert(remaining_key, remaining_value);
 		++m_elements;
 		return node_t(size() - 1, true); 
     }
