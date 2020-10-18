4f79e115a10ceb0f7724c712224691167a697de4
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2020-10-19 08:46:35 +0900

    comment lexparse

--- include/tudocomp/compressors/lcpcomp/compress/LexParseStrategy.hpp
+++ include/tudocomp/compressors/lcpcomp/compress/LexParseStrategy.hpp
@@ -14,51 +14,44 @@
 namespace tdc {
 namespace lcpcomp {
 
-/// A very naive selection strategy for LCPComp.
-///
-/// TODO: Describe
-class LexParseStrategy : public Algorithm {
+/**
+ * the lex-parse compression strategy from the paper
+ *
+ * On the Approximation Ratio of Greedy Parsings
+ * Gonzalo Navarro, Nicola Prezza
+ */
+class LexParseStrategy: public Algorithm {
 public:
     using Algorithm::Algorithm;
 
     inline static Meta meta() {
-        Meta m(comp_strategy_type(), "lexparse", "using lexparse [Prezza,Navarro]");
+        Meta m(comp_strategy_type(), "lexparse", "Greedy parse in text order, referring to lexicographically next-smaller suffix. Name coined by Nicola Prezza");
         return m;
     }
 
     template<typename ds_t>
     inline static void construct_textds(ds_t& ds) {
-        ds.template construct<
-            ds::SUFFIX_ARRAY,
-            ds::PLCP_ARRAY,
-            ds::INVERSE_SUFFIX_ARRAY>();
+        ds.template construct< ds::PHI_ARRAY, ds::PLCP_ARRAY>();
     }
 
     template<typename text_t, typename factorbuffer_t>
     inline void factorize(text_t& text, size_t threshold, factorbuffer_t& factors) {
-	StatPhase::wrap("compute lexparse", [&]{
-            const auto& sa = text.template get<ds::SUFFIX_ARRAY>();
-            const auto& isa = text.template get<ds::INVERSE_SUFFIX_ARRAY>();
-
+		StatPhase::wrap("Search Peaks", [&]{
+            const auto& phi =  text.template get<ds::PHI_ARRAY>();
             auto plcp = text.template relinquish<ds::PLCP_ARRAY>();
 
-            //
-            const size_t n = sa.size();
-		    len_t last_replacement_pos = 0;
+            const size_t n = phi.size();
+
 		    for(len_t i = 0; i+1 < n; ) {
-			    if(plcp[i] >= threshold) {
-				    DCHECK_NE(isa[i], 0u);
-				    const len_t& target_position = i;
-				    const len_t factor_length = plcp[target_position];
-				    DCHECK_LT(target_position+factor_length,n);
-				    const len_t source_position = sa[isa[target_position]-1];
-				    factors.emplace_back(i, source_position, factor_length);
-				    i+= factor_length;
-				    last_replacement_pos = i-1;
-			    }
-			    else {
-				    ++i;
-			    }
+			if(plcp[i] >= threshold) {
+			    const len_t factor_length = plcp[i];
+			    DCHECK_LT(i+factor_length,n);
+			    const len_t source_position = phi[i];
+			    factors.emplace_back(i, source_position, factor_length);
+			    i+= factor_length;
+			} else {
+			    ++i;
+			}
 		    }
         });
     }
