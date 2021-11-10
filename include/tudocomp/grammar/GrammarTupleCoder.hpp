bf416a861af8ab9fc6baab6059f75b528aef1f6c
Etienne Palanga <etieti22@gmail.com>
2021-11-10 15:42:18 +0100

    Add documentation

--- include/tudocomp/grammar/GrammarTupleCoder.hpp
+++ include/tudocomp/grammar/GrammarTupleCoder.hpp
@@ -8,7 +8,18 @@ namespace tdc {
 namespace grammar {
     
 
-
+/**
+ * @brief Codes a grammar's rules into tuples of their length in symbols and a list of their symbols.
+ * 
+ * In the encoded symbol list a 0 bit precedes a terminal and a 1 bit precedes a non-terminal.
+ * The rules are encoded in an order which rules that depend on no other rule (i.e. contains only terminals) are encoded first. 
+ * For a rule to be encoded at a certain point in time, all rules which this rule depends on, have to be encoded already.
+ * This allows for all encoded non-terminals to be references to rules which have already be en- or decoded. 
+ * 
+ * @tparam len_coder_t The coder to use for the length of the rules
+ * @tparam terminal_coder_t The coder to use for terminals
+ * @tparam nonterminal_coder_t The coder to use for non-terminals
+ */
 template<typename len_coder_t, typename terminal_coder_t, typename nonterminal_coder_t>
 class GrammarTupleCoder : public Algorithm {
     
@@ -25,8 +36,6 @@ public:
         return m;
     }
     
-    
-    //template<typename len_coder_t, typename terminal_coder_t, typename nonterminal_coder_t>
     class Encoder {         
         std::shared_ptr<BitOStream> m_out;
         std::unique_ptr<typename len_coder_t::Encoder> m_len_encoder;        
@@ -46,8 +55,10 @@ public:
                 return;
             }
 
+            // Renumber the rules, such that the rules with and id n only depends on rules with id k < n
             grammar.dependency_renumber();
 
+            // Determine the maximum and minimum rule length in the grammar respectively
             size_t min_len = std::numeric_limits<size_t>().max();
             size_t max_len = std::numeric_limits<size_t>().min();
             for (auto rule : *grammar) {
@@ -55,29 +66,29 @@ public:
                 max_len = std::max(max_len, rule.second.size());
             }
             
-
+            // Encode the minimum and maximum rule lengths
             Range rule_len_r = Range(min_len, max_len);
             m_len_encoder->template encode<size_t>(min_len, size_r);
             m_len_encoder->template encode<size_t>(max_len, size_r);
 
-            size_t current_rule_id = 0;
-            for (auto it = (*grammar).begin(); it != (*grammar).end(); it++) {
-                
-                auto symbols = (*it).second;
+            // Iterate through the grammar
+            for (const auto &[current_rule_id, symbols] : *grammar){
+                // Encode the rule's length
                 auto length = symbols.size();
                 m_len_encoder->encode(length, rule_len_r);
                 
-                for(size_t i = 0; i < symbols.size(); i++){
-                    auto symbol = symbols[i];
+                // Iterate through the rule's symbols
+                for(auto symbol : symbols){
+                    // If the symbol is a terminal, write a 0 bit and then encode the symbol with the terminal encoder 
                     if(Grammar::is_terminal(symbol)) {
                         m_out->write_bit(false);
                         m_terminal_encoder->encode((char) symbol, uliteral_r);
                         continue;
                     }
+                    // If the symbol is a nonterminal, write a 1 bit and then encode the rule id with the nonterminal encoder
                     m_out->write_bit(true);
                     m_nonterminal_encoder->encode(symbol - Grammar::RULE_OFFSET, Range(0, current_rule_id));
                 }
-                current_rule_id++;
             }
         }
     };
@@ -100,29 +111,36 @@ public:
     
         Grammar decode_grammar() {
             Grammar gr;
+            // If there is nothing to read, just return the empty grammar
             if (m_in->eof()) return gr;
 
+            // Decode the minimum and maximum rule lengths
             size_t min_len = m_len_decoder->template decode<size_t>(size_r);
             size_t max_len = m_len_decoder->template decode<size_t>(size_r);
             Range rule_len_r(min_len, max_len);
 
             size_t current_rule_id = 0;
             while(!m_in->eof()) {
-
+                
+                // Decode the rule length
                 size_t rule_len = m_len_decoder->template decode<size_t>(rule_len_r);
+                // Decode rule_len symbols 
                 for (size_t i = 0; i < rule_len; i++) {
+                    // Read a bit from the input. If it is 1, the next symbol is a nonterminal. If it is 0, it is a terminal
                     bool is_nonterminal = m_in->read_bit();
+                    // If it is a terminal, use the terminal decoder to decode the next value as a character and insert it into the grammar
                     if(!is_nonterminal) {
                         char terminal = m_terminal_decoder->template decode<char>(uliteral_r);
                         gr.append_terminal(current_rule_id, terminal);
                         continue;
                     }
-
+                    // If it is a nonterminal read the rule id from the grammar and insert it into the grammar
                     size_t rule_id = m_nonterminal_decoder->template decode<size_t>(Range(0, current_rule_id));
                     gr.append_nonterminal(current_rule_id, rule_id);
                 }
                 current_rule_id++;
             }
+            // The rule that was read last, is the one with the most dependencies. That rule is the start rule.
             gr.set_start_rule_id(current_rule_id - 1);
             return gr;
         }
